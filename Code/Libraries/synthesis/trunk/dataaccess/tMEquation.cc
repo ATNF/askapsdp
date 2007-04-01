67916f8509efb26118fa82aebc445370e78da306
Maxim Voronkov <maxim.voronkov@csiro.au>
2007-04-01 10:09:40 +0000

    data accessor is moved to a separate directory
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@60 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Libraries/synthesis/trunk/dataaccess/tMEquation.cc
@@ -0,0 +1,71 @@
+#include <casa/aips.h>
+#include <casa/Exceptions.h>
+#include <lattices/lattices/Lattice.h>
+#include <lattices/lattices/LatticeExpr.h>
+#include <lattices/lattices/LatticeExprNode.h>
+
+#include <iostream.h>
+
+#include "MESimpleSolver.h"
+#include "MEDataSource.h"
+#include "MEComponentEquation.h"
+#include "MEParams.h"
+#include "MEParamsTable.h"
+#include "MEDomain.h"
+#include "MEQuality.h"
+
+using namespace conrad;
+ 
+// Someone needs these templates - I don't know who!
+casa::Matrix<casa::String> c0;
+
+int main() {
+	
+	try {
+		// Initialize the parameters
+		MEParams ip;
+		ip.add("Direction.RA");
+		ip.add("Direction.DEC");
+		ip.add("Flux.I");
+		ip.add("Flux.Q");
+		ip.add("Flux.U");
+		ip.add("Flux.V");
+		
+		MEParamsTable iptab;
+		MEDomain everything;
+		if(!iptab.getParameters(ip, everything)) {
+			cout << "Failed to retrieve old values of parameters" << endl;
+		};
+		
+		// The equation
+		MEComponentEquation cie;
+		
+		// Use the simple solver
+		MESimpleSolver is(ip);
+		is.init();
+
+		// Loop through data, adding equations to the solver
+		MEDataSource msds;
+		msds.init();
+		while (msds.next()) {
+			cie.calcDerivatives(ip, msds.ida(), is);
+		}
+
+		// Now we can do solution
+		MEQuality quality;
+		if(is.solve(quality)) {
+			cout << "Solution succeeded" << endl;
+			iptab.setParameters(is.getParameters(), everything);
+		}
+		else {
+			cout << "Solution failed" << endl;
+		}
+		
+		
+    } catch (casa::AipsError x) {
+        cout << "Caught an exception: " << x.getMesg() << endl;
+        return 1;
+    } 
+	return 0;
+}
+
