5d9cddeb6f4b8851e184f721b017e70280615a3e
Malte Marquarding <malte.marquarding@csiro.au>
2007-05-10 04:09:23 +0000

    added tcsh source script option and some more option
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@197 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- initenv.py
+++ initenv.py
@@ -9,14 +9,61 @@
 import sys
 import os
 
-filename = "initconrad.sh"
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", "--shell",
+                  dest="shell",
+                  action="store", 
+                  type="choice",
+		  choices=["bash", "tcsh"],
+                  default="bash",
+                  help="specify the type of shell to generate the script for")
+
+(opts, args) = parser.parse_args()
+
+shmap = { "bash" : { "suffix": "sh", "envset": "export", 
+		     "envassign": "=",
+		     "init" : "."},
+	  "tcsh" : { "suffix": "csh", "envset": "setenv", 
+		     "envassign": " ",
+		     "init" : "source"} }
+
+
+shell =  shmap[opts.shell]
+
+filename = "initconrad.%s" % shell["suffix"]
 if os.path.exists(filename):
     sys.exit(0)
 f = file(filename, "w")
-exports = """
-export CONRAD_PROJECT_ROOT=%s
-export PYTHONPATH=${CONRAD_PROJECT_ROOT}/Tools/Dev/scons-tools
-""" % os.getcwd()
+exports = "%s CONRAD_PROJECT_ROOT%s%s\n" % ( shell["envset"], 
+					     shell["envassign"],
+					     os.getcwd() )
+exports += "%s PYTHONPATH%s${CONRAD_PROJECT_ROOT}/lib/python:${CONRAD_PROJECT_ROOT}/Tools/Dev/scons-tools\n" % (shell["envset"], shell["envassign"])
+exports += "%s PATH%s${CONRAD_PROJECT_ROOT}/bin:${PATH}" % (shell["envset"], 
+							    shell["envassign"])
 f.write(exports)
 f.close()
-print "Created initconrad.sh, please run '. initconrad.sh' to initalise the environment"
+
+if not os.path.exists("lib"):
+    os.mkdir("lib")
+    os.mkdir("python")
+if not os.path.exists("bin"):
+    os.mkdir("bin")
+dconf = os.path.expanduser("~/.pydistutils.cfg")
+
+if os.path.exists(dconf):
+    print "Moving existing ~/.pydistutils.cfg out of the way"
+    os.rename(dconf, dconf+".save")
+    print """Warning this changes the default location of the 
+installation of python packages to the CONRAD tree."""
+
+f = file(dconf, "w")
+f.write("""
+[install]
+install_lib = $CONRAD_PROJECT_ROOT/lib/python
+install_scripts = $CONRAD_PROJECT_ROOT/bin
+""")
+f.close()
+
+print "Created initconrad.%s, please run '%s initconrad.%s' to initalise the environment" % ( shell["suffix"], shell["init"], shell["suffix"] )
