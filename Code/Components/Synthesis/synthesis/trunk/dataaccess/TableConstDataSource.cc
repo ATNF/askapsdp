209ab21e6a20d718f0bb0ec6e32cf8dea7b52f89
Maxim Voronkov <maxim.voronkov@csiro.au>
2007-06-14 03:48:12 +0000

    TableConstDataSource - a data source implementation to work with TableConstDataIterator
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@303 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/trunk/dataaccess/TableConstDataSource.cc
@@ -0,0 +1,100 @@
+/// @file TableConstDataSource.cc
+///
+/// TableConstDataSource: Allow read-only access to the data stored in the
+/// measurement set. This class implements IConstDataSource interface.
+/// 
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Max Voronkov <maxim.voronkov@csiro.au>
+///
+
+/// own includes
+#include <dataaccess/TableConstDataSource.h>
+#include <dataaccess/TableConstDataIterator.h>
+#include <dataaccess/TableDataSelector.h>
+#include <dataaccess/BasicDataConverter.h>
+#include <dataaccess/DataAccessError.h>
+
+using namespace conrad;
+using namespace synthesis;
+using namespace casa;
+
+/// construct a read-only data source object
+/// @param[in] fname file name of the measurement set to use
+///
+TableConstDataSource::TableConstDataSource(const std::string &fname) :
+         itsMS(fname) {}
+
+/// create a converter object corresponding to this type of the
+/// DataSource. The user can change converting policies (units,
+/// reference frames) by appropriate calls to this converter object
+/// and pass it back to createConstIterator(...). The data returned by
+/// the iteratsr will automatically be in the requested frame/units
+///
+/// @return a shared pointer to a new DataConverter object
+///
+/// The method acts as a factory by creating a new DataConverter.
+/// The lifetime of this converter is the same as the lifetime of the
+/// DataSource object. Therefore, it can be reused multiple times,
+/// if necessary. However, the behavior of iterators created
+/// with a particular DataConverter is undefined, if you change
+/// the DataConverter after the creation of an iterator, unless you
+/// call init() of the iterator (and start a new iteration loop).
+IDataConverterPtr TableConstDataSource::createConverter() const
+{
+  return IDataConverterPtr(new BasicDataConverter);
+}
+
+/// get iterator over a selected part of the dataset represented
+/// by this DataSource object with an explicitly specified conversion
+/// policy. This is the most general createConstIterator(...) call, 
+/// which is used as a default implementation for all less general 
+/// cases (although they can be overriden in the derived classes, if it 
+/// will be necessary because of the performance issues)
+///
+/// @param[in] sel a shared pointer to the selector object defining 
+///            which subset of the data is used
+/// @param[in] conv a shared pointer to the converter object defining
+///            reference frames and units to be used
+/// @return a shared pointer to DataIterator object
+///
+/// The method acts as a factory by creating a new DataIterator.
+/// The lifetime of this iterator is the same as the lifetime of
+/// the DataSource object. Therefore, it can be reused multiple times,
+/// if necessary. Call init() to rewind the iterator.
+boost::shared_ptr<IConstDataIterator>
+TableConstDataSource::createConstIterator(const IDataSelectorConstPtr &sel,
+              const IDataConverterConstPtr &conv) const
+{
+   // cast input selector to "implementation" interface
+   boost::shared_ptr<ITableDataSelectorImpl const> implSel=
+           boost::dynamic_pointer_cast<ITableDataSelectorImpl const>(sel);
+   boost::shared_ptr<IDataConverterImpl const> implConv=
+           boost::dynamic_pointer_cast<IDataConverterImpl const>(conv);
+   	   
+   if (!implSel || !implConv) {
+       CONRADTHROW(DataAccessLogicError, "Incompatible selector and/or "<<
+                 "converter are received by the createConstIterator method");
+   }
+   return boost::shared_ptr<IConstDataIterator>(new TableConstDataIterator(
+                itsMS,implSel,implConv));
+}
+
+/// create a selector object corresponding to this type of the
+/// DataSource
+///
+/// @return a shared pointer to the DataSelector corresponding to
+/// this type of DataSource. DataSource acts as a factory and
+/// creates a selector object of the appropriate type
+///
+/// This method acts as a factory by creating a new DataSelector
+/// appropriate to the given DataSource. The lifetime of the
+/// DataSelector is the same as the lifetime of the DataSource 
+/// object. Therefore, it can be reused multiple times,
+/// if necessary. However, the behavior of iterators already obtained
+/// with this DataSelector is undefined, if one changes the selection
+/// unless the init method is called for the iterator (and the new
+/// iteration loop is started).
+IDataSelectorPtr TableConstDataSource::createSelector() const
+{
+  return IDataSelectorPtr(new TableDataSelector(itsMS));
+}
---
+++ Code/Components/Synthesis/synthesis/trunk/dataaccess/TableConstDataSource.h
@@ -0,0 +1,99 @@
+/// @file TableConstDataSource.h
+///
+/// TableConstDataSource: Allow read-only access to the data stored in the
+/// measurement set. This class implements IConstDataSource interface.
+/// 
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Max Voronkov <maxim.voronkov@csiro.au>
+///
+
+#ifndef TABLE_CONST_DATA_SOURCE_H
+#define TABLE_CONST_DATA_SOURCE_H
+
+/// boost includes
+#include <boost/shared_ptr.hpp>
+
+/// casa includes
+#include <tables/Tables/Table.h>
+
+/// own includes
+#include <dataaccess/IConstDataSource.h>
+
+/// std includes
+#include <string>
+
+namespace conrad {
+
+namespace synthesis {
+
+class TableConstDataSource : virtual public IConstDataSource
+{
+public:
+  /// construct a read-only data source object
+  /// @param[in] fname file name of the measurement set to use
+  ///
+  TableConstDataSource(const std::string &fname);
+  
+  /// create a converter object corresponding to this type of the
+  /// DataSource. The user can change converting policies (units,
+  /// reference frames) by appropriate calls to this converter object
+  /// and pass it back to createConstIterator(...). The data returned by
+  /// the iteratsr will automatically be in the requested frame/units
+  ///
+  /// @return a shared pointer to a new DataConverter object
+  ///
+  /// The method acts as a factory by creating a new DataConverter.
+  /// The lifetime of this converter is the same as the lifetime of the
+  /// DataSource object. Therefore, it can be reused multiple times,
+  /// if necessary. However, the behavior of iterators created
+  /// with a particular DataConverter is undefined, if you change
+  /// the DataConverter after the creation of an iterator, unless you
+  /// call init() of the iterator (and start a new iteration loop).
+  virtual IDataConverterPtr createConverter() const;
+  
+  /// get iterator over a selected part of the dataset represented
+  /// by this DataSource object with an explicitly specified conversion
+  /// policy. This is the most general createConstIterator(...) call, 
+  /// which is used as a default implementation for all less general 
+  /// cases (although they can be overriden in the derived classes, if it 
+  /// will be necessary because of the performance issues)
+  ///
+  /// @param[in] sel a shared pointer to the selector object defining 
+  ///            which subset of the data is used
+  /// @param[in] conv a shared pointer to the converter object defining
+  ///            reference frames and units to be used
+  /// @return a shared pointer to DataIterator object
+  ///
+  /// The method acts as a factory by creating a new DataIterator.
+  /// The lifetime of this iterator is the same as the lifetime of
+  /// the DataSource object. Therefore, it can be reused multiple times,
+  /// if necessary. Call init() to rewind the iterator.
+  virtual boost::shared_ptr<IConstDataIterator> createConstIterator(const
+             IDataSelectorConstPtr &sel,
+             const IDataConverterConstPtr &conv) const;
+
+  /// create a selector object corresponding to this type of the
+  /// DataSource
+  ///
+  /// @return a shared pointer to the DataSelector corresponding to
+  /// this type of DataSource. DataSource acts as a factory and
+  /// creates a selector object of the appropriate type
+  ///
+  /// This method acts as a factory by creating a new DataSelector
+  /// appropriate to the given DataSource. The lifetime of the
+  /// DataSelector is the same as the lifetime of the DataSource 
+  /// object. Therefore, it can be reused multiple times,
+  /// if necessary. However, the behavior of iterators already obtained
+  /// with this DataSelector is undefined, if one changes the selection
+  /// unless the init method is called for the iterator (and the new
+  /// iteration loop is started).
+  virtual IDataSelectorPtr createSelector() const;
+private:
+  casa::Table itsMS; // a measurement set to work with
+};
+ 
+} // namespace synthesis
+
+} // namespace conrad
+
+#endif // #ifndef TABLE_CONST_DATA_SOURCE_H
