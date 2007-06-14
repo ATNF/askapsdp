59bdd05659d78576d06630ec8c882d271688ec94
Maxim Voronkov <maxim.voronkov@csiro.au>
2007-06-14 04:22:35 +0000

    previously forgotten cc file with an empty virtual destructor has been added
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@304 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/trunk/dataaccess/IDataSelector.cc
@@ -0,0 +1,18 @@
+/// @file IDataSelector.cc
+///
+/// IDataSelector: Interface class representing a selection of visibility
+///                data according to some criterion. 
+///
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Max Voronkov <maxim.voronkov@csiro.au>
+///
+
+#include <dataaccess/IDataSelector.h>
+
+using namespace conrad;
+using namespace synthesis;
+
+/// An empty virtual destructor to make the compiler happy
+IDataSelector::~IDataSelector()
+{
+}
