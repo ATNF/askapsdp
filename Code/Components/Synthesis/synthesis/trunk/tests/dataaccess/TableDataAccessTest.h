0585e52eb513ce05dfbceb0dd59eb454d763f1e5
Maxim Voronkov <maxim.voronkov@csiro.au>
2007-07-06 07:49:20 +0000

    with data description table test. We still need to find a way to have a single initialization for the whole fixture
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@476 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/TableDataAccessTest.h
+++ Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/TableDataAccessTest.h
@@ -17,6 +17,7 @@
 
 // std includes
 #include <string>
+#include <vector>
 
 // cppunit includes
 #include <cppunit/extensions/HelperMacros.h>
@@ -32,26 +33,34 @@ namespace synthesis {
 class TableDataAccessTest : public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE(TableDataAccessTest);
-  CPPUNIT_TEST(readOnlyTest);
-  CPPUNIT_TEST(bufferManagerTest);
+  //CPPUNIT_TEST(readOnlyTest);
   CPPUNIT_TEST_EXCEPTION(bufferManagerExceptionTest,casa::TableError);
+  CPPUNIT_TEST(bufferManagerTest);
+  //CPPUNIT_TEST(dataDescTest);  
   CPPUNIT_TEST_SUITE_END();
 public:
+  
+  /// set up the test suite
   void setUp();
+  /// destruct the test suite
   void tearDown();
+  /// test of read only operations of the whole table-based implementation
   void readOnlyTest() {}
+  /// test exception if disk-based buffers are requested for a read-only table  
   void bufferManagerExceptionTest();
-  void bufferManagerTest();  
+  /// extensive test of buffer operations
+  void bufferManagerTest();
+  /// test access to data description subtable
+  void dataDescTest();  
 protected:
   void doBufferTest() const;
 private:
   std::string itsTestMSName;
-  boost::shared_ptr<ITableInfoAccessor> itsTableInfoAccessor;
+  boost::shared_ptr<ITableInfoAccessor> itsTableInfoAccessor;  
 }; // class TableDataAccessTest
 
-// implementation of the TableDataAccessTest class
 void TableDataAccessTest::setUp()
-{
+{  
   itsTestMSName="./.test.ms";
   std::string path2TestMS="../../testdata/trunk/testdataset.ms";
   if (casa::EnvironmentVariable::isDefined("CONRAD_PROJECT_ROOT")) {
@@ -69,7 +78,7 @@ void TableDataAccessTest::setUp()
 }
 
 void TableDataAccessTest::tearDown()
-{
+{ 
   itsTableInfoAccessor.reset();
   casa::Table copiedMS(itsTestMSName,casa::Table::Update);
   copiedMS.markForDelete();  
@@ -95,6 +104,19 @@ void TableDataAccessTest::bufferManagerTest()
                                    casa::Table::Update), false));
   doBufferTest();
 }
+
+/// test access to data description subtable
+void TableDataAccessTest::dataDescTest()
+{
+  CONRADASSERT(itsTableInfoAccessor);
+  const ITableDataDescHolder &dataDescription=itsTableInfoAccessor->
+                    subtableInfo().getDataDescription();
+  CPPUNIT_ASSERT(dataDescription.getSpectralWindowID(0)==0);
+  CPPUNIT_ASSERT(dataDescription.getPolarizationID(0)==0);
+  CPPUNIT_ASSERT(dataDescription.getDescIDsForSpWinID(0).size()==1);
+  CPPUNIT_ASSERT(dataDescription.getDescIDsForSpWinID(1).size()==0);
+}
+
 
 void TableDataAccessTest::doBufferTest() const
 {
