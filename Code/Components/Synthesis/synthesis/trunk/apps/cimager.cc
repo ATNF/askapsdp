3a166a1173b4e2610299c6427bf53b32632866a7
Tim Cornwell <tim.cornwell@csiro.au>
2007-07-14 09:56:33 +0000

    Don't create Solver every cycle
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@527 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/apps/cimager.cc
+++ Code/Components/Synthesis/synthesis/trunk/apps/cimager.cc
@@ -59,7 +59,7 @@ std::ostream& os() {
 // For MPI, we need to divert the output
 void initOutput(int rank)
 {
-// Set the name of the output stream.
+  // Set the name of the output stream.
   std::ostringstream ostr;
   ostr << rank;
   MWIos::setName ("cimager_tmp.cout" + ostr.str());
@@ -71,18 +71,18 @@ MPIConnectionSet::ShPtr initConnections(int nnode, int rank)
   MPIConnectionSet* conns (new MPIConnectionSet());
   MPIConnectionSet::ShPtr cs(conns);
   if(rank==0)
-  {
-// I am the master - I need a connection to every worker
-    for (int i=1; i<nnode; ++i)
     {
-      cs->addConnection (i, 0);
+      // I am the master - I need a connection to every worker
+      for (int i=1; i<nnode; ++i)
+	{
+	  cs->addConnection (i, 0);
+	}
     }
-  }
   else
-  {
-// I am a worker - I only need to talk to the master
-    cs->addConnection (0, 0);
-  }
+    {
+      // I am a worker - I only need to talk to the master
+      cs->addConnection (0, 0);
+    }
   return cs;
 }
 
@@ -108,16 +108,16 @@ void receiveNE(MPIConnectionSet::ShPtr& cs, int nnode, Solver::ShPtr& is)
   NormalEquations ne;
   int rank;
   for (int i=0;i<nnode-1;i++)
-  {
-    cs->read(i, bs);
-    LOFAR::BlobIBufString bib(bs);
-    LOFAR::BlobIStream in(bib);
-    int version=in.getStart("ne");
-    CONRADASSERT(version==1);
-    in >> rank >> ne;
-    in.getEnd();
-    is->addNormalEquations(ne);
-  }
+    {
+      cs->read(i, bs);
+      LOFAR::BlobIBufString bib(bs);
+      LOFAR::BlobIStream in(bib);
+      int version=in.getStart("ne");
+      CONRADASSERT(version==1);
+      in >> rank >> ne;
+      in.getEnd();
+      is->addNormalEquations(ne);
+    }
   return;
 }
 
@@ -133,9 +133,9 @@ void sendModel(MPIConnectionSet::ShPtr& cs, int nnode, const Params& skymodel)
   out << skymodel;
   out.putEnd();
   for (int i=1; i<nnode; ++i)
-  {
-    cs->write(i-1, bs);
-  }
+    {
+      cs->write(i-1, bs);
+    }
 }
 
 
@@ -149,7 +149,7 @@ void receiveModel(MPIConnectionSet::ShPtr& cs, Params& skymodel)
   LOFAR::BlobIStream in(bib);
   int version=in.getStart("model");
   CONRADASSERT(version==1)
-  in >> skymodel;
+    in >> skymodel;
   in.getEnd();
   return;
 }
@@ -158,205 +158,210 @@ void receiveModel(MPIConnectionSet::ShPtr& cs, Params& skymodel)
 int main(int argc, const char** argv)
 {
   try
-  {
+    {
       
-// Initialize MPI (also succeeds if no MPI available).
-    MPIConnection::initMPI (argc, argv);
-    int nnode = MPIConnection::getNrNodes();
-    int rank  = MPIConnection::getRank();
-    MPIConnectionSet::ShPtr cs;
-
-    bool isParallel(nnode>1);
-    bool isMaster(isParallel&&(rank==0));
-
-    initOutput(rank);
-
-    if(isParallel)
-    {
-      cs=initConnections(nnode, rank);
-      if (isMaster)
-      {
-        os() << "CONRAD synthesis imaging program (parallel version) on " << nnode
-          << " nodes (master)" << std::endl;
-      }
+      // Initialize MPI (also succeeds if no MPI available).
+      MPIConnection::initMPI (argc, argv);
+      int nnode = MPIConnection::getNrNodes();
+      int rank  = MPIConnection::getRank();
+      MPIConnectionSet::ShPtr cs;
+      
+      bool isParallel(nnode>1);
+      bool isMaster(isParallel&&(rank==0));
+      
+      initOutput(rank);
+      
+      if(isParallel)
+	{
+	  cs=initConnections(nnode, rank);
+	  if (isMaster)
+	    {
+	      os() << "CONRAD synthesis imaging program (parallel version) on " << nnode
+		   << " nodes (master)" << std::endl;
+	    }
+	  else
+	    {
+	      os() << "CONRAD synthesis imaging program (parallel version) on " << nnode
+		   << " nodes (worker " << rank << ")" << std::endl;
+	    }
+	}
       else
-      {
-        os() << "CONRAD synthesis imaging program (parallel version) on " << nnode
-          << " nodes (worker " << rank << ")" << std::endl;
-      }
-    }
-    else
-    {
-      cout << "CONRAD synthesis imaging program (serial version)" << endl;
-    }
-
-    casa::Timer timer;
-    timer.mark();
-
-    string parsetname("cimager.in");
-    ParameterSet parset(parsetname);
-    ParameterSet subset(parset.makeSubset("Cimager."));
-
-    Params skymodel;
-    
-/// Create the specified images from the definition in the
-/// parameter set
-    SynthesisParamsHelper::add(skymodel, parset, "Images.");
-        
-/// Create the gridder and solver using factories acting on
-/// parametersets
-    IVisGridder::ShPtr gridder=VisGridderFactory::make(subset);
-
-    NormalEquations ne(skymodel);
-
-// Now do the required number of major cycles
-    int nCycles(parset.getInt32("Cimager.solver.cycles", 1));
-    for (int cycle=0;cycle<nCycles;cycle++)
-    {
-
+	{
+	  cout << "CONRAD synthesis imaging program (serial version)" << endl;
+	}
+      
+      casa::Timer timer;
+      timer.mark();
+      
+      string parsetname("cimager.in");
+      ParameterSet parset(parsetname);
+      ParameterSet subset(parset.makeSubset("Cimager."));
+      
+      Params skymodel;
+      
+      /// Create the specified images from the definition in the
+      /// parameter set
+      SynthesisParamsHelper::add(skymodel, parset, "Images.");
+      
+      /// Create the gridder and solver using factories acting on
+      /// parametersets
+      IVisGridder::ShPtr gridder=VisGridderFactory::make(subset);
       Solver::ShPtr solver=ImageSolverFactory::make(skymodel, subset);
-
-      if(nCycles>1)
-      {
-        os() << "*** Starting major cycle " << cycle << " ***" << std::endl;
-      }
-
-/// Now iterate through all data sets
-/// If running in parallel, the master doesn't do this.
-      if(!isParallel||!isMaster) {
-      int slot=1;
-      vector<string> ms=parset.getStringVector("DataSet");
-      for (vector<string>::iterator thisms=ms.begin();thisms!=ms.end();++thisms)
-      {
-        if(!isParallel||(rank==slot))
-        {
-            os() << "Processing data set " << *thisms << std::endl;
-            TableDataSource ds(*thisms);
-            IDataSelectorPtr sel=ds.createSelector();
-            IDataConverterPtr conv=ds.createConverter();
-            conv->setFrequencyFrame(casa::MFrequency::Ref(casa::MFrequency::TOPO),"Hz");
-            IDataSharedIter it=ds.createIterator(sel, conv);
-            it.init();
-            it.chooseOriginal();
-            if((cycle>0)&&(isParallel))
-            {
-              receiveModel(cs, skymodel);
-              os() << "Received model from master" << std::endl;
-            }
-            ImageFFTEquation ie(skymodel, it, gridder);
-            os() << "Constructed measurement equation" << std::endl;
-  
-            ie.calcEquations(ne);
-            os() << "Calculated normal equations" << std::endl;
-            if(isParallel)
-            {
-              sendNE(cs, rank, ne);
-              os() << "Sent normal equations to the solver via MPI" << std::endl;
-            }
-            else
-            {
-              solver->addNormalEquations(ne);
-              os() << "Added normal equations to solver" << std::endl;
-            }
-          }
-          slot++;
-          os() << "user:   " << timer.user () << " system: " << timer.system () 
-            <<" real:   " << timer.real () << std::endl;      }
-      }
-
-// Now do the solution
-// If running in parallel only the master does this
+      
+      
+      NormalEquations ne(skymodel);
+      
+      // Now do the required number of major cycles
+      int nCycles(parset.getInt32("Cimager.solver.cycles", 1));
+      for (int cycle=0;cycle<nCycles;cycle++)
+	{
+	  
+	  // Need to reset the solver so that the normal equations are zero'ed
+	  solver->init();
+	  
+	  if(nCycles>1)
+	    {
+	      os() << "*** Starting major cycle " << cycle << " ***" << std::endl;
+	    }
+	  
+	  /// Now iterate through all data sets
+	  /// If running in parallel, the master doesn't do this.
+	  if(!isParallel||!isMaster) {
+	    int slot=1;
+	    vector<string> ms=parset.getStringVector("DataSet");
+	    for (vector<string>::iterator thisms=ms.begin();thisms!=ms.end();++thisms)
+	      {
+		if(!isParallel||(rank==slot))
+		  {
+		    os() << "Processing data set " << *thisms << std::endl;
+		    TableDataSource ds(*thisms);
+		    IDataSelectorPtr sel=ds.createSelector();
+		    IDataConverterPtr conv=ds.createConverter();
+		    conv->setFrequencyFrame(casa::MFrequency::Ref(casa::MFrequency::TOPO),"Hz");
+		    IDataSharedIter it=ds.createIterator(sel, conv);
+		    it.init();
+		    it.chooseOriginal();
+		    if((cycle>0)&&(isParallel))
+		      {
+			receiveModel(cs, skymodel);
+			os() << "Received model from master" << std::endl;
+		      }
+		    ImageFFTEquation ie(skymodel, it, gridder);
+		    os() << "Constructed measurement equation" << std::endl;
+		    
+		    ie.calcEquations(ne);
+		    os() << "Calculated normal equations" << std::endl;
+		    if(isParallel)
+		      {
+			sendNE(cs, rank, ne);
+			os() << "Sent normal equations to the solver via MPI" << std::endl;
+		      }
+		    else
+		      {
+			solver->addNormalEquations(ne);
+			os() << "Added normal equations to solver" << std::endl;
+		      }
+		    os() << "user:   " << timer.user () << " system: " << timer.system () 
+			 <<" real:   " << timer.real () << std::endl;
+		  }
+		slot++;
+	      }
+	  }
+	    
+	  // Now do the solution
+	  // If running in parallel only the master does this
+	  if(!isParallel||isMaster)
+	    {
+	      // Could be that we are waiting for normal equations
+	      if (isParallel)
+		{
+		  os() << "Waiting for normal equations" << std::endl;
+		  receiveNE(cs, nnode, solver);
+		  os() << "Received all normal equations" << std::endl;
+		}
+	      if(cycle<(nCycles-1)) {
+		os() << "Solving normal equations" << std::endl;
+		Quality q;
+		solver->solveNormalEquations(q);
+		os() << "Solved normal equations" << std::endl;
+		skymodel=solver->parameters();
+		// Don't send the model where there are no workers around
+		if((nCycles>1)&&(isParallel))
+		  {
+		    sendModel(cs, nnode, skymodel);
+		    os() << "Sent model to all workers" << std::endl;
+		  }
+	      }
+	      else {
+		os() << "Writing out result as an image" << std::endl;
+		vector<string> resultimages=skymodel.names();
+		for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
+		  {
+		    SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it);
+		  }
+		if(parset.getBool("Cimager.restore", true)) {
+		  vector<string> beam=parset.getStringVector("Cimager.restore.beam");
+		  casa::Vector<casa::Quantum<double> > qbeam(3);
+		  for (int i=0;i<3;i++) {
+		    casa::Quantity::read(qbeam(i), beam[i]);
+		  }
+		  os() << "Last cycle - restoring model" << std::endl;
+		  // Make an image restore solver from the current solver
+		  // so it can use the normal equations
+		  // And write the images to CASA image files before and after restoring
+		  vector<string> resultimages=skymodel.names();
+		  for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
+		    {
+		      SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it);
+		    }
+		  ImageRestoreSolver ir(skymodel, qbeam);
+		  ir.copyNormalEquations(*solver);
+		  Quality q;
+		  ir.solveNormalEquations(q);
+		  for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
+		    {
+		      SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it+string(".restored"));
+		    }
+		}
+	      }
+	      vector<string> resultimages=skymodel.names();
+	      for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
+		{
+		  casa::Array<double> resultImage(skymodel.value(*it));
+		  os() << *it << std::endl
+		       << "Maximum = " << max(resultImage) << ", minimum = " 
+		       << min(resultImage) << std::endl;
+		}
+	      
+	      os() << "user:   " << timer.user () << " system: " << timer.system () 
+		   <<" real:   " << timer.real () << std::endl;      }
+	}
+      
+      // The solution is complete - now we need to write out the results
       if(!isParallel||isMaster)
-      {
-// Could be that we are waiting for normal equations
-        if (isParallel)
-        {
-          os() << "Waiting for normal equations" << std::endl;
-          receiveNE(cs, nnode, solver);
-          os() << "Received all normal equations" << std::endl;
-        }
-        if(cycle<(nCycles-1)) {
-          os() << "Solving normal equations" << std::endl;
-          Quality q;
-          solver->solveNormalEquations(q);
-          os() << "Solved normal equations" << std::endl;
-          skymodel=solver->parameters();
-          // Don't send the model where there are no workers around
-          if((nCycles>1)&&(isParallel))
-          {
-            sendModel(cs, nnode, skymodel);
-            os() << "Sent model to all workers" << std::endl;
-          }
-        }
-        else {
-          os() << "Writing out result as an image" << std::endl;
-          vector<string> resultimages=skymodel.names();
-          for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
-          {
-            SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it);
-          }
-          if(parset.getBool("Cimager.restore", true)) {
-            vector<string> beam=parset.getStringVector("Cimager.restore.beam");
-            casa::Vector<casa::Quantum<double> > qbeam(3);
-            for (int i=0;i<3;i++) {
-              casa::Quantity::read(qbeam(i), beam[i]);
-            }
-            os() << "Last cycle - restoring model" << std::endl;
-            // Make an image restore solver from the current solver
-            // so it can use the normal equations
-            // And write the images to CASA image files before and after restoring
-            vector<string> resultimages=skymodel.names();
-            for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
-            {
-              SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it);
-            }
-            ImageRestoreSolver ir(skymodel, qbeam);
-            ir.copyNormalEquations(*solver);
-            Quality q;
-            ir.solveNormalEquations(q);
-            for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
-            {
-              SynthesisParamsHelper::saveAsCasaImage(skymodel, *it, *it+string(".restored"));
-            }
-          }
-        }
-        vector<string> resultimages=skymodel.names();
-        for (vector<string>::iterator it=resultimages.begin();it!=resultimages.end();it++)
-        {
-          casa::Array<double> resultImage(skymodel.value(*it));
-          os() << *it << std::endl
-            << "Maximum = " << max(resultImage) << ", minimum = " 
-            << min(resultImage) << std::endl;
-        }
-
-        os() << "user:   " << timer.user () << " system: " << timer.system () 
-          <<" real:   " << timer.real () << std::endl;      }
+	{
+	  // Now write the results to a table
+	  string resultfile(parset.getString("Parms.Result"));
+	  ParamsCasaTable results(resultfile, false);
+	  results.setParameters(skymodel);
+	}
+      os() << "Finished imaging" << std::endl;
+      if(isParallel) {
+	os() << "Ending MPI for rank " << rank << std::endl;
+	MPIConnection::endMPI();
+      }
+      
+      exit(0);
     }
-
-// The solution is complete - now we need to write out the results
-    if(!isParallel||isMaster)
-    {
-// Now write the results to a table
-      string resultfile(parset.getString("Parms.Result"));
-      ParamsCasaTable results(resultfile, false);
-      results.setParameters(skymodel);
-    }
-    os() << "Finished imaging" << std::endl;
-    if(isParallel) {
-      os() << "Ending MPI for rank " << rank << std::endl;
-      MPIConnection::endMPI();
-    }
-
-    exit(0);
-  }
   catch (conrad::ConradError& x)
-  {
-    std::cerr << "Conrad error in " << argv[0] << ": " << x.what() << std::endl;
-    exit(1);
-  }
+    {
+      std::cerr << "Conrad error in " << argv[0] << ": " << x.what() << std::endl;
+      exit(1);
+    }
   catch (std::exception& x)
-  {
-    std::cerr << "Unexpected exception in " << argv[0] << ": " << x.what() << std::endl;
-    exit(1);
-  }
+    {
+      std::cerr << "Unexpected exception in " << argv[0] << ": " << x.what() << std::endl;
+      exit(1);
+    }
 };
+
