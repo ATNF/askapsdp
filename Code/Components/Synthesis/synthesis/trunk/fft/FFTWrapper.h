adfd887685ac8ab91fd71408f19bd3d0d6ecb952
Rudolph van der Merwe <rudolph@ska.ac.za>
2007-08-21 04:19:31 +0000

    Added sub-directory for fftw wrapping code. This should probably go somewhere else.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@635 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/trunk/fft/FFTWrapper.h
@@ -0,0 +1,85 @@
+/// @file
+///
+/// FFTWrapper: This class provides wrapper (adapter) functionality for the FFTW library
+///
+/// (c) 2007 CONRAD, All Rights Reserved.
+/// @author Rudolph van der Merwe <rudolph@ska.ac.za>
+///
+
+#ifndef FFTWRAPPER_H_
+#define FFTWRAPPER_H_
+
+#include <complex>
+#include <fftw3.h>
+#include <casa/Arrays/Vector.h>
+#include <casa/Arrays/Matrix.h>
+
+using namespace casa;
+
+namespace conrad
+{
+    namespace fftw
+    {
+        // template <typename T>
+        // struct complex_trait {
+        //     typedef fftw_complex* complexPtr;
+        // };
+        // template <>
+        // struct complex_trait<DComplex> {
+        //     typedef fftw_complex* complexPtr;
+        // };
+        // template <>
+        // struct complex_trait<Complex> {
+        //     typedef fftwf_complex* complexPtr;
+        // };
+
+        template <typename T>
+        void fft(Vector<T>& vec, const Bool forward=True);
+        
+        template <>
+        void fft(Vector<DComplex>& vec, const Bool forward)
+        {
+            const IPosition shape = vec.shape();
+            const uInt nElements = shape[0];
+            Bool deleteIt;
+            DComplex *dataPtr = vec.getStorage(deleteIt);
+            DComplex *tempPtr = dataPtr;
+            fftw_plan p;
+            p = fftw_plan_dft_1d(nElements, reinterpret_cast<fftw_complex*>(dataPtr), reinterpret_cast<fftw_complex*>(dataPtr), 
+                                 (forward) ? FFTW_FORWARD : FFTW_BACKWARD, FFTW_ESTIMATE);                           
+            fftw_execute(p); 
+            if (!forward) {
+                const DComplex scale = DComplex(1)/DComplex(nElements);
+                for(int i=0; i<nElements; i++){
+                    *tempPtr++ *= scale;
+                }
+            }
+            vec.putStorage(dataPtr, deleteIt);    
+            fftw_destroy_plan(p);
+        }
+        
+        template <>
+        void fft(Vector<Complex>& vec, const Bool forward)
+        {
+            const IPosition shape = vec.shape();
+            const uInt nElements = shape[0];
+            Bool deleteIt;
+            Complex *dataPtr = vec.getStorage(deleteIt);
+            Complex *tempPtr = dataPtr;
+            fftwf_plan p;
+            p = fftwf_plan_dft_1d(nElements, reinterpret_cast<fftwf_complex*>(dataPtr), reinterpret_cast<fftwf_complex*>(dataPtr), 
+                                 (forward) ? FFTW_FORWARD : FFTW_BACKWARD, FFTW_ESTIMATE);                           
+            fftwf_execute(p); 
+            if (!forward) {
+                const Complex scale = Complex(1)/Complex(nElements);
+                for(int i=0; i<nElements; i++){
+                    *tempPtr++ *= scale;
+                }
+            }
+            vec.putStorage(dataPtr, deleteIt);    
+            fftwf_destroy_plan(p);
+        }        
+        
+    }
+}
+#endif
