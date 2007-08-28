47865a300ddb308aa3251b766c9d3d169156e0cb
Malte Marquarding <malte.marquarding@csiro.au>
2007-08-28 12:45:51 +0000

    work-around as doxygen can't document using ::xyz imports
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@668 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/conrad/Cc/trunk/conrad/ConradTypes.h
+++ Code/Base/conrad/Cc/trunk/conrad/ConradTypes.h
@@ -11,53 +11,33 @@
 
 namespace conrad {
   // Use sys/types.h where available
-
-  /// @typedef ushort
-  /// typedef for sized type  
+  /// @name Sized types
+  /// typedefs and imports for sized types
+  ///
+  /// \b ulong
+  ///
+  /// \b uint
+  ///
+  /// \b ushort
+  //@{
   using ::ushort;
-  /// @typedef uint
-  /// typedef for sized type
   using ::uint;
   // Apple doesn't define uLong
-  /// @typedef ulong
-  /// typedef for sized type
 #if defined(__APPLE__)
   typedef unsigned long        ulong;
 #else
   using ::ulong;
 #endif
-  /// @typedef longlong
-  /// typedef for sized type
   typedef long long            longlong;
-  /// @typedef ulonglong
-  /// typedef for sized type
   typedef unsigned long long   ulonglong;
-  /// @typedef ldouble
-  /// typedef for sized type
   typedef long double          ldouble;
-  /// @typedef int8
-  /// typedef for sized type
   typedef char                 int8;
-  /// @typedef uint8
-  /// typedef for sized type
   typedef unsigned char        uint8;
-  /// @typedef int16
-  /// typedef for sized type
   typedef short                int16;
-  /// @typedef int32
-  /// typedef for sized type
   typedef int                  int32;
-  /// @typedef int64
-  /// typedef for sized type
   typedef long long            int64;
-  /// @typedef uint16
-  /// typedef for sized type
   typedef unsigned short       uint16;
-  /// @typedef uint32
-  /// typedef for sized type
   typedef unsigned int         uint32;
-  /// @typedef uint64
-  /// typedef for sized type
   typedef unsigned long long   uint64;
   //@}
 
