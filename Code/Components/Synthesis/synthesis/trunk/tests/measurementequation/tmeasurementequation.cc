570ad12e9a955f7cad74e49316b9eeaf767d2773
Maxim Voronkov <maxim.voronkov@csiro.au>
2007-09-13 16:34:59 +0000

    uncommented all measurement equation tests
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@712 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/tests/measurementequation/tmeasurementequation.cc
+++ Code/Components/Synthesis/synthesis/trunk/tests/measurementequation/tmeasurementequation.cc
@@ -10,8 +10,8 @@ int main( int argc, char **argv)
   CppUnit::TextUi::TestRunner runner;
   runner.addTest( conrad::synthesis::VectorOperationsTest::suite() );
   runner.addTest( conrad::synthesis::ComponentEquationTest::suite() );
-  //runner.addTest( conrad::synthesis::ImageDFTEquationTest::suite() );
-//  runner.addTest( conrad::synthesis::ImageFFTEquationTest::suite() );
+  runner.addTest( conrad::synthesis::ImageDFTEquationTest::suite() );
+  runner.addTest( conrad::synthesis::ImageFFTEquationTest::suite() );
   runner.run();
   return 0;
 }
