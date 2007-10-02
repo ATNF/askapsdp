e077455efa800cb35538e59cb3c834d564e86c95
Tim Cornwell <tim.cornwell@csiro.au>
2007-10-02 18:26:25 +0000

    Forgot to commit WStackVisGridder
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@733 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/trunk/gridding/WStackVisGridder.cc
@@ -0,0 +1,206 @@
+#include <gridding/WStackVisGridder.h>
+
+#include <conrad/ConradError.h>
+#include <conrad/ConradUtil.h>
+
+#include <casa/Arrays/Array.h>
+#include <casa/Arrays/ArrayMath.h>
+
+#include <casa/BasicSL/Constants.h>
+#include <fft/FFTWrapper.h>
+
+using namespace conrad;
+
+#include <cmath>
+
+namespace conrad
+{
+	namespace synthesis
+	{
+
+		WStackVisGridder::WStackVisGridder(const double wmax, const int nwplanes)
+		{
+			CONRADCHECK(wmax>0.0, "Baseline length must be greater than zero");
+			CONRADCHECK(nwplanes>0, "Number of w planes must be greater than zero");
+
+			itsNWPlanes=2*nwplanes+1;
+			itsWScale=wmax/double(nwplanes);
+		}
+
+		WStackVisGridder::~WStackVisGridder()
+		{
+		}
+
+		/// Clone a copy of this Gridder
+		IVisGridder::ShPtr WStackVisGridder::clone()
+		{
+			return IVisGridder::ShPtr(new WStackVisGridder(*this));
+		}
+
+		/// Initialize the convolution function into the cube. If necessary this
+		/// could be optimized by using symmetries.
+		void WStackVisGridder::initIndices(IDataSharedIter& idi)
+		{
+			/// We have to calculate the lookup function converting from
+			/// row and channel to plane of the w-dependent convolution
+			/// function
+			const int nSamples = idi->uvw().size();
+			const int nChan = idi->frequency().size();
+			const int nPol = idi->visibility().shape()(2);
+
+			itsGMap.resize(nSamples, nPol, nChan);
+			int cenw=(itsNWPlanes-1)/2;
+			for (int i=0; i<nSamples; i++)
+			{
+				double w=(idi->uvw()(i)(2))/(casa::C::c);
+				for (int chan=0; chan<nChan; chan++)
+				{
+					for (int pol=0; pol<nPol; pol++)
+					{
+						double freq=idi->frequency()[chan];
+						/// Calculate the index into the grids
+						itsGMap(i, pol, chan)=cenw+nint(w*freq/itsWScale);
+						if (itsGMap(i, pol, chan)<0)
+						{
+							std::cout << w << " "<< freq << " "<< itsWScale << " "<< itsGMap(
+							    i, pol, chan) << std::endl;
+						}
+						CONRADCHECK(itsGMap(i,pol,chan)<itsNWPlanes, "W scaling error: recommend allowing larger range of w");
+						CONRADCHECK(itsGMap(i,pol,chan)>-1, "W scaling error: recommend allowing larger range of w");
+					}
+				}
+			}
+		}
+
+		void WStackVisGridder::multiply(casa::Array<casa::Complex>& scratch, int i)
+		{
+			/// These are the actual cell sizes used
+			float cellx=1.0/(float(itsShape(0))*itsUVCellSize(0));
+			float celly=1.0/(float(itsShape(1))*itsUVCellSize(1));
+
+			int nx=itsShape(0);
+			int ny=itsShape(1);
+
+			int cenw=(itsNWPlanes-1)/2;
+
+			float w=2.0f*casa::C::pi*float(i-cenw)*itsWScale;
+
+			casa::ArrayIterator<casa::Complex> it(scratch, 2);
+			while (!it.pastEnd())
+			{
+				casa::Matrix<casa::Complex> mat(it.array());
+
+				for (int iy=0; iy<ny; iy++)
+				{
+					float y2=float(iy-ny/2)*celly;
+					y2*=y2;
+					for (int ix=0; ix<nx; ix++)
+					{
+						float x2=float(ix-nx/2)*cellx;
+						x2*=x2;
+						float r2=x2+y2;
+						float phase=w*(1.0-sqrt(1.0-r2));
+						mat(ix, iy)*=casa::Complex(cos(phase), -sin(phase));
+					}
+				}
+				it.next();
+			}
+		}
+
+		/// This is the default implementation
+		void WStackVisGridder::finaliseGrid(casa::Array<double>& out)
+		{
+			
+			std::cout << "Stacking W planes" << std::endl;
+
+			/// Loop over all grids Fourier transforming and accumulating
+			for (int i=0; i<itsGrid.size(); i++)
+			{
+				casa::Array<casa::Complex> scratch(itsGrid[i].copy());
+				fft2d(scratch, false);
+				multiply(scratch, i);
+
+				if (i==0)
+				{
+					toDouble(out, scratch);
+				}
+				else
+				{
+					casa::Array<double> work(out.shape());
+					toDouble(work, scratch);
+					out+=work;
+				}
+			}
+			// Now we can do the convolution correction
+			correctConvolution(out);
+			out*=double(out.shape()(0))*double(out.shape()(1));
+		}
+
+		/// This is the default implementation
+		void WStackVisGridder::finalisePSF(casa::Array<double>& out)
+		{
+
+			/// Loop over all grids Fourier transforming and accumulating
+			for (int i=0; i<itsGrid.size(); i++)
+			{
+				casa::Array<casa::Complex> scratch(itsGridPSF[i].copy());
+				fft2d(scratch, false);
+				multiply(scratch, i);
+
+				if (i==0)
+				{
+					toDouble(out, scratch);
+				}
+				else
+				{
+					casa::Array<double> work(out.shape());
+					toDouble(work, scratch);
+					out+=work;
+				}
+			}
+			// Now we can do the convolution correction
+			correctConvolution(out);
+			out*=double(out.shape()(0))*double(out.shape()(1));
+		}
+
+		void WStackVisGridder::initialiseDegrid(const scimath::Axes& axes,
+		    const casa::Array<double>& in)
+		{
+
+			itsAxes=axes;
+			itsShape=in.shape();
+
+			CONRADCHECK(itsAxes.has("RA")&&itsAxes.has("DEC"), "RA and DEC specification not present in axes");
+
+			double raStart=itsAxes.start("RA");
+			double raEnd=itsAxes.end("RA");
+
+			double decStart=itsAxes.start("DEC");
+			double decEnd=itsAxes.end("DEC");
+
+			itsUVCellSize.resize(2);
+			itsUVCellSize(0)=1.0/(raEnd-raStart);
+			itsUVCellSize(1)=1.0/(decEnd-decStart);
+
+			std::cout << "Filling W stack with image" << std::endl;
+
+			itsGrid.resize(itsNWPlanes);
+			for (int i=0; i<itsNWPlanes; i++)
+			{
+				itsGrid[i].resize(itsShape);
+
+				casa::Array<double> scratch(in.copy());
+				correctConvolution(scratch);
+				toComplex(itsGrid[i], scratch);
+				multiply(itsGrid[i], i);
+				fft2d(itsGrid[i], true);
+			}
+		}
+
+		int WStackVisGridder::gIndex(int row, int pol, int chan)
+		{
+			return itsGMap(row, pol, chan);
+		}
+
+	}
+}
---
+++ Code/Components/Synthesis/synthesis/trunk/gridding/WStackVisGridder.h
@@ -0,0 +1,72 @@
+/// @file
+///
+/// WStackVisGridder: W projection gridding
+
+/// (c) 2007 CONRAD, All Rights Reserved.
+/// @author Tim Cornwell <tim.cornwell@csiro.au>
+///
+#ifndef CONRAD_SYNTHESIS_WSTACKVISGRIDDER_H_
+#define CONRAD_SYNTHESIS_WSTACKVISGRIDDER_H_
+
+#include <gridding/SphFuncVisGridder.h>
+
+namespace conrad
+{
+	namespace synthesis
+	{
+		/// @brief Visibility gridder using W projection
+		/// @ingroup gridding
+		class WStackVisGridder : public SphFuncVisGridder
+		{
+			public:
+
+				/// @brief Construct a gridder for W stacking
+				/// @param wmax Maximum baseline (wavelengths)
+				/// @param nwplanes Number of w planes
+				WStackVisGridder(const double wmax, const int nwplanes);
+
+				virtual ~WStackVisGridder();
+
+				/// Form the final output image
+				/// @param out Output double precision image
+				virtual void finaliseGrid(casa::Array<double>& out);
+
+				/// Form the final output image
+				/// @param out Output double precision PSF
+				virtual void finalisePSF(casa::Array<double>& out);
+
+				/// @brief Initialise the degridding
+				/// @param axes axes specifications
+				/// @param image Input image: cube: u,v,pol,chan
+				virtual void initialiseDegrid(const scimath::Axes& axes,
+				    const casa::Array<double>& image);
+
+				/// Clone a copy of this Gridder
+				virtual IVisGridder::ShPtr clone();
+
+			protected:
+				/// @brief Initialise the indices
+				virtual void initIndices(IDataSharedIter& idi);
+
+				/// Offset into grid
+				/// @param row Row number
+				/// @param row Polarisation
+				/// @param chan Channel number
+				virtual int gIndex(int row, int pol, int chan);
+
+				/// Multiply by the phase screen
+				/// @param scratch To be multiplied
+				/// @param i Index
+				void multiply(casa::Array<casa::Complex>& scratch, int i);
+				
+				/// Scaling
+				double itsWScale;
+				/// Number of w planes
+				int itsNWPlanes;
+
+				/// Mapping from row, pol, and channel to planes of grid
+				casa::Cube<int> itsGMap;
+		};
+	}
+}
+#endif
