1e64f7ef89038ec829fc4bfdc90823fb3c52b251
Tim Cornwell <tim.cornwell@csiro.au>
2007-11-02 12:49:08 +0000

    Extract model from correct place
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@821 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
+++ Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-echo "Extracting model"
+echo "Running standard simulation and imaging for CONRAD"
 
-echo tar zxvf ../models/10uJy.model.small.tgz
+echo "Extracting 10uJy model"
 
-export LD_LIBRARY_PATH=.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib:$LD_LIBRARY_PATH
+tar zxvf ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz
 
-export DYLD_LIBRARY_PATH=.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib
 
-echo "Running csimulator"
-echo time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator -inputs stdtest.in
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib
 
-echo "Running cimager"
-echo time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager -inputs stdtest.in
+export AIPSPATH=${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk
+
+echo "Running csimulator to create MeasurementSet"
+time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator -inputs stdtest.in
+
+echo "Running cimager to form Cleaned mosaic"
+time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager -inputs stdtest.in
