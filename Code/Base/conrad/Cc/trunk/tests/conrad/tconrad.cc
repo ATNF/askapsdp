e18eb773a416c68ac7b99e20305127d7d4615b6d
Malte Marquarding <malte.marquarding@csiro.au>
2007-11-06 15:36:25 +0000

    mobed app for ConradError tests to unit test
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@828 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/conrad/Cc/trunk/tests/conrad/tconrad.cc
+++ Code/Base/conrad/Cc/trunk/tests/conrad/tconrad.cc
@@ -1,11 +1,13 @@
 #include <cppunit/ui/text/TestRunner.h>
 
 #include <IndexedLessTest.h>
+#include <ConradErrorTest.h>
 
 int main( int argc, char **argv)
 {
   CppUnit::TextUi::TestRunner runner;
   runner.addTest( conrad::utility::IndexedLessTest::suite() );
+  runner.addTest( conrad::ConradErrorTest::suite() );
   runner.run();
   return 0;
 }
