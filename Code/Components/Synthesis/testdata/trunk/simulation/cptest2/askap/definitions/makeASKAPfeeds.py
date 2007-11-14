9fd4504e3e7be2369a969230f8b363753465c64d
Tim Cornwell <tim.cornwell@csiro.au>
2007-11-14 12:55:06 +0000

    Update algorithm in makeASKAPfeeds
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@848 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/cptest2/askap/definitions/makeASKAPfeeds.py
+++ Code/Components/Synthesis/testdata/trunk/simulation/cptest2/askap/definitions/makeASKAPfeeds.py
@@ -10,19 +10,21 @@ feeds="%s, feed%d]"%(feeds,4*nfeeds-1)
 print "feeds.names=%s"%feeds
 
 feedid=0
-for cenx in [3,4]:
-    for ceny in [3,4]:
+for cenx in [-0.5,+0.5]:
+    for ceny in [-0.5,+0.5]:
     
         for feed in range(nfeeds):
             if feed<4:
-                x=feed
+                x=feed+1
                 y=0
             elif feed<28:
                 x=(feed+2)%6
                 y=(feed+2-x)/6
             else:
-                x=feed-28
+                x=feed-28+1
                 y=5
-            print "feeds.feeds%d=[%f,%f]"%(feedid,2*x-cenx,2*y-ceny)
+            fx=2*x+cenx-5.0
+            fy=2*y+ceny-5.0
             feedid=feedid+1
-    
+            print "feeds.feed%d=[%f,%f]"%(feedid,fx,fy)
+
