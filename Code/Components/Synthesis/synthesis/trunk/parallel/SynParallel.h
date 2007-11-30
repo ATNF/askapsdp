19baa001a5bf20e2cb1676befd0f5582970c4e28
Tim Cornwell <tim.cornwell@csiro.au>
2007-11-30 15:55:18 +0000

    Fix format only
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@906 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/parallel/SynParallel.h
+++ Code/Components/Synthesis/synthesis/trunk/parallel/SynParallel.h
@@ -17,90 +17,90 @@
 
 namespace conrad
 {
-	namespace synthesis
-	{
-		/// @brief Support for parallel algorithms 
-		///
-		/// @details Support for parallel applications in the area.
-		/// An application is derived from this abstract base. The model used is that the
-		/// application has many workers and one master, running in separate MPI processes
-		/// or in one single thread. The master is the master so the number of processes
-		/// is one more than the number of workers. 
-		///
-		/// If the number of nodes is 1 then everything occurs in the same process with
-		/// no overall for transmission of model.
-		///
-		/// @ingroup parallel
-		class SynParallel
-		{
-			public:
+  namespace synthesis
+  {
+    /// @brief Support for parallel algorithms 
+    ///
+    /// @details Support for parallel applications in the area.
+    /// An application is derived from this abstract base. The model used is that the
+    /// application has many workers and one master, running in separate MPI processes
+    /// or in one single thread. The master is the master so the number of processes
+    /// is one more than the number of workers. 
+    ///
+    /// If the number of nodes is 1 then everything occurs in the same process with
+    /// no overall for transmission of model.
+    ///
+    /// @ingroup parallel
+    class SynParallel
+    {
+  public:
 
-				/// @brief Constructor 
-				/// @details The command line inputs are needed solely for MPI - currently no
-				/// application specific information is passed on the command line.
-				/// @param argc Number of command line inputs
-				/// @param argv Command line inputs
-				SynParallel(int argc, const char** argv);
+      /// @brief Constructor 
+      /// @details The command line inputs are needed solely for MPI - currently no
+      /// application specific information is passed on the command line.
+      /// @param argc Number of command line inputs
+      /// @param argv Command line inputs
+      SynParallel(int argc, const char** argv);
 
-				~SynParallel();
+      ~SynParallel();
 
-				/// @brief Return an output stream suitable for use in parallel environment
-				/// @details Sending messages to std::cout can be error prone in a parallel
-				/// environment. Hence one should write to this stream, which is currently
-				/// connected to a file tagged with the rank number. Eventually the conrad
-				/// logging system will be used.
-				std::ostream& os();
+      /// @brief Return an output stream suitable for use in parallel environment
+      /// @details Sending messages to std::cout can be error prone in a parallel
+      /// environment. Hence one should write to this stream, which is currently
+      /// connected to a file tagged with the rank number. Eventually the conrad
+      /// logging system will be used.
+      std::ostream& os();
 
-				/// Is this running in parallel?
-				bool isParallel();
+      /// Is this running in parallel?
+      bool isParallel();
 
-				/// Is this the master?
-				bool isMaster();
+      /// Is this the master?
+      bool isMaster();
 
-				/// Is this a worker?
-				bool isWorker();
+      /// Is this a worker?
+      bool isWorker();
 
-				/// Return the model
-				conrad::scimath::Params::ShPtr& params();
+      /// Return the model
+      conrad::scimath::Params::ShPtr& params();
 
-				/// @brief Broadcast the model to all workers
-				void broadcastModel();
+      /// @brief Broadcast the model to all workers
+      void broadcastModel();
 
-				/// @brief Receive the model from the master
-				void receiveModel();
+      /// @brief Receive the model from the master
+      void receiveModel();
 
-			protected:
-				/// Initialize the MPI connections
-				void initConnections();
+  protected:
+      /// Initialize the MPI connections
+      void initConnections();
 
-				/// The set of all connections between processes. For the master, there
-				/// are connections to every worker, but each worker has only one
-				/// connection, which is to the master.
-				conrad::cp::MPIConnectionSet::ShPtr itsConnectionSet;
+      /// The set of all connections between processes. For the master, there
+      /// are connections to every worker, but each worker has only one
+      /// connection, which is to the master.
+      conrad::cp::MPIConnectionSet::ShPtr itsConnectionSet;
 
-				/// The model
-				conrad::scimath::Params::ShPtr itsModel;
+      /// The model
+      conrad::scimath::Params::ShPtr itsModel;
 
-				/// Rank of this process : 0 for the master, >0 for workers
-				int itsRank;
+      /// Rank of this process : 0 for the master, >0 for workers
+      int itsRank;
 
-				/// Number of nodes
-				int itsNNode;
+      /// Number of nodes
+      int itsNNode;
 
-				/// Is this parallel? itsNNode > 1?
-				bool itsIsParallel;
+      /// Is this parallel? itsNNode > 1?
+      bool itsIsParallel;
 
-				/// Is this the Master?
-				bool itsIsMaster;
+      /// Is this the Master?
+      bool itsIsMaster;
 
-				/// Is this a worker?
-				bool itsIsWorker;
-				
-				/// Substitute %w by worker number. This allows workers to do different work!
-				std::string substituteWorkerNumber(const std::string& s);
+      /// Is this a worker?
+      bool itsIsWorker;
 
-		};
+      /// Substitute %w by worker number. This allows workers to do different work!
+      std::string substituteWorkerNumber(const std::string& s);
 
-	}
+    };
+
+  }
 }
 #endif
