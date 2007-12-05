8ac6d904679d3d6921a910fba3d2aa980a487e72
Tim Cornwell <tim.cornwell@csiro.au>
2007-12-05 10:16:33 +0000

    Convert to use Malte's scripts
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@919 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
+++ Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
@@ -6,14 +6,10 @@ echo "Extracting 10uJy model"
 
 tar zxvf ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib
-
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:.:${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/lib:${CONRAD_PROJECT_ROOT}/Code/Base/scimath/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/gsl/default/lib:${CONRAD_PROJECT_ROOT}/Code/Components/CP/mwcommon/trunk/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Blob/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/casacore/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/cfitsio/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/lapack/default/.:${CONRAD_PROJECT_ROOT}/3rdParty/blas/default:${CONRAD_PROJECT_ROOT}/3rdParty/wcslib/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/APS/default/lib:${CONRAD_PROJECT_ROOT}/3rdParty/LOFAR/Common/tags/default:/usr/lib:${CONRAD_PROJECT_ROOT}/Code/Base/conrad/Cc/trunk/lib
-
 export AIPSPATH=${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk
 
 echo "Running csimulator to create MeasurementSet for a single pointing"
-time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator -inputs stdtest.in
+time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in
 
 echo "Running cimager to form Clean image of single pointing"
-time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager -inputs stdtest.in
+time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in
