936f11e48bca1bab7a98b22e2073a3b57817f18e
Tim Cornwell <tim.cornwell@csiro.au>
2007-12-06 12:16:17 +0000

    Moved parallel stuff out of synthesis so that duchamp can use it.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@921 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/CP/parallel/trunk/apps/cduchamp.cc
@@ -0,0 +1,82 @@
+///
+/// @file : Duchamp driver
+///
+///
+/// Control parameters are passed in from a LOFAR ParameterSet file.
+///
+/// (c) 2007 CONRAD, All Rights Reserved.
+/// @author Tim Cornwell <tim.cornwell@csiro.au>
+#include <conrad/ConradError.h>
+
+#include <parallel/DuchampParallel.h>
+
+#include <APS/ParameterSet.h>
+
+#include <stdexcept>
+#include <iostream>
+
+#include <casa/OS/Timer.h>
+
+using std::cout;
+using std::endl;
+
+using namespace conrad;
+using namespace conrad::synthesis;
+using namespace conrad::scimath;
+using namespace LOFAR::ACC::APS;
+
+// Move to Conrad Util
+std::string getInputs(const std::string& key, const std::string& def, int argc,
+    const char** argv)
+{
+  if (argc>2)
+  {
+    for (int arg=0; arg<(argc-1); arg++)
+    {
+      std::string argument=string(argv[arg]);
+      if (argument==key)
+      {
+        return string(argv[arg+1]);
+      }
+    }
+  }
+  return def;
+}
+
+// Main function
+int main(int argc, const char** argv)
+{
+  try
+  {
+
+    casa::Timer timer;
+
+    timer.mark();
+
+    std::string parsetFile(getInputs("-inputs", "cduchamp.in", argc, argv));
+
+    ParameterSet parset(parsetFile);
+    ParameterSet subset(parset.makeSubset("Cduchamp."));
+
+    DuchampParallel duchamp(argc, argv, subset);
+    duchamp.os() << "parset file " << parsetFile << std::endl;
+    
+    duchamp.findLists();
+    duchamp.condenseLists();
+    duchamp.findStatistics();
+
+    ///==============================================================================
+  }
+  catch (conrad::ConradError& x)
+  {
+    std::cerr << "Conrad error in " << argv[0] << ": " << x.what() << std::endl;
+    exit(1);
+  }
+  catch (std::exception& x)
+  {
+    std::cerr << "Unexpected exception in " << argv[0] << ": " << x.what() << std::endl;
+    exit(1);
+  }
+  exit(0);
+}
+
---
+++ Code/Components/CP/parallel/trunk/parallel/ConradParallel.cc
@@ -0,0 +1,171 @@
+/// @file
+///
+/// @brief Base class for parallel applications
+/// @details
+/// Supports algorithms by providing methods for initialization
+/// of MPI connections, sending and models around.
+/// There is assumed to be one master and many workers.
+///
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Tim Cornwell <tim.cornwell@csiro.au>
+/// 
+#include <mwcommon/MPIConnection.h>
+#include <mwcommon/MPIConnectionSet.h>
+#include <mwcommon/MWIos.h>
+
+#include <Blob/BlobString.h>
+#include <Blob/BlobIBufString.h>
+#include <Blob/BlobOBufString.h>
+#include <Blob/BlobIStream.h>
+#include <Blob/BlobOStream.h>
+
+#include <casa/OS/Timer.h>
+#include <casa/Utilities/Regex.h>
+#include <casa/BasicSL/String.h>
+#include <casa/OS/Path.h>
+
+#include <conrad/ConradError.h>
+
+#include <parallel/ConradParallel.h>
+
+#include <sstream>
+
+using namespace std;
+using namespace conrad;
+using namespace conrad::cp;
+using namespace conrad::scimath;
+
+namespace conrad
+{
+  namespace synthesis
+  {
+
+    ConradParallel::ConradParallel(int argc, const char** argv)
+    {
+      // Initialize MPI (also succeeds if no MPI available).
+      MPIConnection::initMPI(argc, argv);
+      itsNNode = MPIConnection::getNrNodes();
+      itsRank = MPIConnection::getRank();
+
+      itsIsParallel=(itsNNode>1);
+      itsIsMaster=(itsRank==0);
+      itsIsWorker=(!itsIsParallel)||(itsRank>0);
+
+      initConnections();
+
+      if (isParallel())
+      {
+        // For MPI, send all output to separate log files. Eventually we'll do this
+        // using the log system.
+        std::ostringstream ostr;
+        casa::Path progname(argv[0]);
+        ostr << progname.baseName() << "_tmp.cout" << itsRank;
+        MWIos::setName(ostr.str());
+
+        if (isMaster())
+        {
+          os() << "CONRAD program (parallel) running on "<< itsNNode
+              << " nodes (master/master)"<< std::endl;
+        }
+        else
+        {
+          os() << "CONRAD program (parallel) running on "<< itsNNode
+              << " nodes (worker "<< itsRank << ")"<< std::endl;
+        }
+      }
+      else
+      {
+        os() << "CONRAD program (serial)"<< std::endl;
+      }
+
+    }
+
+    ConradParallel::~ConradParallel()
+    {
+      if (isParallel())
+      {
+        os() << "Exiting MPI"<< std::endl;
+        MPIConnection::endMPI();
+      }
+    }
+
+    /// Is this running in parallel?
+    bool ConradParallel::isParallel()
+    {
+      return itsIsParallel;
+    }
+
+    /// Is this the master?
+    bool ConradParallel::isMaster()
+    {
+      return itsIsMaster;
+    }
+
+    /// Is this a worker?
+    bool ConradParallel::isWorker()
+    {
+      return itsIsWorker;
+    }
+
+    std::ostream& ConradParallel::os()
+    {
+      if (isParallel())
+      {
+        if (isWorker())
+        {
+          MWCOUT << "WORKER " << itsRank << " : ";
+          return MWCOUT;
+        }
+        else
+        {
+          std::cout << "MASTER : ";
+          return std::cout;
+        }
+      }
+      else
+      {
+        return std::cout;
+      }
+    }
+
+    // Initialize connections
+    void ConradParallel::initConnections()
+    {
+      if (isParallel())
+      {
+        itsConnectionSet=MPIConnectionSet::ShPtr(new MPIConnectionSet());
+        if (isMaster())
+        {
+          // I am the master - I need a connection to every worker
+          for (int i=1; i<itsNNode; ++i)
+          {
+            itsConnectionSet->addConnection(i, 0);
+          }
+        }
+        if (isWorker())
+        {
+          // I am a worker - I only need to talk to the master
+          itsConnectionSet->addConnection(0, 0);
+        }
+      }
+    }
+
+    string ConradParallel::substituteWorkerNumber(const string& s)
+    {
+      casa::Regex reg("\%w");
+      ostringstream oos;
+      if (itsNNode>1)
+      {
+        oos << itsRank-1;
+      }
+      else
+      {
+        oos << 0;
+      }
+      casa::String cs(s);
+      cs.gsub(reg, oos.str());
+      return string(cs);
+    }
+
+  }
+}
