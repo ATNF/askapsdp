252293d8ae303f5f5aff5e400c4e52c69c2b2d42
Tim Cornwell <tim.cornwell@csiro.au>
2007-12-06 15:52:54 +0000

    Moves
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@932 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/CP/conradparallel/trunk/include/conrad_conradparallel.h
@@ -0,0 +1,14 @@
+// This is an automatically generated file. Please DO NOT edit!
+/// @file
+///
+/// Package config file. ONLY include in ".cc" files never in header files!
+#ifndef CONRAD_CONRADPARALLEL_H
+#define CONRAD_CONRADPARALLEL_H
+
+  /// The name of the package
+#define CONRAD_PACKAGE_NAME "conradparallel"
+
+  /// The version of the package
+#define CONRAD_PACKAGE_VERSION "conradparallel==trunk r5086 (2007-12-06)"
+
+#endif
