456964f6b7917a1c5bb856e2bbdc78edd372f88b
Tim Cornwell <tim.cornwell@csiro.au>
2007-12-18 15:59:38 +0000

    Standardize scripts
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@987 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
+++ Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash -l
 
-echo "Running standard simulation and imaging for CONRAD"
+HOSTNAME=`hostname -s`
 
-echo "Extracting 10uJy model"
+echo "Extracting 10uJy model" > stdtest.$HOSTNAME.out
 
-tar zxvf ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz
+tar zxvf ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz >> stdtest.$HOSTNAME.out
 
 export AIPSPATH=${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/testdata/trunk
 
-echo "Running csimulator to create MeasurementSet for a single pointing"
-time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in
+echo "Running csimulator to create MeasurementSet for a single pointing" >> stdtest.$HOSTNAME.out
+${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in >> stdtest.$HOSTNAME.out
 
-echo "Running cimager to form Clean image of single pointing"
-time ${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in
+echo "Running cimager to form Clean image of single pointing" >> stdtest.$HOSTNAME.out
+${CONRAD_PROJECT_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in >> stdtest.$HOSTNAME.out
