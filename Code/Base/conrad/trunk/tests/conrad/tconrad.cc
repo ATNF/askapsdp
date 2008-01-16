c899ed5f4177af12e8cfad721657bd36f394e208
Maxim Voronkov <maxim.voronkov@csiro.au>
2008-01-16 16:04:17 +0000

    added an adapter iterator which iterates over map keys. It is handy to extract lists of parameters from maps.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1044 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/conrad/trunk/tests/conrad/tconrad.cc
+++ Code/Base/conrad/trunk/tests/conrad/tconrad.cc
@@ -3,12 +3,14 @@
 #include <conrad_conrad.h>
 #include <IndexedLessTest.h>
 #include <ConradErrorTest.h>
+#include <MapKeyIteratorTest.h>
 
 int main( int argc, char **argv)
 {
   CppUnit::TextUi::TestRunner runner;
   runner.addTest( conrad::utility::IndexedLessTest::suite() );
   runner.addTest( conrad::ConradErrorTest::suite() );
+  runner.addTest( conrad::utility::MapKeyIteratorTest::suite() );
   runner.run();
   return 0;
 }
