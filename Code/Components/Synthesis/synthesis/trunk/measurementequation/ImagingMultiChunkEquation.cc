8c4ef35309d1db4c931f0dcb526e19315ce20502
Maxim Voronkov <maxim.voronkov@csiro.au>
2008-02-14 03:01:55 +0000

    added new structural element to the class hierarchy analogous to GenericMultiChunkEquation, but for imaging. Next step is to convert imaging measurement equations so they can be derived from this class. Such conversion would allow to use any measurement equation as a perfect measurement equation in the calibration framework. Currently, only measurement equations based on the full normal equation matrix can be used with the calibration code.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1096 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/trunk/measurementequation/ImagingMultiChunkEquation.cc
@@ -0,0 +1,77 @@
+/// @file
+/// 
+/// @brief A structural type joining together ImagingEquation and MultiChunkEquation
+/// @details Because we deal with the double inheritance here, we need to overload
+/// explicitly predict and calcImagingEquation methods, otherwise the methods
+/// in the scimath::Equation tree are left pure abstract. Theoretically, we can
+/// get rid of the double inheritance here and the need of this class would 
+/// disappear. However, MultiChunkEquation is envisaged to be a temporary
+/// class before we start to work with accessors only. Therefore, the inheritance
+/// is left double to highlight the future interface 
+/// (derived from IMeasurementEquation).
+///
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Max Voronkov <maxim.voronkov@csiro.au>
+
+
+#include <measurementequation/ImagingMultiChunkEquation.h>
+#include <conrad/ConradError.h>
+
+namespace conrad {
+
+namespace synthesis {
+
+/// @brief Standard constructor, which remembers data iterator.
+/// @param[in] idi data iterator
+ImagingMultiChunkEquation::ImagingMultiChunkEquation(const IDataSharedIter& idi) :
+         MultiChunkEquation(idi) {}
+
+
+/// @brief Calculate the normal equations for the iterator
+/// @details This version iterates through all chunks of data and
+/// calls an abstract method declared in IMeasurementEquation for each 
+/// individual accessor (each iteration of the iterator). 
+/// @param[in] ne Normal equations
+void ImagingMultiChunkEquation::calcImagingEquations(conrad::scimath::ImagingNormalEquations& ne) const
+{
+  MultiChunkEquation::calcEquations(ne);
+}
+
+/// @brief Calculate the normal equation for one accessor (chunk).
+/// @details This calculation is done for a single chunk of
+/// data only (one iteration).It seems that all measurement
+/// equations should work with accessors rather than iterators
+/// (i.e. the iteration over chunks should be moved to the higher
+/// level, outside this class). This method overrides an abstract method
+/// of MultiChunkEquation. It calls calcImagingEquations(chunk,ne)  with 
+/// ne converted to ImagingNormalEquations
+/// @param[in] chunk a read-write accessor to work with
+/// @param[in] ne Normal equations
+void ImagingMultiChunkEquation::calcEquations(const IConstDataAccessor &chunk,
+                   conrad::scimath::INormalEquations& ne) const
+{
+  try {
+     calcImagingEquations(chunk, 
+             dynamic_cast<conrad::scimath::ImagingNormalEquations&>(ne));
+  }
+  catch (const std::bad_cast &bc) {
+     CONRADTHROW(ConradError, "An attempt to use incompatible type of "
+                 "the normal equations class with a derivative from "
+                 "ImagingMultiChunkEquation. It accepts only ImagingNormalEquations "
+                 "and derivatives. This exception probably indicates a logic error");    
+  }
+}
+
+/// @brief Predict model visibility for the iterator.
+/// @details This version of the predict method iterates
+/// over all chunks of data and calls an abstract method declared
+/// in IMeasurementEquation for each accessor. 
+void ImagingMultiChunkEquation::predict() const      
+{
+  MultiChunkEquation::predict();
+}
+
+
+} // namespace synthesis
+
+} // namespace conrad
---
+++ Code/Components/Synthesis/synthesis/trunk/measurementequation/ImagingMultiChunkEquation.h
@@ -0,0 +1,94 @@
+/// @file
+/// 
+/// @brief A structural type joining together ImagingEquation and MultiChunkEquation
+/// @details Because we deal with the double inheritance here, we need to overload
+/// explicitly predict and calcImagingEquation methods, otherwise the methods
+/// in the scimath::Equation tree are left pure abstract. Theoretically, we can
+/// get rid of the double inheritance here and the need of this class would 
+/// disappear. However, MultiChunkEquation is envisaged to be a temporary
+/// class before we start to work with accessors only. Therefore, the inheritance
+/// is left double to highlight the future interface 
+/// (derived from IMeasurementEquation).
+///
+/// @copyright (c) 2007 CONRAD, All Rights Reserved.
+/// @author Max Voronkov <maxim.voronkov@csiro.au>
+
+#ifndef IMAGING_MULTI_CHUNK_EQUATION_H
+#define IMAGING_MULTI_CHUNK_EQUATION_H
+
+// own include
+#include <measurementequation/MultiChunkEquation.h>
+#include <fitting/ImagingEquation.h>
+#include <fitting/ImagingNormalEquations.h>
+
+
+namespace conrad {
+
+namespace synthesis {
+
+
+/// @brief A structural type joining together ImagingEquation and MultiChunkEquation
+/// @details Because we deal with the double inheritance here, we need to overload
+/// explicitly predict and calcImagingEquation methods, otherwise the methods
+/// in the scimath::Equation tree are left pure abstract. Theoretically, we can
+/// get rid of the double inheritance here and the need of this class would 
+/// disappear. However, MultiChunkEquation is envisaged to be a temporary
+/// class before we start to work with accessors only. Therefore, the inheritance
+/// is left double to highlight the future interface 
+/// (derived from IMeasurementEquation).
+/// @ingroup measurementequation
+struct ImagingMultiChunkEquation : virtual public MultiChunkEquation,
+                                   virtual public conrad::scimath::ImagingEquation    
+{  
+  /// @brief Standard constructor, which remembers data iterator.
+  /// @param[in] idi data iterator
+  ImagingMultiChunkEquation(const IDataSharedIter& idi);
+
+  /// @brief Calculate the normal equations for the iterator
+  /// @details This version iterates through all chunks of data and
+  /// calls an abstract method declared in IMeasurementEquation for each 
+  /// individual accessor (each iteration of the iterator). 
+  /// @param[in] ne Normal equations
+  virtual void calcImagingEquations(conrad::scimath::ImagingNormalEquations& ne) const;
+
+  /// @brief Calculate the normal equation for one accessor (chunk).
+  /// @details This calculation is done for a single chunk of
+  /// data only (one iteration).It seems that all measurement
+  /// equations should work with accessors rather than iterators
+  /// (i.e. the iteration over chunks should be moved to the higher
+  /// level, outside this class). This method overrides an abstract method
+  /// of MultiChunkEquation. It calls calcImagingEquations(chunk,ne)  with 
+  /// ne converted to ImagingNormalEquations
+  /// @param[in] chunk a read-write accessor to work with
+  /// @param[in] ne Normal equations
+  virtual void calcEquations(const IConstDataAccessor &chunk,
+                          conrad::scimath::INormalEquations& ne) const;
+  
+  /// @brief Calculate the normal equation for one accessor (chunk).
+  /// @details This calculation is done for a single chunk of
+  /// data only (one iteration).It seems that all measurement
+  /// equations should work with accessors rather than iterators
+  /// (i.e. the iteration over chunks should be moved to the higher
+  /// level, outside this class). 
+  /// This class translates calls to calcEquations(chunk, ne) to 
+  /// this method and does type conversion.
+  /// @param[in] chunk a read-write accessor to work with
+  /// @param[in] ne Normal equations
+  virtual void calcImagingEquations(const IConstDataAccessor &chunk,
+                          conrad::scimath::ImagingNormalEquations& ne) const = 0;
+
+  /// @brief Predict model visibility for the iterator.
+  /// @details This version of the predict method iterates
+  /// over all chunks of data and calls an abstract method declared
+  /// in IMeasurementEquation for each accessor. 
+  virtual void predict() const;  
+ 
+  using MultiChunkEquation::calcEquations;
+};
+
+
+} // namespace synthesis
+
+} // namespace conrad
+
+#endif // #ifndef IMAGING_MULTI_CHUNK_EQUATION_H
