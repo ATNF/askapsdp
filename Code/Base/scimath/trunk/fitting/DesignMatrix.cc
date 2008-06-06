df3bbec731217cec6f844a509bc0467bf31cc15c
Tony Maher <tony.maher@csiro.au>
2008-06-06 03:28:07 +0000

    Fix Tim's email address.  Since I have touched all these files with new copyrights fix Tim' emails while I am here.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1341 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/scimath/trunk/fitting/DesignMatrix.cc
+++ Code/Base/scimath/trunk/fitting/DesignMatrix.cc
@@ -24,7 +24,7 @@
 /// along with this program; if not, write to the Free Software
 /// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ///
-/// @author Tim Cornwell tim.cornwel@csiro.au
+/// @author Tim Cornwell <tim.cornwell@csiro.au>
 ///
 #include <fitting/DesignMatrix.h>
 #include <fitting/Params.h>
