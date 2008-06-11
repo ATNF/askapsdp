0b648d898fa2f1d784450382cdd988db82f3bbb4
Tony Maher <tony.maher@csiro.au>
2008-06-11 00:49:49 +0000

    See ticket:260 - add license to files that did not have any.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1343 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/analysis/trunk/sourcefitting/RadioSource.h
+++ Code/Components/Analysis/analysis/trunk/sourcefitting/RadioSource.h
@@ -3,9 +3,31 @@
 /// Defines a radio source, combining the Duchamp Detection object
 ///  with fitted component analysis
 ///
-/// (c) 2008 ASKAP, All Rights Reserved
+/// @copyright (c) 2008 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
 /// @author Matthew Whiting <matthew.whiting@csiro.au>
 ///
+
 #ifndef ASKAP_ANALYSIS_RADIOSOURCE_H_
 #define ASKAP_ANALYSIS_RADIOSOURCE_H_
 
