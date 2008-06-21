31e181d5e694b791fbccef6b0140046b0f823a67
Tim Cornwell <tim.cornwell@csiro.au>
2008-06-21 22:31:49 +0000

    Changes to track new parameters for cimager
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1368 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
+++ Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
+export AIPSPATH=${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk
 
 HOSTNAME=`hostname -s`
 
-echo "Extracting 10uJy model" > stdtest.$HOSTNAME.out
+echo "This is the ASKAPsoft stdtest. It will run for about 20-30 minutes."  | tee stdtest.$HOSTNAME.out
 
-tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz >> stdtest.$HOSTNAME.out
+echo "Extracting 10uJy model" | tee -a stdtest.$HOSTNAME.out
+tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz | tee -a stdtest.$HOSTNAME.out
 
-export AIPSPATH=${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk
+echo "Running csimulator to create MeasurementSet for a single pointing" | tee -a  stdtest.$HOSTNAME.out
+${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in | tee -a stdtest.$HOSTNAME.out
 
-echo "Running csimulator to create MeasurementSet for a single pointing" >> stdtest.$HOSTNAME.out
-${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in >> stdtest.$HOSTNAME.out
-
-echo "Running cimager to form Clean image of single pointing" >> stdtest.$HOSTNAME.out
-${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in >> stdtest.$HOSTNAME.out
+echo "Running cimager to form Clean image of single pointing" | tee -a  stdtest.$HOSTNAME.out
+${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in | tee -a stdtest.$HOSTNAME.out
