23f6ddaec2cc9b8d3757790666520fade733be2d
Tim Cornwell <tim.cornwell@csiro.au>
2008-06-22 04:31:04 +0000

    Add construction of dirty image to stdtest. We should convert this into a regression test soon.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1372 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
+++ Code/Components/Synthesis/testdata/trunk/simulation/stdtest/stdtest.sh
@@ -9,7 +9,10 @@ echo "Extracting 10uJy model" | tee -a stdtest.$HOSTNAME.out
 tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk/simulation/models/10uJy.model.small.tgz | tee -a stdtest.$HOSTNAME.out
 
 echo "Running csimulator to create MeasurementSet for a single pointing" | tee -a  stdtest.$HOSTNAME.out
-${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.in | tee -a stdtest.$HOSTNAME.out
+${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/csimulator.sh -inputs stdtest.simulator.in | tee -a stdtest.$HOSTNAME.out
+
+echo "Running cimager to form Dirty image of single pointing" | tee -a  stdtest.$HOSTNAME.out
+${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.dirty.in | tee -a stdtest.$HOSTNAME.out
 
 echo "Running cimager to form Clean image of single pointing" | tee -a  stdtest.$HOSTNAME.out
-${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.in | tee -a stdtest.$HOSTNAME.out
+${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/trunk/bin/cimager.sh -inputs stdtest.clean.in | tee -a stdtest.$HOSTNAME.out
