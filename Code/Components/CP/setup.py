d4818f0a3d3bf3bea5508a1cfc9b9d7c8851fb6c
Ben Humphreys <ben.humphreys@csiro.au>
2009-03-02 00:37:00 +0000

    Deleted obsolete and unused artifacts in central processor area.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1689 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -1,8 +1,6 @@
 from recursivebuild import build
 
-nobuild = {'accelerators': "Not integrated into build system yet.",
-           'benchmarks'  : "Not integrated into build system yet.",
-           'config'      : "minicp config does not really belong here?!",
+nobuild = {'benchmarks'  : "Not integrated into build system yet.",
            'mwcontrol'   : "Obsolete - wiki/AS05_Infrastructure/PackageStatus."}
 for pkg, msg in nobuild.iteritems():
     print("warn: %s no install. %s" % (pkg, msg))
