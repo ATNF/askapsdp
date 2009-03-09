49af5a6f071e3a4dd64dd5b295a882b587af6ede
Tony Maher <tony.maher@csiro.au>
2009-03-09 04:10:54 +0000

    Make this build.py file look more like the standard and not duplicate code from thirdpartybuilder. Still not perfect as end users should not know about internal methods.  I think the shell script should be written as a scons file (or a scons file wrapper around it.)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1706 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/simulations/simData/trunk/build.py
+++ Code/Components/Analysis/simulations/simData/trunk/build.py
@@ -1,60 +1,14 @@
 # @file
-import os
-import sys
-import shutil
-import md5
-
+from recursivebuild.thirdpartybuilder import ThirdPartyBuilder
 from recursivebuild import run
-from recursivebuild.utils import q_print
 
-def create_sig():
-    md5sig = md5.new(file("build.py").read())
-    outsig = file(".packagesig", "w")
-    outsig.write(md5sig.hexdigest())
-    outsig.close()
+class MyBuilder(ThirdPartyBuilder):
+    def _clean(self):
+        run("/bin/rm -f data/*uJy_*")
 
-def cleandata():
-    run("/bin/rm -f data/*uJy_*")
+    def _build(self):
+        run("/bin/sh createSourceLists.sh")
 
-def clean(files):
-    for f in files:
-        if os.path.exists(f):
-            if os.path.isdir(f):
-                shutil.rmtree(f)
-            else:
-                os.remove(f)
+builder = MyBuilder(pkgname=".")
 
-def install(pkg):
-    print("running install script")
-    run("sh createSourceLists.sh")
-    create_sig()
-
-#
-# main
-#
-
-bfile   = 'build.py'
-sigfile = '.packagesig'
-pkg     = 'simData'
-
-print("warn: non-standard build.py script used for %s" % pkg)
-
-if "clean" in sys.argv[1:]:
-    clean([sigfile])
-    cleandata()
-    sys.exit(0)
-
-if "install" in sys.argv[1:]:
-    if os.path.exists(sigfile):
-        pkgsig = file(sigfile).read()
-        md5sig = md5.new(file(bfile).read()).hexdigest()
-        if pkgsig == md5sig:
-            sys.exit(0)
-        else:
-            q_print("info: %s checksum differs from value in %s - install." %
-                    (bfile, sigfile))
-    else:
-        q_print("info: No %s file - install" % sigfile)
-    install(pkg)
-else:
-    print("warn: unknown build option in %s" % sys.argv[1:])
+builder.build()
