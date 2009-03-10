3b1c423077ca79dfc60668434d8db06e4e4e18f2
Tony Maher <tony.maher@csiro.au>
2009-03-10 03:59:29 +0000

    Similar to changeset:6991 modify these build.py to more closely follow our system and not duplicate code.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1709 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/build.py
+++ Code/Components/Synthesis/testdata/trunk/build.py
@@ -1,61 +1,14 @@
 # @file
-import os
-import sys
-import shutil
-import md5
-
+from recursivebuild.thirdpartybuilder import ThirdPartyBuilder
 from recursivebuild import run
-from recursivebuild.utils import q_print
 
-def create_sig():
-    md5sig = md5.new(file("build.py").read())
-    outsig = file(".packagesig", "w")
-    outsig.write(md5sig.hexdigest())
-    outsig.close()
+class MyBuilder(ThirdPartyBuilder):
+    def _build(self):
+        for fn in ['measdata', 'testdataset']:
+            run("tar -xjf %s.tar.bz2" % fn)
 
+builder = MyBuilder(pkgname=".")
+builder.add_extra_clean_targets('data')
+builder.add_extra_clean_targets('testdataset.ms')
 
-def clean(files):
-    for f in files:
-        if os.path.exists(f):
-            if os.path.isdir(f):
-                shutil.rmtree(f)
-            else:
-                os.remove(f)
-
-
-def install(pkg):
-    run("tar -xjf measdata.tar.bz2")
-    run("tar -xjf testdataset.tar.bz2")
-    #run("chmod 555 testdataset.ms") # why is this here?
-
-#
-# main
-#
-
-bfile   = 'build.py'
-sigfile = '.packagesig'
-
-pkg     = 'testdata'
-files   = ["data", "testdataset.ms"]
-
-print("warn: non-standard build.py script used for %s" % pkg)
-
-if "clean" in sys.argv[1:]:
-    clean(files + [sigfile])
-    sys.exit(0)
-
-if "install" in sys.argv[1:]:
-    if os.path.exists(sigfile):
-        pkgsig = file(sigfile).read()
-        md5sig = md5.new(file(bfile).read()).hexdigest()
-        if pkgsig == md5sig:
-            sys.exit(0)
-        else:
-            q_print("info: %s checksum differs from value in %s - install." %
-                    (bfile, sigfile))
-    else:
-        q_print("info: No %s file - install" % sigfile)
-    install(pkg)
-    print("warn: simulation subdirectory is not being built.")
-else:
-    print("warn: unknown build option in %s" % sys.argv[1:])
+builder.build()
