6a6275cb1f3b4827c3ab80fe4c96b3bf29c96d3d
Malte Marquarding <malte.marquarding@csiro.au>
2009-03-23 02:45:24 +0000

    added log_debug decorator, to log a debug message with the function signature
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1716 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/py-askap/trunk/askap/logging/__init__.py
+++ Code/Base/py-askap/trunk/askap/logging/__init__.py
@@ -9,9 +9,52 @@
 # make this module look like logging
 # since we know what we are doing allow wildcard
 # pylint: disable-msg=W0401
-
 from logging import *
+
+# insert xml socket handler
 from xmllayout import RawSocketHandler as XMLSocketHandler
 
+# decorator
+def log_debug(func):
+    """Logging decrorator for quick debugging of function calls and their arguments.
+You need to set the logging level to ''DEBUG'' to see these message ans should have a logging instance called ''logger''.
 
+Example: ..
 
+from askap.logging import getLogger, log_debug
+
+# use module name as logger name
+logger = getLogger(__name__)
+
+class Foo:
+    def __init__(self):
+        pass
+
+    @log_debug
+    def multiply(self, x, y=2):
+        return x*y
+    
+"""
+    import inspect
+    # retrieve logger from calling scope
+    logger = inspect.currentframe().f_back.f_globals.get("logger", None)
+    if not logger:
+        logger = getLogger("Unspecified")
+        basicConfig(level=DEBUG)
+    fname = func.func_name
+    def postlog(*args, **kwargs):
+        logname = ""
+        outargs = None
+        # check if we are a method of a class
+        if len(args) > 0 and args[0].__class__.__dict__.has_key(fname):
+            logname = args[0].__class__.__name__+"."
+            # remove self from args output
+            outargs = args[1:]
+        else:
+            outargs = args
+        logname += fname
+        logger.log(DEBUG, 
+                   "%s:  %s %s" % (logname, str(outargs or ""), str(kwargs or ""))) 
+        # pylint: disable-msg=W0142
+        return func(*args, **kwargs)
+    return postlog
