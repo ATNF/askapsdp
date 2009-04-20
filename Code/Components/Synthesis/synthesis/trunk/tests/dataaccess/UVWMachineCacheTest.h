58ff99eb554de140fc150b418a3070999e96dc20
Maxim Voronkov <maxim.voronkov@csiro.au>
2009-04-20 04:41:43 +0000

    added unit tests for uvw machine cache
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1742 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/UVWMachineCacheTest.h
+++ Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/UVWMachineCacheTest.h
@@ -33,6 +33,9 @@
 #include <dataaccess/UVWMachineCache.h>
 
 #include <cppunit/extensions/HelperMacros.h>
+#include <casa/Quanta/MVDirection.h>
+#include <measures/Measures/MDirection.h>
+#include <measures/Measures/UVWMachine.h>
 
 #include <boost/shared_ptr.hpp>
 
@@ -42,16 +45,66 @@ namespace synthesis {
 
 class UVWMachineCacheTest : public CppUnit::TestFixture {
    CPPUNIT_TEST_SUITE(UVWMachineCacheTest);
-   CPPUNIT_TEST(cacheTest);
+   CPPUNIT_TEST_EXCEPTION(exceptionTest,AskapError);
+   CPPUNIT_TEST(oneElementCacheTest);
+   CPPUNIT_TEST(twoElementsCacheTest);
    CPPUNIT_TEST_SUITE_END();
 public:
    void setUp() {
       itsMachineCache.reset();
    }
    
-   void cacheTest() {
-      
+   void exceptionTest() {
+      itsMachineCache.reset(new UVWMachineCache(0,1e-6));
+      testCaching();
    };
+      
+   void oneElementCacheTest() {
+      itsMachineCache.reset(new UVWMachineCache(1,1e-6));
+      testCaching();
+   };
+   
+   void twoElementsCacheTest() {
+      itsMachineCache.reset(new UVWMachineCache(2,1e-6));
+      testCaching();
+   }
+   
+protected:
+   void testCaching() const {
+      casa::MVDirection dir1(0.123456, -0.123456);
+      casa::MVDirection dir2(-0.123456, -0.123456);
+      casa::MVDirection dir3(1.123456, -0.2);
+      testDirections(dir1,dir2);
+      testDirections(dir1,dir2);
+      testDirections(dir2,dir1);
+      testDirections(dir3,dir1);
+      testDirections(dir2,dir3);      
+      testDirections(dir2,dir1);
+      testDirections(dir3,dir1);         
+   }
+   
+   void testDirections(const casa::MVDirection &dir1, const casa::MVDirection &dir2) const {
+      casa::MDirection dir1j2000(dir1, casa::MDirection::J2000);
+      casa::MDirection dir2j2000(dir2, casa::MDirection::J2000);
+      ASKAPASSERT(itsMachineCache);
+      const casa::UVWMachine &cachedMachine = itsMachineCache->machine(dir1,dir2);
+      // create a proper machine by hand
+      casa::UVWMachine machine2(dir1,dir2,false, true);
+      compareMachines(cachedMachine, machine2);         
+   }
+   
+   static void compareMachines(const casa::UVWMachine &m1, const casa::UVWMachine &m2) {
+       casa::Vector<double> uvw(3);
+       uvw[0]=1000.0; uvw[1]=-3250.0; uvw[2]=12.5;
+       casa::Vector<double> uvwCopy(uvw.copy());
+       double delay = 0, delayCopy = 0;
+       m1.convertUVW(delay, uvw);
+       m2.convertUVW(delayCopy,uvwCopy);
+       CPPUNIT_ASSERT(fabs(delay-delayCopy)<1e-6);
+       for (size_t dim=0;dim<3;++dim) {
+            CPPUNIT_ASSERT(fabs(uvw[dim]-uvwCopy[dim])<1e-6);
+       }
+   }   
    
 private:   
    boost::shared_ptr<UVWMachineCache> itsMachineCache;
