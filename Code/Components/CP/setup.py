be8367a8e725152f7040faaacb06962dbf5f3384
Ben Humphreys <ben.humphreys@csiro.au>
2009-05-26 05:22:00 +0000

    Add imager to the CP build if and only if libmpi.so can be found.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1875 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -1,11 +1,34 @@
 from askapdev.rbuild import build
+import os
+
+def whichlib(program):
+   def is_exe(fpath):
+       return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+   fpath, fname = os.path.split(program)
+   if fpath:
+       if is_exe(program):
+           return program
+   else:
+       for path in os.environ["LD_LIBRARY_PATH"].split(os.pathsep):
+           exe_file = os.path.join(path, program)
+           if is_exe(exe_file):
+               return exe_file
+
+   return None
 
 nobuild = {'benchmarks'  : "Not integrated into build system yet."}
 
 for pkg, msg in nobuild.iteritems():
     print("warn: %s no install. %s" % (pkg, msg))
 
-build([
-       'mwcommon/trunk/build.py',
-       'askapparallel/trunk/build.py',
-       ])
+tobuild = ['mwcommon/trunk/build.py', 'askapparallel/trunk/build.py']
+
+# Only build the MPI dependant components if libmpi.so is present
+if whichlib("libmpi.so"):
+    tobuild.append('imager/trunk/build.py')
+else:
+    print "No MPI support found on this platform"
+
+# Build
+build(tobuild)
