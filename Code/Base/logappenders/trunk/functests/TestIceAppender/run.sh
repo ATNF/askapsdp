333a390d022efc9ff9a94d7aef13c40e37417206
Ben Humphreys <ben.humphreys@csiro.au>
2009-05-26 02:21:12 +0000

    Ensure the test uses the Ice in ASKAPsoft and not from the local system.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1869 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/logappenders/trunk/functests/TestIceAppender/run.sh
+++ Code/Base/logappenders/trunk/functests/TestIceAppender/run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Setup the environment
+ICE_ROOT=$ASKAP_ROOT/3rdParty/Ice/tags/Ice-3.3.0/install
+
+# For Unix/Linux
+export LD_LIBRARY_PATH=$ICE_ROOT/lib:$LD_LIBRARY_PATH
+
+# For Mac OSX
+export DYLD_LIBRARY_PATH=$ICE_ROOT/lib:$DYLD_LIBRARY_PATH
+
 # Create directories for IceGrid and IceStorm
 mkdir -p data/registry
 mkdir -p data/node
 
 # Start the Ice Grid
-icegridnode --Ice.Config=config.icegrid > /dev/null 2>&1 &
+$ICE_ROOT/bin/icegridnode --Ice.Config=config.icegrid > /dev/null 2>&1 &
 ICEGRIDPID=$!
 
 # TODO: Find a method of waiting for the icegrid to startup so we
@@ -13,7 +22,7 @@ ICEGRIDPID=$!
 sleep 1
 
 # Add the IceStorm service
-icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "application add icestorm.xml" > /dev/null 2>&1
+$ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "application add icestorm.xml" > /dev/null 2>&1
 
 # TODO: Find a method of waiting for icestorm to startup so we
 # don't need to do this sleep
@@ -23,7 +32,7 @@ sleep 1
 $ASKAP_ROOT/Code/Base/logappenders/trunk/apps/tIceAppender.sh --Ice.Config=config.tIceAppender
 
 # Request IceGrid shutdown and wait
-icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "node shutdown Node1"
+$ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "node shutdown Node1"
 wait $ICEGRIDPID
 
 # Remove temporary directories
