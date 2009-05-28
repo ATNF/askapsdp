277aca6eb5ad246bd9924a986481a31362cdf8ab
Ben Humphreys <ben.humphreys@csiro.au>
2009-05-28 00:10:16 +0000

    Fix for the problem described in ticket:572 - Should have been looking for mpicxx all along anyway.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1880 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -1,35 +1,22 @@
 from askapdev.rbuild import build
 import os
-import sys
 
-STD_LIB_PATHS = ['/usr/lib', '/usr/local/lib']
+def which(program):
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
 
-def whichlib(program):
-   if sys.platform == "darwin":
-       lib_envvar = "DYLD_LIBRARY_PATH"
-   else:
-       lib_envvar = "LD_LIBRARY_PATH"
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        if os.environ.has_key("PATH"):
+           for path in os.environ["PATH"].split(os.pathsep):
+               exe_file = os.path.join(path, program)
+               if is_exe(exe_file):
+                   return exe_file
 
-   def is_readable(fpath):
-       return os.path.exists(fpath) and os.access(fpath, os.R_OK)
-
-   fpath, fname = os.path.split(program)
-   if fpath:
-       if is_readable(program):
-           return program
-   else:
-       if os.environ.has_key(lib_envvar):
-           search_paths = os.environ[lib_envvar].split(os.pathsep)
-           search_paths.extend(STD_LIB_PATHS)
-       else:
-           search_paths = STD_LIB_PATHS
-
-       for path in search_paths:
-           lib_file = os.path.join(path, program)
-           if is_readable(lib_file):
-               return lib_file
-
-   return None
+    return None
 
 nobuild = {'benchmarks'  : "Not integrated into build system yet."}
 
@@ -38,13 +25,8 @@ for pkg, msg in nobuild.iteritems():
 
 tobuild = ['mwcommon/trunk/build.py', 'askapparallel/trunk/build.py']
 
-# Only build the MPI dependant components if libmpi.so is present
-if sys.platform == "darwin":
-    lib_name = "libmpi.dylib"
-else:
-    lib_name = "libmpi.so"
-
-if whichlib(lib_name):
+# Only build the MPI dependant components if mpicxx is present
+if which("mpicxx"):
     tobuild.append('imager/trunk/build.py')
 else:
     print "No MPI support found on this platform"
