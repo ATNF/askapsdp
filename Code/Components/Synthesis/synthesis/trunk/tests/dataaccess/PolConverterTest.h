7853920eec1b1bd7ff5fe61b219e3f70c09e644e
Maxim Voronkov <maxim.voronkov@csiro.au>
2009-06-03 07:26:04 +0000

    added a unit test to ensure the relative order of the stokes parameters defined in casa stays the same
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1898 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/PolConverterTest.h
+++ Code/Components/Synthesis/synthesis/trunk/tests/dataaccess/PolConverterTest.h
@@ -40,6 +40,7 @@ namespace synthesis {
 class PolConverterTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(PolConverterTest);
   CPPUNIT_TEST(dimensionTest);
+  CPPUNIT_TEST(stokesEnumTest);
   CPPUNIT_TEST_SUITE_END();
 public:
   void dimensionTest() {
@@ -56,6 +57,37 @@ public:
      casa::Vector<casa::Complex> inVec(in.nelements(), casa::Complex(0,-1.));
      casa::Vector<casa::Complex> outVec = pc(inVec);
      CPPUNIT_ASSERT(outVec.nelements() == out.nelements());
+  }
+  
+  void stokesEnumTest() {
+     // our code relies on a particular order of the stokes parameters in the enum defined in casacore.
+     // The following code tests that enums components corresponding to the same polarisation
+     // frame are following each other and the order is preserved.
+     
+     // I,Q,U,V
+     CPPUNIT_ASSERT(int(casa::Stokes::Q)-int(casa::Stokes::I) == 1);
+     CPPUNIT_ASSERT(int(casa::Stokes::U)-int(casa::Stokes::I) == 2);
+     CPPUNIT_ASSERT(int(casa::Stokes::V)-int(casa::Stokes::I) == 3);
+     
+     // XX,XY,YX,YY
+     CPPUNIT_ASSERT(int(casa::Stokes::XY)-int(casa::Stokes::XX) == 1);
+     CPPUNIT_ASSERT(int(casa::Stokes::YX)-int(casa::Stokes::XX) == 2);
+     CPPUNIT_ASSERT(int(casa::Stokes::YY)-int(casa::Stokes::XX) == 3);
+     
+     // RR,RL,LR,LL
+     CPPUNIT_ASSERT(int(casa::Stokes::RL)-int(casa::Stokes::RR) == 1);
+     CPPUNIT_ASSERT(int(casa::Stokes::LR)-int(casa::Stokes::RR) == 2);
+     CPPUNIT_ASSERT(int(casa::Stokes::LL)-int(casa::Stokes::RR) == 3);
+     
+     // mixed products
+     CPPUNIT_ASSERT(int(casa::Stokes::RY)-int(casa::Stokes::RX) == 1);
+     CPPUNIT_ASSERT(int(casa::Stokes::LX)-int(casa::Stokes::RX) == 2);
+     CPPUNIT_ASSERT(int(casa::Stokes::LY)-int(casa::Stokes::RX) == 3);
+     CPPUNIT_ASSERT(int(casa::Stokes::XR)-int(casa::Stokes::RX) == 4);
+     CPPUNIT_ASSERT(int(casa::Stokes::XL)-int(casa::Stokes::RX) == 5);
+     CPPUNIT_ASSERT(int(casa::Stokes::YR)-int(casa::Stokes::RX) == 6);
+     CPPUNIT_ASSERT(int(casa::Stokes::YL)-int(casa::Stokes::RX) == 7);
+     
   }
 };
 
