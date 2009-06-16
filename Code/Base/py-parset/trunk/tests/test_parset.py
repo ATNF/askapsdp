6516e792e4a53d9e3046bf1e8d355fb41e67abbc
Malte Marquarding <malte.marquarding@csiro.au>
2009-06-16 04:02:44 +0000

    added unut test for string ranges
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1935 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/py-parset/trunk/tests/test_parset.py
+++ Code/Base/py-parset/trunk/tests/test_parset.py
@@ -34,6 +34,7 @@ def decoder(k, v):
 def test_decode():
     expr = {'1..9': range(1, 10),
             '9..1': range(9, 0, -1),
+            'abc1..2.txt': ['abc1.txt', 'abc2.txt'],
             '[1,2]': [1,2],
             '[x, y]': ['x', 'y'],
             '[1, x]': [1, 'x'],
