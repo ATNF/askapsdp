d910c6af165b1171e5e012b109d8cde3cdecb848
Ger van Diepen <ger.vandiepen@csiro.au>
2009-06-16 01:09:27 +0000

    Added testloadgridder
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1931 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/setup.py
+++ Code/Components/Synthesis/setup.py
@@ -2,4 +2,5 @@ from askapdev.rbuild import build
 
 build(['testdata/setup.py',
        'synthesis/setup.py',
-       'synthutil/setup.py'])
+       'synthutil/setup.py',
+       'testloadgridder/setup.py'])
