248cc6c9206001f8dc52ee7fe8b0663d9f0cda81
Malte Marquarding <malte.marquarding@csiro.au>
2009-06-17 03:48:57 +0000

    added comments
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1938 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/py-parset/trunk/tests/test_parset.py
+++ Code/Base/py-parset/trunk/tests/test_parset.py
@@ -2,35 +2,42 @@ import os
 from askap.parset import ParameterSet, decode
 from nose.tools import raises
 
+# default constructor test
 def test_constructor():
     p = ParameterSet()
 
+# from file
 def test_fileconstructor():
     testfile = os.path.join(os.path.split(__file__)[0], 'example.parset')
     p = ParameterSet(testfile)
 
+# call constructor with args
 def constructor(args):
     if not hasattr(args, "__len__"):
         args = (args,)
     p = ParameterSet(*args)
 
+# generator for contructor arguments
 def test_args():
     for t in [('x.y', 1), ('x', [1,2])]:
         yield constructor, t
 
+# test __str__ function
 def test_str():
     key = 'x.y'
     p = ParameterSet(key, 1)
     assert(p.x.y == 1)
 
+# test getting sub-parset "node"
 def test_slice():
     p = ParameterSet('x.y.z', 1)
     assert isinstance(p.x.y, ParameterSet)
 
-
+# check decoded value
 def decoder(k, v):
     assert decode(k) == v
 
+# test generator for parset expressions
 def test_decode():
     expr = {'1..9': range(1, 10),
             '9..1': range(9, 0, -1),
