b45a1b3187de20aea1b932b5592cd2603d760d8d
Malte Marquarding <malte.marquarding@csiro.au>
2009-06-18 01:19:26 +0000

    Added sphinx Makefile
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1947 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/py-parset/trunk/doc/conf.py
+++ Code/Base/py-parset/trunk/doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'askap.parset'
-copyright = u'2009, Malte Marquarding'
+copyright = u'2009, CSIRO'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
