e7295eb1216d8cf8081e2573c3991127bd7bb1b6
Ben Humphreys <ben.humphreys@csiro.au>
2009-06-24 06:29:16 +0000

    Added support for major cycles in the spectral line imager. Consolidated testdata in functest. Updates to account for interface change made in changeset:7689.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@1986 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/imager/branches/specline-dev/functests/continuum/run.sh
+++ Code/Components/CP/imager/branches/specline-dev/functests/continuum/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT=output.txt
+
 export AIPSPATH=${ASKAP_ROOT}/Code/Components/Synthesis/testdata/trunk
 
 if [ ! -x ${ASKAP_ROOT}/Code/Components/CP/imager/trunk/apps/imager.sh ]; then
@@ -11,13 +13,13 @@ rm -rf image.i.10uJy_dirty_stdtest/ psf.i.10uJy_dirty_stdtest/ weights.i.10uJy_d
 echo Done
 
 echo -n Extracting measurement set...
-tar zxf 10uJy_stdtest.ms.tgz
+tar zxf ../10uJy_stdtest.ms.tgz
 mv 10uJy_stdtest.ms 10uJy_stdtest_0.ms
-tar zxf 10uJy_stdtest.ms.tgz
+tar zxf ../10uJy_stdtest.ms.tgz
 mv 10uJy_stdtest.ms 10uJy_stdtest_1.ms
 echo Done
 
-mpirun -np 2 ${ASKAP_ROOT}/Code/Components/CP/imager/trunk/apps/imager.sh -inputs dirty.in
+mpirun -np 2 ${ASKAP_ROOT}/Code/Components/CP/imager/trunk/apps/imager.sh -inputs dirty.in | tee $OUTPUT
 
 echo -n Removing measurement set...
 rm -rf 10uJy_stdtest_0.ms
@@ -26,12 +28,15 @@ echo Done
 
 if [ ! -d image.i.10uJy_dirty_stdtest ]; then
     echo Image file was not created
+    exit 1
 fi
 
 if [ ! -d psf.i.10uJy_dirty_stdtest ]; then
     echo PSF file was not created
+    exit 1
 fi
 
 if [ ! -d weights.i.10uJy_dirty_stdtest ]; then
     echo Weights image was not created
+    exit 1
 fi
