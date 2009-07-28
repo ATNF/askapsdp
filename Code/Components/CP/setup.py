20f2d58fe5ae7265d2da9d899c9bfecce047dfb8
Tony Maher <tony.maher@csiro.au>
2009-07-28 02:15:01 +0000

    See ticket:673.  Make CP/setup.py less rogue-like and move the test for MPI support down into imager/trunk/build.py. Make the 'which' function a utility as it may be used elsewhere.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2070 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -1,36 +1,11 @@
 from askapdev.rbuild import build
-import os
-
-def which(program):
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        if os.environ.has_key("PATH"):
-           for path in os.environ["PATH"].split(os.pathsep):
-               exe_file = os.path.join(path, program)
-               if is_exe(exe_file):
-                   return exe_file
-
-    return None
-
 
 nobuild = {'benchmarks'  : "Not integrated into build system yet."}
 
 for pkg, msg in nobuild.iteritems():
     print("warn: %s no install. %s" % (pkg, msg))
 
-tobuild = ['mwcommon/trunk/build.py', 'askapparallel/trunk/build.py']
-
-# Only build the MPI dependant components if mpicxx is present
-if which("mpicxx"):
-    tobuild.append('imager/trunk/build.py')
-else:
-    print "warn: No MPI support found on this platform."
-
-# Build
-build(tobuild)
+build(['mwcommon/trunk/build.py',
+       'askapparallel/trunk/build.py',
+       'imager/trunk/build.py',
+      ])
