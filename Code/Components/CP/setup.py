6d44ccc312447ca3ea74132285e2fb34edd9d791
Ben Humphreys <ben.humphreys@csiro.au>
2009-07-31 05:42:50 +0000

    Updated setup.py in CP so it no longer tries to build the moved packages. This was missed from changeset:7946.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2081 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -5,7 +5,5 @@ nobuild = {'benchmarks'  : "Not integrated into build system yet."}
 for pkg, msg in nobuild.iteritems():
     print("warn: %s no install. %s" % (pkg, msg))
 
-build(['mwcommon/trunk/build.py',
-       'askapparallel/trunk/build.py',
-       'imager/trunk/build.py',
+build(['imager/trunk/build.py',
       ])
