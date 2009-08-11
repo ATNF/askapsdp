a83010f619478d63a98e5575e4e17ad8cc0dea40
Tony Maher <tony.maher@csiro.au>
2009-08-11 06:03:05 +0000

    Doh! revert changeset:8078.  There are machines which have mpicxx but do not have modules, so 3rdParty/openmpi will fail to create the virtual package symlink but we can still build imager. i.e. cannot just check 3rdParty/openmpi dependency (which we should - will do in later commit), we still need to check path for system openmpi.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2107 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/imager/trunk/build.py
+++ Code/Components/CP/imager/trunk/build.py
@@ -1,8 +1,14 @@
 # @file
 # build script for AutoBuild
 
+import sys
+
 from askapdev.rbuild.builders import Scons as Builder
 import askapdev.rbuild.utils as utils
+
+if not utils.which("mpicxx"):
+    print "warn: Cannot build package as no MPI support found on this platform."
+    sys.exit(0)
 
 b = Builder(".")
 b.build()
