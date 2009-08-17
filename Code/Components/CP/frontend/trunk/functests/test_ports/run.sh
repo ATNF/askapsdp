2e1bc8dcb579db78d95890c32560398828031975
Ben Humphreys <ben.humphreys@csiro.au>
2009-08-17 06:45:12 +0000

    Partial fix for ticket:713 however the problem is not resolved for Mac OSX.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2126 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/functests/test_ports/run.sh
+++ Code/Components/CP/frontend/trunk/functests/test_ports/run.sh
@@ -2,6 +2,7 @@
 
 # Setup the environment
 ICE_ROOT=$ASKAP_ROOT/3rdParty/Ice/tags/Ice-3.3.0/install
+export PATH=$PATH:$ICE_ROOT/bin
 
 # For Unix/Linux
 export LD_LIBRARY_PATH=$ICE_ROOT/lib:$LD_LIBRARY_PATH
