61c9b09f13865ca45cc337168854941731406e7d
Ben Humphreys <ben.humphreys@csiro.au>
2009-08-21 05:15:59 +0000

    Fixes two problems. 1) This test only worked on systems which has ICE installed and in the PATH, now it works using the ASKAPsoft build of ICE. 2) The sleep 1 was not sufficiently long, particularly on the Mac where network calls tend to take longer than on Linux.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2150 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/logappenders/trunk/functests/TestIceAppender/run.sh
+++ Code/Base/logappenders/trunk/functests/TestIceAppender/run.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
+waitIceGrid()
+{ # Wait for IceGrid to start, but timeout after 10 seconds
+    echo -n Waiting for IceGrid to startup...
+    TIMEOUT=10
+    STATUS=1
+    while [ $STATUS -ne 0 ] && [ $TIMEOUT -ne 0 ]; do
+        $ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "node list" > /dev/null 2>&1
+        STATUS=$?
+        TIMEOUT=`expr $TIMEOUT - 1`
+        sleep 1
+    done
+
+    if [ $TIMEOUT -eq 0 ]; then
+        echo FAILED
+    else
+        echo STARTED
+    fi
+}
+
+waitIceStorm()
+{ # Wait for IceGrid to start, but timeout after 10 seconds
+    echo -n Waiting for IceStorm to startup...
+    TIMEOUT=10
+    STATUS=1
+    INACTIVE="<inactive>"
+    RESULT=$INACTIVE
+    while [ $STATUS -ne 0 ] && [ "${RESULT}" = "${INACTIVE}" ] && [ $TIMEOUT -ne 0 ]; do
+        RESULT=`$ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "adapter endpoints IceStorm.TopicManager" > /dev/null 2>&1`
+        STATUS=$?
+        TIMEOUT=`expr $TIMEOUT - 1`
+        sleep 1
+    done
+
+    if [ $TIMEOUT -eq 0 ]; then
+        echo FAILED
+    else
+        echo STARTED
+    fi
+}
+
 # Setup the environment
 ICE_ROOT=$ASKAP_ROOT/3rdParty/Ice/tags/Ice-3.3.0/install
+export PATH=$ICE_ROOT/bin:$PATH
 
 # For Unix/Linux
 export LD_LIBRARY_PATH=$ICE_ROOT/lib:$LD_LIBRARY_PATH
@@ -17,23 +58,24 @@ mkdir -p data/node
 $ICE_ROOT/bin/icegridnode --Ice.Config=config.icegrid > /dev/null 2>&1 &
 ICEGRIDPID=$!
 
-# TODO: Find a method of waiting for the icegrid to startup so we
-# don't need to do this sleep
-sleep 1
+waitIceGrid
 
 # Add the IceStorm service
 $ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "application add icestorm.xml" > /dev/null 2>&1
 
-# TODO: Find a method of waiting for icestorm to startup so we
-# don't need to do this sleep
-sleep 1
+waitIceStorm
 
 # Run the test
 $ASKAP_ROOT/Code/Base/logappenders/trunk/apps/tIceAppender.sh --Ice.Config=config.tIceAppender
+STATUS=$?
 
 # Request IceGrid shutdown and wait
+echo -n "Stopping IceGrid and IceStorm..."
 $ICE_ROOT/bin/icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "node shutdown Node1"
 wait $ICEGRIDPID
+echo "STOPPED"
 
 # Remove temporary directories
 rm -rf data
+
+exit $STATUS
