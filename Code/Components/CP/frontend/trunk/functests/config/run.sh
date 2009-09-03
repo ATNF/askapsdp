25f8ccf7d80f17afff81a4f88414711b753d6680
Ben Humphreys <ben.humphreys@csiro.au>
2009-09-03 00:32:16 +0000

    Created common scripts to start and stop cpfe_runtime for functional tests.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2208 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/functests/config/run.sh
+++ Code/Components/CP/frontend/trunk/functests/config/run.sh
@@ -7,13 +7,15 @@ source $ASKAP_ROOT/Code/Components/CP/frontend/trunk/init_package_env.sh
 ../start_services.sh config.icegrid
 
 # Start the runtime
-echo "Starting the Central Processor Frontend Runtime..."
-icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "application add cpfe_runtime.xml"
+../start_runtime.sh config.icegrid
 sleep 2
 
 # Run the test
 $ASKAP_ROOT/Code/Components/CP/frontend/trunk/apps/tConfig.sh --Ice.Config=config.tConfig -inputs workflow.in
 STATUS=$?
+
+# Stop the runtime
+../stop_runtime.sh config.icegrid
 
 # Stop the Ice Services
 ../stop_services.sh config.icegrid
--- Code/Components/CP/frontend/trunk/functests/control/run.sh
+++ Code/Components/CP/frontend/trunk/functests/control/run.sh
@@ -14,13 +14,15 @@ export DYLD_LIBRARY_PATH=$ICE_ROOT/lib:$DYLD_LIBRARY_PATH
 ../start_services.sh config.icegrid
 
 # Start the runtime
-echo "Starting the Central Processor Frontend Runtime..."
-icegridadmin --Ice.Config=config.icegrid -u foo -p bar -e "application add cpfe_runtime.xml"
+../start_runtime.sh config.icegrid
 sleep 2
 
 # Run the test
 $ASKAP_ROOT/Code/Components/CP/frontend/trunk/apps/tControl.sh --Ice.Config=config.tControl -inputs workflow.in
 STATUS=$?
+
+# Stop the runtime
+../stop_runtime.sh config.icegrid
 
 # Stop the Ice Services
 ../stop_services.sh config.icegrid
