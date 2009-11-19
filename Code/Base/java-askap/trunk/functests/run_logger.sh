4b1f0a97a6190849db5015e4ce68d457adbcee92
David Brodrick <david.brodrick@csiro.au>
2009-11-19 00:12:26 +0000

    Remove redundant comment.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2439 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/java-askap/trunk/functests/run_logger.sh
+++ Code/Base/java-askap/trunk/functests/run_logger.sh
@@ -3,8 +3,5 @@
 #Invoke the askap.logger.Logger class so that it generates some log messages
 #using the LoggerIce implementation.
 #
-#Really want to avoid populating the CLASSPATH in this way but I am not sure
-#how else to go about it at the moment.
-#
 export CLASSPATH=$ASKAP_ROOT/lib/java-askap.jar:$CLASSPATH
 java -Daskap.logger.configuration=askaplogger-ice-locator-localhost-4061.properties askap.logger.Logger
