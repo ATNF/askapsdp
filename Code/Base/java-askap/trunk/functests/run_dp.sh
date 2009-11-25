10fd4e412d4651f8abbcfe4aaecab7a60868aa4e
David Brodrick <david.brodrick@csiro.au>
2009-11-25 04:04:08 +0000

    Fix errors in script comments.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2459 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/java-askap/trunk/functests/run_dp.sh
+++ Code/Base/java-askap/trunk/functests/run_dp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-#Invoke the askap.logger.Logger class so that it generates some log messages
-#using the LoggerIce implementation.
+#Invoke the DataPublisher to publish some test data.
 #
 export CLASSPATH=$ASKAP_ROOT/lib/java-askap.jar:$CLASSPATH
 java askap.datapublisher.DataPublisher DataPublisher.test localhost 4061
