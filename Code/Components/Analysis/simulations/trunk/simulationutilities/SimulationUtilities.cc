333ad1eb0bedd73237d0ff86f52aacac72686fad
Matthew Whiting <matthew.whiting@csiro.au>
2009-12-18 11:53:05 +0000

    Typo fixed.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2517 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/simulations/trunk/simulationutilities/SimulationUtilities.cc
+++ Code/Components/Analysis/simulations/trunk/simulationutilities/SimulationUtilities.cc
@@ -261,7 +261,7 @@ namespace askap {
 	    unsigned int yref = int(y+0.5);
 	    unsigned int spatialPixel = xref + axes[0]*yref;
 
-	    while(length < 2.*zeroPointMax && (xref>=0 && xref<axes[0]) && (yref>=0 && yref<axes[0])){
+	    while(length < 2.*zeroPointMax && (xref>=0 && xref<axes[0]) && (yref>=0 && yref<axes[1])){
 // 	      ASKAPLOG_DEBUG_STR(logger, "At (x,y)=("<<x<<','<<y<<") and (xref,yref)=("<<xref<<','<<yref<<")");
 
 	      if(!specialCase) {
