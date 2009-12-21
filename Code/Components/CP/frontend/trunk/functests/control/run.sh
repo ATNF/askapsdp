1a6c22840cc6a1afdb88c586d8db207754211a43
Ben Humphreys <ben.humphreys@csiro.au>
2009-12-21 01:56:35 +0000

    Remove hardcoding of Ice version and use init_package_env.sh instead to initialise the environment.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2522 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/functests/control/run.sh
+++ Code/Components/CP/frontend/trunk/functests/control/run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # Setup the environment
-ICE_ROOT=$ASKAP_ROOT/3rdParty/Ice/tags/Ice-3.3.0/install
-export PATH=$PATH:$ICE_ROOT/bin
-
-# For Unix/Linux
-export LD_LIBRARY_PATH=$ICE_ROOT/lib:$LD_LIBRARY_PATH
-
-# For Mac OSX
-export DYLD_LIBRARY_PATH=$ICE_ROOT/lib:$DYLD_LIBRARY_PATH
+source $ASKAP_ROOT/Code/Components/CP/frontend/trunk/init_package_env.sh
 
 # Start the Ice Services
 ../start_services.sh config.icegrid
