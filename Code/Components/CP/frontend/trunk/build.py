cfd635c57f722bc5a63934a5a4219f15f9b97ee8
Tony Maher <tony.maher@csiro.au>
2010-01-05 01:12:30 +0000

    See #733 and #718.  Cannot fix because some libraries come from system and link against one thread version, and thus cannot in ASKAPsoft uncondtional link against the other one. This will be resolved in Debian lenny 32 bit where the old threading library is gone.  In Debian etch amd64 there is only the new threading library so it was never affected.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2550 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/build.py
+++ Code/Components/CP/frontend/trunk/build.py
@@ -1,7 +1,17 @@
 # @file
 # build script for AutoBuild
+import os
+import sys
 
 from askapdev.rbuild.builders import Scons as Builder
+
+# Does not build on Debian etch 32 bit but should work on lenny.
+# As yet do not have any 32 bit lenny machines.
+# The conditional picks up all 
+
+if sys.platform == 'linux2' and os.uname()[4] == 'i686':
+    print "warn: Unable to build on etch 32 bit due to thread library confusion"
+    sys.exit()
 
 b = Builder(".")
 b.build()
