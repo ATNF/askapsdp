7cdb386c0ba2f6c36386d7b7c4f32e77860ecdee
Tony Maher <tony.maher@csiro.au>
2010-01-08 00:04:23 +0000

    Make these specific to Debian etch using platform module as suggested by Malte.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2561 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/build.py
+++ Code/Components/CP/frontend/trunk/build.py
@@ -1,15 +1,18 @@
 # @file
 # build script for AutoBuild
 import os
+import platform
 import sys
 
 from askapdev.rbuild.builders import Scons as Builder
 
 # Does not build on Debian etch 32 bit but should work on lenny.
 # As yet do not have any 32 bit lenny machines.
-# The conditional picks up all 
 
-if sys.platform == 'linux2' and os.uname()[4] == 'i686':
+dist, ver = platform.dist()[0:2]
+arch = platform.architecture()[0]
+
+if dist == 'debian' and ver.startswith('4.') and arch == '32bit':
     print "warn: Unable to build on etch 32 bit due to thread library confusion"
     sys.exit()
 
