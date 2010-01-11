da5e535229bbed0cf22a49a202dcb6de19e49791
Ben Humphreys <ben.humphreys@csiro.au>
2010-01-11 03:39:37 +0000

    The correlator simulator now sends the reqired metadata and visibilities from the measurement set. This code however is a bit of a mess, is untested, makes a ton of assumptions and needs refactoring.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2566 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/correlatorsim/trunk/simplayback/MSReader.cc
+++ Code/Components/CP/correlatorsim/trunk/simplayback/MSReader.cc
@@ -28,7 +28,7 @@
 #include "MSReader.h"
 
 // Include package level header file
-#include <askap_correlatorsim.h>
+#include "askap_correlatorsim.h"
 
 // System includes
 #include <string>
@@ -41,6 +41,11 @@
 #include "askap/AskapLogging.h"
 #include "ms/MeasurementSets/MeasurementSet.h"
 #include "ms/MeasurementSets/MSColumns.h"
+#include "ms/MeasurementSets/MSDerivedValues.h"
+#include "casa/Arrays/Matrix.h"
+#include "casa/Arrays/Vector.h"
+#include "measures/Measures/MDirection.h"
+#include "measures/Measures/MEpoch.h"
 #include "cpcommon/VisPayload.h"
 
 // Local package includes
@@ -71,23 +76,23 @@ bool MSReader::fillNext(askap::interfaces::TimeTaggedTypedValueMap& metadata,
     ROMSColumns msc(itsMS);
 
     // Get a reference to the columns of interest
-    const casa::ROMSAntennaColumns &antc = msc.antenna();
-    const casa::ROMSFeedColumns &feedc = msc.feed();
-    //const casa::ROMSFieldColumns &fieldc = msc.field();
-    //const casa::ROMSSpWindowColumns &spwc = msc.spectralWindow();
-    const casa::ROMSDataDescColumns &ddc = msc.dataDescription();
-    const casa::ROMSPolarizationColumns &polc = msc.polarization();
+    const casa::ROMSAntennaColumns& antc = msc.antenna();
+    const casa::ROMSFeedColumns& feedc = msc.feed();
+    const casa::ROMSFieldColumns& fieldc = msc.field();
+    const casa::ROMSSpWindowColumns& spwc = msc.spectralWindow();
+    const casa::ROMSDataDescColumns& ddc = msc.dataDescription();
+    const casa::ROMSPolarizationColumns& polc = msc.polarization();
+    //const casa::ROMSPointingColumns& pointingc = msc.pointing();
 
     // Define some useful variables
     const int dataDescId = msc.dataDescId()(itsCurrentRow);
     const unsigned int descPolId = ddc.polarizationId()(dataDescId);
-    //const unsigned int descSpwId = ddc.spectralWindowId()(dataDescId);
+    const unsigned int descSpwId = ddc.spectralWindowId()(dataDescId);
     const unsigned int nRow = msc.nrow(); // In the whole table, not just for this integration
     const unsigned int nCorr = polc.numCorr()(descPolId);
-    //const unsigned int nChan = spwc.numChan()(descSpwId);
+    const unsigned int nChan = spwc.numChan()(descSpwId);
     const unsigned int nAntenna = antc.nrow();
     const unsigned int nBeam = feedc.nrow() / nAntenna;
-    //const unsigned int nBaseline = (nAntenna * (nAntenna + 1)) / 2;
     const unsigned int nCoarseChan = 1;
 
     // Record the timestamp for the current integration that is
@@ -100,6 +105,9 @@ bool MSReader::fillNext(askap::interfaces::TimeTaggedTypedValueMap& metadata,
     //////////////////////////////////////////////////////////////
     // Metadata
     //////////////////////////////////////////////////////////////
+
+    // Some constraints
+    ASKAPCHECK(fieldc.nrow() == 1, "Currently only support a single field");
 
     // time
     {
@@ -154,18 +162,33 @@ bool MSReader::fillNext(askap::interfaces::TimeTaggedTypedValueMap& metadata,
     for (unsigned int i = 0; i < nAntenna; ++i) {
         const std::string name = antc.name().getColumn()(i);
 
+        const int fieldId = msc.fieldId()(itsCurrentRow);
+        const casa::Vector<casa::MDirection> dirVec = fieldc.phaseDirMeasCol()(fieldId);
+        const casa::MDirection direction = dirVec(0);
+
         {
             // <antenna name>.dish_pointing
             Direction dishPointing;
-            dishPointing.coord1 = 0.0; // TODO
-            dishPointing.coord2 = 0.0; // TODO
+            dishPointing.coord1 = direction.getAngle().getValue()(0);
+            dishPointing.coord2 = direction.getAngle().getValue()(1);
             dishPointing.sys = J2000;
             metadata.data[makeMapKey(name, "dish_pointing")] = new TypedValueDirection(TypeDirection, dishPointing);
         }
 
         {
             // <antenna name>.frequency
-            metadata.data[makeMapKey(name, "frequency")] = new TypedValueDouble(TypeDouble, 0.0); // TODO
+
+            // FIXME: This is a mess and is wrong, but the CP will likely ignore this
+            // metadata anyway. The CP should get the correlator setup from the
+            // scheduling block.
+            casa::Vector<double> freqs = spwc.chanFreq()(descSpwId);
+            const double baseFreq = std::min(freqs(0), freqs(freqs.size() - 1));
+            const casa::Vector<double> chanWidth = spwc.chanWidth()(descSpwId);
+            const double freqInc = chanWidth(0);
+
+            const double centreFreq = baseFreq + (freqInc * (16416 / 2));
+
+            metadata.data[makeMapKey(name, "frequency")] = new TypedValueDouble(TypeDouble, centreFreq);
         }
 
         {
@@ -184,38 +207,48 @@ bool MSReader::fillNext(askap::interfaces::TimeTaggedTypedValueMap& metadata,
             // <antenna name>.phase_tracking_centre
             DirectionSeq ptc;
             ptc.resize(nBeam * nCoarseChan);
-            // TODO
+            for (unsigned int i = 0; i < ptc.size(); ++i) {
+                ptc[i].coord1 = direction.getAngle().getValue()(0);
+                ptc[i].coord2 = direction.getAngle().getValue()(1);
+            }
             metadata.data[makeMapKey(name, "phase_tracking_centre")] = new TypedValueDirectionSeq(TypeDirectionSeq, ptc);
         }
 
         {
             // <antenna name>.parallactic_angle
-            metadata.data[makeMapKey(name, "parallactic_angle")] = new TypedValueDouble(TypeDouble, 0.0); // TODO
+            casa::Double parAngle = 0.0; // TODO
+            metadata.data[makeMapKey(name, "parallactic_angle")] = new TypedValueDouble(TypeDouble, parAngle);
         }
 
         {
             // <antenna name>.flag.on_source
-            metadata.data[makeMapKey(name, "flag.on_source")] = new TypedValueBool(TypeBool, true); // TODO
+            // TODO: Current no flagging, but it would be good to read this from the 
+            // actual measurement set
+            metadata.data[makeMapKey(name, "flag.on_source")] = new TypedValueBool(TypeBool, true);
         }
 
         {
             // <antenna name>.flag.hw_error
-            metadata.data[makeMapKey(name, "flag.hw_error")] = new TypedValueBool(TypeBool, true); // TODO
+            // TODO: Current no flagging, but it would be good to read this from the 
+            // actual measurement set
+            metadata.data[makeMapKey(name, "flag.hw_error")] = new TypedValueBool(TypeBool, true);
         }
 
         {
             // <antenna name>.flag.detailed
             BoolSeq flag;
-            flag.resize(nBeam * nCoarseChan * nCorr);
-            // TODO
+            // TODO: Current no flagging, but it would be good to read this from the 
+            // actual measurement set
+            flag.assign(nBeam * nCoarseChan * nCorr, false);
             metadata.data[makeMapKey(name, "flag.detailed")] = new TypedValueBoolSeq(TypeBoolSeq, flag);
         }
 
         {
             // <antenna name>.system_temp
             FloatSeq systemTemp;
-            systemTemp.resize(nBeam * nCoarseChan * nCorr);
-            // TODO
+            // TODO: Current no system temperature, but it would be good to read this
+            // from the actual measurement set
+            systemTemp.assign(nBeam * nCoarseChan * nCorr, 0.0);
             metadata.data[makeMapKey(name, "system_temp")] = new TypedValueFloatSeq(TypeFloatSeq, systemTemp);
         }
     }
@@ -227,11 +260,45 @@ bool MSReader::fillNext(askap::interfaces::TimeTaggedTypedValueMap& metadata,
     // Process rows until none are left or the timestamp
     // changes, indicating the end of this integration
     while (itsCurrentRow != nRow && (currentIntegration == msc.time()(itsCurrentRow))) {
+
+        // Some constraints
         // This code needs the dataDescId to remain constant for all rows
         // in the integration being processed
         ASKAPCHECK(msc.dataDescId()(itsCurrentRow) == dataDescId,
                 "Data description ID must remain constant for a given integration");
+
+        ASKAPCHECK(nChan == n_fine_per_coarse,
+                "Number of channels in this spectral window should be "
+                << n_fine_per_coarse);
+
+        // Populate the VisPayload
         VisPayload payload;
+        const long timestamp =
+            static_cast<long>(msc.time()(itsCurrentRow) * 1000 * 1000);
+        payload.timestamp = timestamp;
+        payload.coarseChannel = 0;
+        payload.antenna1 = msc.antenna1()(itsCurrentRow);
+        payload.antenna2 = msc.antenna2()(itsCurrentRow);
+        payload.beam1 = msc.feed1()(itsCurrentRow);
+        payload.beam2 = msc.feed2()(itsCurrentRow);
+
+        // Set all nSamples to 1 and ensure nominalNSamples is also 1
+        for (unsigned int i = 0; i < n_fine_per_coarse * n_pol; ++i) {
+            payload.nSamples[i] = 1;
+        }
+
+
+        // This matrix is: Matrix<Complex> data(nCorr, nChan)
+        const casa::Matrix<casa::Complex> data = msc.data()(itsCurrentRow);
+        for (unsigned int chan = 0; chan < n_fine_per_coarse; ++chan) {
+            for (unsigned int pol = 0; pol < n_pol; ++pol) {
+                const int idx = chan + (n_fine_per_coarse * pol);
+                payload.vis[idx].real = data(pol, chan).real();
+                payload.vis[idx].imag = data(pol, chan).imag();
+            }
+        }
+
+        // Finished populating
         visVec.push_back(payload);
         itsCurrentRow++;
     }
