f0b2cfd118c8a1b72c01d0d014f130da3e38110c
Maxim Voronkov <maxim.voronkov@csiro.au>
2010-01-13 06:54:56 +0000

    testing trivial way to compare gains
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2582 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/apps/cmpgains.cc
+++ Code/Components/Synthesis/synthesis/trunk/apps/cmpgains.cc
@@ -26,20 +26,49 @@
 
 // casa includes
 #include <casa/BasicSL/Complex.h>
+#include <casa/Logging/LogIO.h>
+
+// LOFAR
+#include <Common/ParameterSet.h>
 
 // own includes
+#include <askap_synthesis.h>
 #include <askap/AskapUtil.h>
 #include <askap/AskapError.h>
+#include <askap/AskapLogging.h>
 #include <measurementequation/SynthesisParamsHelper.h>
+#include <fitting/Params.h>
+#include <askap/Log4cxxLogSink.h>
 
 // command line parser
 #include <CommandLineParser.h>
 
+// std includes
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
+ASKAP_LOGGER(logger, ".cmpgains");
+
+template<typename Cont>
+bool exists(const Cont &cnt, const typename Cont::value_type &val) 
+{
+  const typename Cont::const_iterator ci = std::find(cnt.begin(),cnt.end(),val);
+  return  ci != cnt.end();
+}
+
 int main(int argc, char **argv)
 {
    using namespace askap;
+   using namespace askap::synthesis;
 
    try {
+      // Ensure that CASA log messages are captured
+      casa::LogSinkInterface* globalSink = new Log4cxxLogSink();
+      casa::LogSink::globalSink(globalSink);
+        
       cmdlineparser::Parser parser; // a command line parser
       // command line parameters
       cmdlineparser::GenericParameter<std::string> gainsFileName1;
@@ -50,6 +79,27 @@ int main(int argc, char **argv)
       parser.add(gainsFileName2);
       
       parser.process(argc,argv);
+      
+      ASKAPLOG_INFO_STR(logger,"Loading gains from file "<<gainsFileName1.getValue());
+      scimath::Params gains1;
+      gains1 << LOFAR::ParameterSet(gainsFileName1.getValue());
+      ASKAPLOG_INFO_STR(logger,"Loading gains from file "<<gainsFileName1.getValue());
+      scimath::Params gains2;
+      gains2 << LOFAR::ParameterSet(gainsFileName2.getValue());
+      
+      // build a union of all parameters
+      const std::vector<std::string> names1 = gains1.names();
+      const std::vector<std::string> names2 = gains2.names();
+      std::set<std::string> allnames;
+      std::set_union(names1.begin(),names1.end(),names2.begin(),names2.end(),std::inserter(allnames,allnames.begin()));
+      std::ofstream os("out.dat");
+      for (std::set<std::string>::const_iterator ci = allnames.begin(); ci!=allnames.end(); ++ci) {
+           ASKAPCHECK(exists(names1,*ci), "Gain parameter "<<*ci<<" is missing in the first parameter set");
+           ASKAPCHECK(exists(names2,*ci), "Gain parameter "<<*ci<<" is missing in the second parameter set");
+           const casa::Complex g1 = gains1.complexValue(*ci);
+           const casa::Complex g2 = gains2.complexValue(*ci);           
+           os<<*ci<<" "<<abs(g1)<<" "<<abs(g2)<<std::endl;
+      }
    }
    catch (const cmdlineparser::XParser &ex) {
       std::cerr<<"Usage: "<<argv[0]<<" gains1.par gains2.par"<<std::endl;
