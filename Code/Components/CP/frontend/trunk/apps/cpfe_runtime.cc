e2e54b8e24632c03817d547cd9e931345b62c7d3
Ben Humphreys <ben.humphreys@csiro.au>
2010-01-15 03:25:00 +0000

    Further to changeset:9370, I have cleaned out the frontend trunk directory.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2593 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/frontend/trunk/apps/cpfe_runtime.cc
+++ Code/Components/CP/frontend/trunk/apps/cpfe_runtime.cc
@@ -28,94 +28,14 @@
 #include "askap_cpfrontend.h"
 
 // System includes
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <stdexcept>
 
 // ASKAPsoft includes
-#include "askap/AskapLogging.h"
-#include "askap/AskapError.h"
-#include "askap/Log4cxxLogSink.h"
-#include "casa/Logging/LogIO.h"
-#include "casa/Logging/LogSinkInterface.h"
-#include "Ice/Ice.h"
 
 // Local package includes
-#include "runtime/Runtime.h"
 
 // Using
-using namespace askap;
-using askap::cp::Runtime;
-
-ASKAP_LOGGER(logger, ".main");
-
-static std::string getNodeName(void)
-{
-    const int HOST_NAME_MAXLEN = 256;
-    char name[HOST_NAME_MAXLEN];
-    gethostname(name, HOST_NAME_MAXLEN);
-    std::string nodename(name);
-
-    std::string::size_type idx = nodename.find_first_of('.');
-    if (idx != std::string::npos) {
-        // Extract just the hostname part
-        nodename = nodename.substr(0, idx);
-    }
-
-    return nodename;
-}
 
 int main(int argc, char *argv[])
 {
-    // Now we have to initialize the logger before we use it
-    // If a log configuration exists in the current directory then
-    // use it, otherwise try to use the programs default one
-    std::ifstream config("askap.log_cfg", std::ifstream::in);
-    if (config) {
-        ASKAPLOG_INIT("askap.log_cfg");
-    } else {
-        std::ostringstream ss;
-        ss << argv[0] << ".log_cfg";
-        ASKAPLOG_INIT(ss.str().c_str());
-    }
-
-    std::string hostname = getNodeName();
-    ASKAPLOG_REMOVECONTEXT("hostname");
-    ASKAPLOG_PUTCONTEXT("hostname", hostname.c_str());
-
-    // Ensure that CASA log messages are captured
-    casa::LogSinkInterface* globalSink = new Log4cxxLogSink();
-    casa::LogSink::globalSink(globalSink);
-
-    // ### Logging is now setup, can use logger beyond this point ###
-
-    ASKAPLOG_INFO_STR(logger, "ASKAP Central Processor Frontend Runtime - " << ASKAP_PACKAGE_VERSION);
-
-
-    // Initialise and start the runtime, run() blocks until
-    // the runtime is shutdown (via its ICE interface)
-    try {
-        Ice::CommunicatorPtr ic = Ice::initialize(argc, argv);
-        Runtime rt(ic);
-        rt.run();
-    } catch (const Ice::Exception& e) {
-        ASKAPLOG_FATAL_STR(logger, "Ice exception in: " << argv[0] << ": " << e);
-        std::cerr << "Ice exception in " << argv[0] << ": " << e << std::endl;
-        return 1;
-    } catch (const askap::AskapError& e) {
-        ASKAPLOG_FATAL_STR(logger, "Askap error in " << argv[0] << ": " << e.what());
-        std::cerr << "Askap error in " << argv[0] << ": " << e.what() << std::endl;
-        return 1;
-    } catch (const std::runtime_error& e) {
-        ASKAPLOG_FATAL_STR(logger, "runtime_error: " << e.what());
-        std::cerr << "runtime_error: " << e.what() << std::endl;
-        return 1;
-    } catch (const char* msg) {
-        ASKAPLOG_FATAL_STR(logger, "Error: " << msg);
-        std::cerr << "Error: " << msg << std::endl;
-        return 1;
-    }
-
     return 0;
 }
--- Code/Components/CP/frontend/trunk/functests/run.sh
+++ Code/Components/CP/frontend/trunk/functests/run.sh
@@ -16,47 +16,11 @@ else
 fi
 cd $INITIALDIR
 
-# test_activities testcase
-cd test_activities
-./run.sh
-if [ $? -eq 0 ]; then
-    R2="test-activities  PASS"
-else
-    R2="test-acivities  FAIL"
-    FAIL=1
-fi
-cd $INITIALDIR
-
-# Simple testcase
-cd control
-./run.sh
-if [ $? -eq 0 ]; then
-    R3="control      PASS"
-else
-    R3="control      FAIL"
-    FAIL=1
-fi
-cd $INITIALDIR
-
-# Simple testcase
-cd config
-./run.sh
-if [ $? -eq 0 ]; then
-    R4="config      PASS"
-else
-    R4="config      FAIL"
-    FAIL=1
-fi
-cd $INITIALDIR
-
 # Print Results
 echo
 echo Result Summary:
 echo ===============
 echo $R1
-echo $R2
-echo $R3
-echo $R4
 
 if [ $FAIL -eq 0 ]; then
     exit 0
