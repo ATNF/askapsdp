2b7852254a2ab9b7c9d516478a52ee304f9ec5cc
Matthew Whiting <matthew.whiting@csiro.au>
2010-01-15 03:46:53 +0000

    Added an extra feature to cubeslice. The former usage now requires a -c in front of the start_chan value. The alternative is to provide a subsection string with the -s flag. This string is of the format [1:100,1:100,*,*], where the full axis range is given by * and a subsection of it is given as firstpixel:lastpixel. The usage message has been expanded as well.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2595 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/trunk/apps/cubeslice.cc
+++ Code/Components/Synthesis/synthesis/trunk/apps/cubeslice.cc
@@ -12,6 +12,8 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace askap;
 using namespace std;
@@ -24,14 +26,18 @@ int main(int argc, const char** argv) {
      cmdlineparser::FlaggedParameter<int> nchan("-n",1);
      cmdlineparser::GenericParameter<std::string> imgfile;
      cmdlineparser::GenericParameter<std::string> outfile;
-	 cmdlineparser::GenericParameter<int> startchan;
-     
+// 	 cmdlineparser::GenericParameter<int> startchan;
+     cmdlineparser::FlaggedParameter<int> startchan("-c",-1);
+     cmdlineparser::FlaggedParameter<std::string> subsection("-s","[]");
+	 
      parser.add(nchan,cmdlineparser::Parser::return_default); // optional
-     parser.add(startchan);
-	 parser.add(imgfile);
-	 parser.add(outfile);
-	
-	 parser.process(argc, argv);
+     parser.add(startchan,cmdlineparser::Parser::return_default);
+     parser.add(subsection,cmdlineparser::Parser::return_default);
+     parser.add(imgfile);
+     parser.add(outfile);
+     
+     
+     parser.process(argc, argv);
      casa::PagedImage<casa::Float> img(imgfile.getValue());
      ASKAPCHECK(img.ok(),"Error loading "<<imgfile.getValue());
      ASKAPCHECK(img.shape().nelements()>=3,"Work with at least 3D cubes!");
@@ -39,12 +45,45 @@ int main(int argc, const char** argv) {
      const casa::IPosition shape = img.shape();
      
      casa::IPosition blc(shape.nelements(),0);
-     blc[3] = startchan;
      casa::IPosition trc(shape);
-     trc[3] = nchan;
-     ASKAPCHECK(blc[3]>=0 && blc[3]<shape[3], "Start channel is outside the number of channels or negative, shape: "<<shape);
-     ASKAPCHECK(trc[3]+blc[3]<=shape[3], "Subcube extends beyond the original cube, shape:"<<shape);
+
+     std::string section = subsection.getValue();
+
+     if(startchan>=0){
+
+       blc[3] = startchan;
+       trc[3] = nchan;
+       ASKAPCHECK(blc[3]>=0 && blc[3]<shape[3], "Start channel is outside the number of channels or negative, shape: "<<shape);
+       ASKAPCHECK(trc[3]+blc[3]<=shape[3], "Subcube extends beyond the original cube, shape:"<<shape);
      
+     }
+     else if(section!="[]"){
+       
+       std::vector<std::string> ranges(shape.nelements()); 
+       std::string temp;
+       std::stringstream ss;
+       ss.str(section);
+       getline(ss,temp,'[');
+       for(size_t i=0;i<shape.nelements()-1;i++){
+	 getline(ss,ranges[i],',');
+       }
+       getline(ss,ranges[shape.nelements()-1],']');
+
+       for(size_t i=0;i<shape.nelements();i++){
+	 if(ranges[i] != "*"){
+	   // only need to change from default for the case of a:b
+	   blc[i] = atoi( ranges[i].substr(0, ranges[i].find(":")).c_str() );
+	   trc[i] = atoi( ranges[i].substr(ranges[i].find(":")+1).c_str() ) - blc[i] + 1;
+	 }
+       }
+
+     }
+     else{
+       throw cmdlineparser::XParser();
+     }
+
+     std::cerr << "blc  = " << blc << ", trc = " << trc << "\n";
+
      casa::Slicer slc(blc,trc,casa::IPosition(shape.nelements(),1));
      
      casa::SubImage<casa::Float> si = casa::SubImage<casa::Float>(img,slc,casa::AxesSpecifier(casa::True));
@@ -53,8 +92,10 @@ int main(int argc, const char** argv) {
   }
   ///==============================================================================
   catch (const cmdlineparser::XParser &ex) {
-	 std::cerr << "Usage: " << argv[0] << " [-n number_of_chan] start_chan input_cube output_image"
-			<< std::endl;
+	 std::cerr << "Usage: " << argv[0] << " [-n number_of_chan] [-c start_chan] [-s subsection_string] input_cube output_image"
+			<< std::endl
+		   << "       One of -c or -s must be used" << std::endl
+		   << "       Format for subsection_string is [*,100:200,*,*], where * means entire axis, and range is minimum to maximum pixel value" << std::endl;
   }
 
   catch (const askap::AskapError& x) {
