28e5d87f517ea862d239a315918149523f4bb387
Ben Humphreys <ben.humphreys@csiro.au>
2010-01-15 03:35:32 +0000

    Fixed up empty frontend package so it can build, and added it back to setup.py
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2594 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/setup.py
+++ Code/Components/CP/setup.py
@@ -7,7 +7,7 @@ for pkg, msg in nobuild.iteritems():
 
 build(['common/trunk/build.py',
        'imager/trunk/build.py',
-       #'frontend/trunk/build.py',
+       'frontend/trunk/build.py',
        'manager/trunk/build.py',
        'correlatorsim/trunk/build.py',
       ])
