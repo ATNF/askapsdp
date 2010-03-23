6df830f4f9c3c57bce9868ce65a530aceb44c286
Maxim Voronkov <maxim.voronkov@csiro.au>
2010-03-23 04:03:52 +0000

    improved test with access to the image statistics
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2711 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/synthregression/wtermtest.py
+++ Code/Components/Synthesis/testdata/trunk/simulation/synthregression/wtermtest.py
@@ -26,11 +26,16 @@ class SynthesisProgramRunner:
          os.environ['AIPSPATH'] = os.path.join(os.environ['ASKAP_ROOT'],'Code/Components/Synthesis/testdata/trunk')
       self.simulator = os.path.join(os.environ['ASKAP_ROOT'],'Code/Components/Synthesis/synthesis/trunk/install/bin/csimulator.sh')
       self.imager = os.path.join(os.environ['ASKAP_ROOT'],'Code/Components/Synthesis/synthesis/trunk/install/bin/cimager.sh')
+      self.imgstat = os.path.join(os.environ['ASKAP_ROOT'],'Code/Components/Synthesis/synthesis/trunk/install/bin/imgstat.sh')
+
       if not os.path.exists(self.simulator):
           raise RuntimeError, "csimulator is missing at %s" % self.simulator
 
       if not os.path.exists(self.imager):
           raise RuntimeError, "cimager is missing at %s" % self.imager
+
+      if not os.path.exists(self.imgstat):
+          raise RuntimeError, "imgstat is missing at %s" % self.imgstat
 
       self.tmp_parset = "temp_parset.in"
       if os.path.exists(self.tmp_parset):
@@ -61,6 +66,48 @@ class SynthesisProgramRunner:
       '''
       self.runCommand(self.imager)
 
+   def imageStats(self, name):
+      '''
+         Get image statistics
+
+         name - image name
+      '''
+      if not os.path.exists(name):
+         raise RuntimeError, "Image %s doesn't exist" % name
+      imgstat_out = ".tmp.imgstat"
+      if os.path.exists(imgstat_out):
+         os.system("rm -f %s" % imgstat_out)
+      res = os.system("%s %s > %s" % (self.imgstat,name,imgstat_out))
+      if res != 0:
+         raise RuntimeError, "Command %s failed with error %s" % (self.imgstat,res)
+      result = {}
+      f = file(imgstat_out)
+      try:
+         row = 0
+         for line in f:
+            parts = line.split()
+            if len(parts)<2 and row>0:
+                   raise RuntimeError, "Expected at least 2 elements in row %i, you have: %s" % (row+1,parts)
+            if row == 0:
+               if len(parts)<4:
+                  raise RuntimeError, "Expected at least 4 columns on the first row, you have: %s " % (parts,)
+               result['peak'] = float(parts[0])
+               if parts[3] != "(J2000)":
+                   raise RuntimeError, "Expected J2000 as the 4th element, you have: %s " % (parts,) 
+            elif row == 1:
+               result['ra'] = float(parts[0])
+               result['dec'] = float(parts[1])
+            elif row == 2:
+               result['rms'] = float(parts[0])
+               result['median'] = float(parts[1])
+            row = row + 1
+      finally:
+         f.close()
+      return result
+      
+
 spr = SynthesisProgramRunner()
 spr.runSimulator()
 spr.runImager()
+print spr.imageStats('image.field1.restored')
+print spr.imageStats('image.field1')
