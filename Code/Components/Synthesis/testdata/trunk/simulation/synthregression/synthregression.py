5acc9aabb3b3593522f81dc228d827a0bc6830d7
Maxim Voronkov <maxim.voronkov@csiro.au>
2010-03-30 08:24:37 +0000

    bugfix: fixed python syntax
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2734 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/synthregression/synthregression.py
+++ Code/Components/Synthesis/testdata/trunk/simulation/synthregression/synthregression.py
@@ -1,4 +1,4 @@
 print "wtermtest: WProject and WStack gridders"
-import wtermtest.py
+import wtermtest
 print "pbcorrtest: single field with mosaicing gridders (primary beam correction)"
-import pbcorrtest.py
+import pbcorrtest
