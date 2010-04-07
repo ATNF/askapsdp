4475c4f7a26027f909a8f204f41e82f4afe8ffe8
Ben Humphreys <ben.humphreys@csiro.au>
2010-04-07 07:42:02 +0000

    Added testcase for CircularBuffer
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2757 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/CP/ingest/trunk/tests/ingestpipeline/CircularBufferTest.h
@@ -0,0 +1,95 @@
+/// @file CircularBufferTest.cc
+///
+/// @copyright (c) 2010 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Ben Humphreys <ben.humphreys@csiro.au>
+
+// CPPUnit includes
+#include <cppunit/extensions/HelperMacros.h>
+
+// Support classes
+#include "boost/shared_ptr.hpp"
+
+// Classes to test
+#include "ingestpipeline/sourcetask/CircularBuffer.h"
+
+namespace askap
+{
+    namespace cp
+    {
+        class CircularBufferTest : public CppUnit::TestFixture
+        {
+            CPPUNIT_TEST_SUITE(CircularBufferTest);
+            CPPUNIT_TEST(testSingle);
+            CPPUNIT_TEST(testMultiple);
+            CPPUNIT_TEST(testOverflow);
+            CPPUNIT_TEST_SUITE_END();
+
+            public:
+            // Test the addition and retrieval of a single pointer
+            void testSingle()
+            {
+                CircularBuffer<int> instance(2);
+                boost::shared_ptr<int> inPtr(new int);
+                *inPtr = 1;
+                instance.add(inPtr);
+
+                boost::shared_ptr<int> outPtr(instance.next());
+                CPPUNIT_ASSERT_EQUAL(*inPtr, *outPtr);
+            };
+
+            // Test the addition and retrieval of a large number of pointers
+            void testMultiple()
+            {
+                const int count = 1024 * 1024 * 10;
+                CircularBuffer<int> instance(count);
+                for (int i = 0; i < count; ++i) {
+                    boost::shared_ptr<int> inPtr(new int);
+                    *inPtr = count;
+                    instance.add(inPtr);
+                }
+
+                for (int i = 0; i < count; ++i) {
+                    boost::shared_ptr<int> outPtr(instance.next());
+                    CPPUNIT_ASSERT_EQUAL(count, *outPtr);
+                }
+            };
+
+            // Test the addition of more pointers than the buffer has capacity
+            // to handle
+            void testOverflow()
+            {
+                const int count = 1024;
+                CircularBuffer<int> instance(10);
+                for (int i = 0; i < count; ++i) {
+                    boost::shared_ptr<int> inPtr(new int);
+                    *inPtr = count;
+                    instance.add(inPtr);
+                }
+            };
+
+        };
+
+    }   // End namespace cp
+
+}   // End namespace askap
