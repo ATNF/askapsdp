74479dd1cf3f2c6bd37b78216478b1f38ae5e864
Maxim Voronkov <maxim.voronkov@csiro.au>
2010-04-27 06:17:38 +0000

    plugged the new test into the batch job regularly run in Hudson
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@2823 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/trunk/simulation/synthregression/synthregression.py
+++ Code/Components/Synthesis/testdata/trunk/simulation/synthregression/synthregression.py
@@ -4,3 +4,5 @@ print "pbcorrtest: single field with mosaicing gridders (primary beam correction
 import pbcorrtest
 print "noisetest: testing that the noise in the output image is as expected"
 import noisetest
+print "facetingtest: test of faceted imaging with spherical function gridder"
+import facetingtest
