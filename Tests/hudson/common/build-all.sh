5f2fa07057925ad1b3b1cb2ba571fd238d8fd623
Tony Maher <tony.maher@csiro.au>
2010-06-23 07:46:53 +0000

    See Issue #2647.  Make the common build script more configurable.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3071 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/hudson/common/build-all.sh
+++ Tests/hudson/common/build-all.sh
@@ -1,33 +1,96 @@
 #!/bin/bash -l
 
-# Bootstrap.
-unset ASKAP_ROOT
-cd $WORKSPACE/trunk
-/usr/bin/python2.6 bootstrap.py
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-# Setup ASKAP environment.
-source initaskap.sh
+ECH0='' # Can be overidden by -n option.
+ARGS='-x'
 
-# Build 3rdParty.
-cd $ASKAP_ROOT/3rdParty
-python autobuild.py -q -x install
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+PNAME=`basename $0`
+SDATE=`date`
+FDATE=`date +"%y%m%d"`
 
-# Build Code.
-cd $ASKAP_ROOT/Code
-python autobuild.py -q -x install
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+function HelpMessage
+{
+    printf "\n${PNAME} [-hnq] [<targets>]\n"
+    printf "\t-h\thelp\n"
+    printf "\t-n\tno execute (debugging)\n"
+    printf "\t-q\tbuild Code quietly\n"
+    printf "\t<targets> whitespace separated list of ASKAPsoft Code targets\n"
+    printf "\t\te.g. doc, test, functest\n"
+    printf "\n"
+}
 
-# Build documentation in Code.
-cd $ASKAP_ROOT/Code
-python autobuild.py -q doc
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+
+function bootstrap
+{
+    ${ECHO} unset ASKAP_ROOT
+    ${ECHO} cd $WORKSPACE/trunk
+    ${ECHO} /usr/bin/python2.6 bootstrap.py
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+
+function build
+{
+    directory=$1
+    args=$2
+    target=$3
+    ${ECHO} cd ${ASKAP_ROOT}/${directory}
+    ${ECHO} python autobuild.py ${args} ${target}
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+
+#
+# Get options
+#
+
+
+while getopts hnq OPT
+do
+    case $OPT in
+        h)  HelpMessage
+            ;;
+        n)  ECHO="echo"
+            WORKSPACE="WORKSPACE"
+            ASKAP_ROOT="ASKAP_ROOT"
+            ;;
+        q)  ARGS+=" -q"
+            ;;
+        ?)  HelpMessage
+            exit 1
+    esac
+done
+
+shift $(($OPTIND - 1))
+TARGETS=$@
+NENV=$#
+
+#
+# Main
+#
+
+# Basic build
+printf "==> Running ${PNAME}\n"
+printf "==> Starting standard build at ${SDATE}\n\n"
+bootstrap
+${ECHO} source initaskap.sh # Setup ASKAP environment.
+build "3rdParty" "-x -q" "install"  # Always be quiet and fail immediately.
+build "Code" "${ARGS}" "install"
+DATE=`date`
+printf "\n==> Finished standard build at ${DATE}\n"
+
+# Additional targets
+for target in ${TARGETS};
+do
+    DATE=`date`
+    printf "\n==> Starting ${target} target at ${DATE}\n\n"
+    build "Code" "-q" ${target}
+done
+
+DATE=`date`
+printf "\n==> Finished ${PNAME} at ${DATE}\n"
