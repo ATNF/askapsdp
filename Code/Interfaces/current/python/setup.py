3776d5123eeab8875d0006848e468da609373b23
Malte Marquarding <malte.marquarding@csiro.au>
2010-06-23 03:18:33 +0000

    Ticket #2648: Created python slice package named askap.slice
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3065 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Interfaces/current/python/setup.py
@@ -0,0 +1,29 @@
+from askapdev.rbuild import setup
+from askapdev.rbuild.dependencies import Dependency
+from setuptools import find_packages
+import glob
+
+dep = Dependency()
+dep.add_package()
+
+slice_files = glob.glob('../slice/*.ice')
+
+ROOTPKG = "askap"
+PKGNAME = "slice"
+
+setup(name = "%s.%s" % (ROOTPKG, PKGNAME),
+      version = 'current',
+      description = 'ASKAP package for slice2py generated code.',
+      author = 'Malte Marquarding',
+      author_email = 'Malte.Marquarding@csiro.au',
+      url = 'http://svn.atnf.csiro.au/askap',
+      keywords = ['ASKAP', 'Interfaces', 'Ice'],
+      long_description = '',
+      packages = find_packages(),
+      namespace_packages = [ROOTPKG],
+      license = 'GPL',
+      zip_safe = 0,
+      dependency = dep,
+      ice_interfaces = {"askap.slice": slice_files },
+      #test_suite = "nose.collector",
+)
