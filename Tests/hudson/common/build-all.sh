b6197a39e06d9dfe68f662e7d6e2c2e4197c27a6
Tony Maher <tony.maher@csiro.au>
2010-06-23 05:10:07 +0000

    Revert back to the original version.  Forgot I had local edits when I moved this file in r10282.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3069 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/hudson/common/build-all.sh
+++ Tests/hudson/common/build-all.sh
@@ -1,56 +1,33 @@
 #!/bin/bash -l
 
-function bootstrap
-{
-    unset ASKAP_ROOT
-    cd $WORKSPACE/trunk
-    /usr/bin/python2.6 bootstrap.py
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
+# Bootstrap.
+unset ASKAP_ROOT
+cd $WORKSPACE/trunk
+/usr/bin/python2.6 bootstrap.py
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
+# Setup ASKAP environment.
+source initaskap.sh
 
-function build_3rdParty
-{
-    cd $ASKAP_ROOT/3rdParty
-    python autobuild.py -q -x install
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
+# Build 3rdParty.
+cd $ASKAP_ROOT/3rdParty
+python autobuild.py -q -x install
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-function build_Code
-{
-    cd $ASKAP_ROOT/Code
-    python autobuild.py -q -x install
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
+# Build Code.
+cd $ASKAP_ROOT/Code
+python autobuild.py -q -x install
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-
-function build_Code_doc
-{
-    cd $ASKAP_ROOT/Code
-    python autobuild.py -q doc
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
-
-# Run all tests in Code - should not need exit status
-function test_Code
-{
-    cd $ASKAP_ROOT/Code
-    python autobuild.py -q test
-}
-
-#
-# main
-#
-
-bootstrap
-source initaskap.sh # Setup ASKAP environment.
-build_3rdParty
-build_Code
+# Build documentation in Code.
+cd $ASKAP_ROOT/Code
+python autobuild.py -q doc
+if [ $? -ne 0 ]; then
+    exit 1
+fi
