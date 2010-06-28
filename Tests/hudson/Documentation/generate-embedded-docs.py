1ae570ae6aa1e34753a69910ba9c38a499035047
Tony Maher <tony.maher@csiro.au>
2010-06-28 01:40:59 +0000

    Catch up with Maltes interface rearrangement (#2648) yet again! ;-)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3091 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/hudson/Documentation/generate-embedded-docs.py
+++ Tests/hudson/Documentation/generate-embedded-docs.py
@@ -187,4 +187,4 @@ CopyDocs("javadocs", "current/doc", "stylesheet.css")
 print "info: Sphinx"
 CopyDocs("sphinx", "current/doc/_build/html", "objects.inv")
 print "info: Ice"
-CopyDocs("ice", "current/slice/doc", "_sindex.html")
+CopyDocs("ice", "slice/current/doc", "_sindex.html")
