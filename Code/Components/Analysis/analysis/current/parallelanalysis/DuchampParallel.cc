9781953b8e293aa5db05a6dbb52a65bcf7e2435c
Matthew Whiting <matthew.whiting@csiro.au>
2010-07-07 01:01:28 +0000

    Fix a bug and add a flag I missed
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3133 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/analysis/current/parallelanalysis/DuchampParallel.cc
+++ Code/Components/Analysis/analysis/current/parallelanalysis/DuchampParallel.cc
@@ -988,7 +988,7 @@ namespace askap {
                 ASKAPLOG_INFO_STR(logger, this->workerPrefix() << "Finished adding sources to cube. Now have " << this->itsCube.getNumObj() << " objects.");
 
             }
-	    else if(this->isWorker()) this->fitRemaining();
+	    else if(this->isWorker() && this->itsFlagDistribFit) this->fitRemaining();
         }
 
         //**************************************************************//
@@ -1065,7 +1065,7 @@ namespace askap {
 	    LOFAR::BlobOStream out(bob);
 	    out.putStart("fitsrc", 1);
 	    sourcefitting::RadioSource nullsrc;
-	    out << false << nullsrc;
+	    out << false << nullsrc << 0;
 	    out.putEnd();
 	    this->itsConnectionSet->writeAll(bs);
 	  }
