9c6a5de2577504657f58e400eb82c616c3357bb3
Tony Maher <tony.maher@csiro.au>
2010-07-30 05:34:42 +0000

    More packages that cannot handle doc target and added a couple NO_BUILDS in Analysis en2end packages as discussed with Matt.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3206 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/data/current/build.py
+++ Code/Components/Analysis/data/current/build.py
@@ -1,4 +1,10 @@
+import sys
+
 from askapdev.rbuild.builders import Setuptools as Builder
+
+if 'doc' in sys.argv:
+    print 'warn: unable to handle doc target'
+    sys.exit(0)
 
 builder = Builder(".")
 builder.build()
