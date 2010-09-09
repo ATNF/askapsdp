2a32ab12060208fc2465df9efc2525db665f6352
Matthew Whiting <matthew.whiting@csiro.au>
2010-09-09 05:17:46 +0000

    Trying to fix sourcefitting test - not quite there, but recording changes made.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3276 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/analysis/current/tests/sourcefitting/RadioSourceTests.h
+++ Code/Components/Analysis/analysis/current/tests/sourcefitting/RadioSourceTests.h
@@ -27,6 +27,7 @@
 /// @author Matthew Whiting <matthew.whiting@csiro.au>
 ///
 #include <askap_analysis.h>
+#include <cppunit/extensions/HelperMacros.h>
 
 #include <askap/AskapLogging.h>
 #include <askap/AskapError.h>
@@ -41,6 +42,8 @@
 #include <duchamp/Detection/detection.hh>
 #include <duchamp/Cubes/cubes.hh>
 #include <duchamp/fitsHeader.hh>
+#include <duchamp/PixelMap/Object2D.hh>
+#include <duchamp/Utils/Statistics.hh>
 
 #include <casa/Arrays/Array.h>
 #include <Common/ParameterSet.h>
@@ -56,112 +59,137 @@ namespace askap {
   namespace analysis {
 
     namespace sourcefitting {
-				
+	
+      const int srcDim=8;
+      const int srcSize=srcDim*srcDim;
+      const int arrayDim=10;
+      const int arraySize=arrayDim*arrayDim;
+      const float src[srcSize]={1.,1.,1.,1.,1.,1.,1.,1.,
+				1.,1.,1.,9.,11.,1.,1.,1.,
+				1.,1.,1.,10.,10.,1.,1.,1.,
+				1.,40.,39.,51.,50.,20.,19.,1.,
+				1.,41.,40.,50.,49.,20.,22.,1.,
+				1.,1.,1.,28.,30.,1.,1.,1.,
+				1.,1.,1.,31.,27.,1.,1.,1.,
+				1.,1.,1.,1.,1.,1.,1.,1.};
+      
+
       class RadioSourceTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(RadioSourceTest);
 	CPPUNIT_TEST(findSource);
-	 CPPUNIT_TEST(subthreshold); 
+	CPPUNIT_TEST(subthreshold); 
 	CPPUNIT_TEST_SUITE_END();
 
       private:
 
-	float *array;
-	long *dim;
-	casa::Matrix<casa::Double> itsPos;
-	casa::Vector<casa::Double> itsF;
-	duchamp::Image *image;
-	duchamp::Section itsSection;
-	std::vector<Object2D> objlist;
-	std::vector<SubComponent> sublist;
-	RadioSource itsSource;
-	FittingParameters fitparams;
+	float                      *itsArray;
+	long                       *itsDim;
+	std::vector<PixelInfo::Object2D>      itsObjlist;
+	std::vector<SubComponent>  itsSublist;
+	RadioSource                itsSource;
+	FittingParameters          itsFitparams;
 
       public:
 
 	void setUp() {
+	  
+	  ASKAPLOG_DEBUG_STR(logger, "****setUp****");
+	  
+	  itsArray = new float[arraySize];
+	  for(int i=0;i<arraySize;i++) itsArray[i] = 0.;
+	  
 
-	  const float src[64]={1.,1.,1.,1.,1.,1.,1.,1.,
-			       1.,1.,1.,9.,11.,1.,1.,1.,
-			       1.,1.,1.,10.,10.,1.,1.,1.,
-			       1.,40.,39.,51.,50.,20.,19.,1.,
-			       1.,41.,40.,50.,49.,20.,22.,1.,
-			       1.,1.,1.,28.,30.,1.,1.,1.,
-			       1.,1.,1.,31.,27.,1.,1.,1.,
-			       1.,1.,1.,1.,1.,1.,1.,1.};
-
-	  array = new float[100];
-	  itsPos.resize(100, 2);
-	  itsF.resize(100);
-	  for(int i=0;i<100;i++) array[i] = 0.;
-	  casa::Vector<casa::Double> curpos(2);
-	  curpos = 0;
-	  for(int x=0;x<8;x++) {
-	    for(int y=0;y<8;y++){ 
-	      array[(x+1)+(y+1)*10]=src[x+y*8];
+	  for(int x=0;x<srcDim;x++) {
+	    for(int y=0;y<srcDim;y++){ 
+	      itsArray[(x+1)+(y+1)*arrayDim]=src[x+y*srcDim];
 	    }
 	  }	      
-	  for(int x=0;x<10;x++) {
-	    for(int y=0;y<10;y++){ 
-	      itsF(x+y*10) = array[x+y*10];
-	      curpos(0) = x;
-	      curpos(1) = y;
-	      itsPos.row(x+y*10) = curpos;
-	    }
-	  }
+
+	  std::cerr << itsArray[45] << "\n";
 							
-	  dim = new long;
-	  dim[0] = dim[1] = 10;
-	  image = new duchamp::Image(dim);
-	  image->saveArray(array,100);
-	  image->pars().setThreshold(5);
-	  image->pars().setMinPix(1);
+	  itsDim = new long[2];
+	  itsDim[0] = itsDim[1] = arrayDim;
 
-	  objlist = image->findSources2D();
+	  duchamp::Image *itsImage = new duchamp::Image(itsDim);
+	  itsImage->saveArray(itsArray,arraySize);
+	  std::cerr << itsImage->getSize() << " " << itsImage->getArray()[45] << "\n";
+	  itsImage->pars().setThreshold(5);
+	  itsImage->pars().setFlagUserThreshold(true);
+	  itsImage->stats().setThreshold(5);
+	  itsImage->pars().setMinPix(1);
 
-	  fitparams = FittingParameters(LOFAR::ParameterSet());
-//	  fitparams.setNumSubThresholds(10);
+// 	  itsObjlist = itsImage->findSources2D();
+	  std::vector<PixelInfo::Object2D> newlist = itsImage->findSources2D();
+	  itsObjlist = newlist;
+	  std::cerr << newlist.size() << itsObjlist.size() << itsImage->isDetection(4,5) << "\n";
+// 	  std::cerr << itsImage->pars() << "\n";
+	  ASKAPLOG_DEBUG_STR(logger, "Num objects = " << itsObjlist.size()); 
+	  delete itsImage;
+
+	  itsFitparams = FittingParameters(LOFAR::ParameterSet());
+	  /* 	  fitparams.setNumSubThresholds(10); */
 
 	}
 
 	void tearDown() {
-	  delete image;
-	  delete array;
-	};
-
-	void findSource() {
-	  ASKAPLOG_DEBUG_STR(logger, objlist.size());
-	  CPPUNIT_ASSERT(objlist.size()==1);
+	  ASKAPLOG_DEBUG_STR(logger, "****tearDown****");
+	  /* 	  delete itsImage; */
+	  delete [] itsArray;
+	  delete [] itsDim;
+	  itsObjlist.clear();
 	}
 
-	 void subthreshold() { 
-	   ASKAPLOG_DEBUG_STR(logger,"Defining detection"); 
-	   duchamp::Detection det; 
-	   ASKAPLOG_DEBUG_STR(logger, objlist.size()); 
-	   det.addChannel(0,objlist[0]); 
-		 det.calcFluxes(array,dim);
-		 ASKAPLOG_DEBUG_STR(logger,"Defining radiosource"); 
-	   itsSource = RadioSource(det); 
-	   std::string secstring=duchamp::nullSection(2); 
-	   duchamp::Section sec(secstring); 
-		 sec.parse(dim,2);
-	   ASKAPLOG_DEBUG_STR(logger,"Defining box"); 
-		 itsSource.defineBox(sec,fitparams,2); 
-	   ASKAPLOG_DEBUG_STR(logger,"Done - size is " << itsSource.boxSize()); 
-		 itsSource.setDetectionThreshold(5);
-		 itsSource.setNoiseLevel(1.);
-		 itsSource.setHeader(duchamp::FitsHeader());
-		 itsSource.setFitParams(fitparams);
-		 
-	   ASKAPLOG_DEBUG_STR(logger,"Finding subcomponents"); 
-		 sublist=itsSource.getSubComponentList(itsPos,itsF);	
-		 
-		 ASKAPLOG_DEBUG_STR(logger, "Number of subcomponents = " << sublist.size());
-		 for(size_t i=0;i<sublist.size();i++)
-					//ASKAPLOG_DEBUG_STR(logger, "Component " << i << ": " << sublist[i]);
-			 std::cerr <<  "Component " << i << ": " << sublist[i] << "\n";
-		 
-	   CPPUNIT_ASSERT(sublist.size()==5); 
-	 } 
+	void findSource() {
+	  ASKAPLOG_DEBUG_STR(logger, "Num objects = " << itsObjlist.size());
+	  CPPUNIT_ASSERT(itsObjlist.size()==1);
+	}
+
+	void subthreshold() { 
+	  ASKAPLOG_DEBUG_STR(logger,"Defining detection"); 
+	  /* 	   for(size_t i=0;i<100;i++) std::cerr << itsArray[i] << "\n"; */
+	  ASKAPLOG_DEBUG_STR(logger, "Num objects = " << itsObjlist.size()); 
+	  CPPUNIT_ASSERT(itsObjlist.size()==1);
+	  duchamp::Detection det; 
+	  det.addChannel(0,itsObjlist[0]); 
+	  det.calcFluxes(itsArray,itsDim);
+	  ASKAPLOG_DEBUG_STR(logger,"Defining radiosource"); 
+	  itsSource = RadioSource(det); 
+	  std::string secstring=duchamp::nullSection(2); 
+	  duchamp::Section sec(secstring); 
+	  sec.parse(itsDim,2);
+	  ASKAPLOG_DEBUG_STR(logger,"Defining box"); 
+	  itsSource.defineBox(sec,itsFitparams,2); 
+	  ASKAPLOG_DEBUG_STR(logger,"Done - size is " << itsSource.boxSize()); 
+	  itsSource.setDetectionThreshold(5);
+	  itsSource.setNoiseLevel(1.);
+	  itsSource.setHeader(duchamp::FitsHeader());
+	  itsSource.setFitParams(itsFitparams);
+	   
+	  ASKAPLOG_DEBUG_STR(logger,"Finding subcomponents"); 
+	  casa::Matrix<casa::Double> itsPos;
+	  casa::Vector<casa::Double> itsF;
+	  itsPos.resize(arraySize, 2);
+	  itsF.resize(arraySize);
+	  casa::Vector<casa::Double> curpos(2);
+	  curpos = 0;
+	  for(int x=0;x<arrayDim;x++) {
+	    for(int y=0;y<arrayDim;y++){ 
+	      itsF(x+y*arrayDim) = itsArray[x+y*arrayDim];
+	      curpos(0) = x;
+	      curpos(1) = y;
+	      itsPos.row(x+y*arrayDim) = curpos;
+	    }
+	  }
+	  itsSublist=itsSource.getSubComponentList(itsPos,itsF);	
+	   
+	  ASKAPLOG_DEBUG_STR(logger, "Number of subcomponents = " << itsSublist.size());
+	  for(size_t i=0;i<itsSublist.size();i++){
+	    //ASKAPLOG_DEBUG_STR(logger, "Component " << i << ": " << itsSublist[i]);
+	    std::cerr <<  "Component " << i << ": " << itsSublist[i] << "\n";
+	  }
+   
+	  CPPUNIT_ASSERT(itsSublist.size()==5); 
+	} 
 
       };
 
