bcfad3b4c018042bb63c37fbcce5eb21cd50151c
Malte Marquarding <malte.marquarding@csiro.au>
2010-09-14 04:08:55 +0000

    This package doesn't need documentation
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3288 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Interfaces/python/current/build.py
+++ Code/Interfaces/python/current/build.py
@@ -2,8 +2,8 @@ import sys
 
 from askapdev.rbuild.builders import Setuptools as Builder
 
+# This generated package is not documented. The docs are build as part of ice.
 if 'doc' in sys.argv:
-    print 'warn: unable to handle doc target'
     sys.exit(0)
 
 builder = Builder(".")
