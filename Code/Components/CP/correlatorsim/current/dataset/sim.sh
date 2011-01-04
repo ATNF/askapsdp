202f2989be8c75e29c5fafe183f613f6fac2d73f
Ben Humphreys <ben.humphreys@csiro.au>
2011-01-04 10:01:31 +0000

    Update correlator simulator functest and dataset to reflect a two correlator shelf BETA configuration.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3464 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/correlatorsim/current/dataset/sim.sh
+++ Code/Components/CP/correlatorsim/current/dataset/sim.sh
@@ -7,5 +7,6 @@ tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/current/simulation/mod
 echo >> output.out
 
 echo Begin csimulator `date` >> output.out
-$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator.in | tee -a output.out
+$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator1.in | tee -a output1.out
+$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator2.in | tee -a output2.out
 echo End csimulator `date` >> output.out
--- Code/Components/CP/correlatorsim/current/functests/test_playback/run.sh
+++ Code/Components/CP/correlatorsim/current/functests/test_playback/run.sh
@@ -12,18 +12,22 @@ $ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/msnoop -inputs msnoop.
 MDPID=$!
 
 # Start the visibilities receiver (don't use the script so this script can kill it)
-$ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/vsnoop -v -p 3000 > vsnoop.log 2>&1 &
-VISPID=$!
+$ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/vsnoop -v -p 3000 > vsnoop1.log 2>&1 &
+VISPID1=$!
+
+# And for the second correlator shelf
+$ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/vsnoop -v -p 3001 > vsnoop2.log 2>&1 &
+VISPID2=$!
 
 # Run the test
-mpirun -np 2 $ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/playback.sh -inputs playback.in
+mpirun -np 3 $ASKAP_ROOT/Code/Components/CP/correlatorsim/current/apps/playback.sh -inputs playback.in
 STATUS=$?
 
 # Give the subscriber a moment to get the last messages then exit
 sleep 5
-kill $MDPID $VISPID
+kill $MDPID $VISPID1 $VISPID2
 sleep 1
-kill -9 $MDPID $VISPID > /dev/null 2>&1
+kill -9 $MDPID $VISPID $VISPID2 > /dev/null 2>&1
 
 # Stop the Ice Services
 ../stop_services.sh config.icegrid
