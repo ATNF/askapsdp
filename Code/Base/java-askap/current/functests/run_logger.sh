4cd99b0a85f69ce8fe1fa96f4123d81a596a5af2
David Brodrick <david.brodrick@csiro.au>
2011-01-06 22:21:43 +0000

    Remove obsolete Java logger package.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3470 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/java-askap/current/functests/run_logger.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#Invoke the askap.logger.Logger class so that it generates some log messages
-#using the LoggerIce implementation.
-#
-export CLASSPATH=$ASKAP_ROOT/lib/java-askap.jar:$CLASSPATH
-java -Daskap.logger.configuration=askaplogger-ice-locator-localhost-4061.properties askap.logger.Logger
