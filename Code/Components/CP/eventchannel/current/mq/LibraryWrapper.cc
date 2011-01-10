f48ecaf638a386ec8443643f1ef365e43da9cea2
Ben Humphreys <ben.humphreys@csiro.au>
2011-01-10 06:33:46 +0000

    eventchannel: Add a wrapper to manage library initialisation & shutdown
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3486 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/CP/eventchannel/current/mq/LibraryWrapper.cc
@@ -0,0 +1,52 @@
+/// @file LibraryWrapper.cc
+///
+/// @copyright (c) 2011 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Ben Humphreys <ben.humphreys@csiro.au>
+
+// Include own header file first
+#include "LibraryWrapper.h"
+
+// ASKAPsoft includes
+#include "activemq/library/ActiveMQCPP.h"
+
+// Using
+using namespace askap::cp::eventchannel;
+
+bool LibraryWrapper::theirInitialized = false;
+
+LibraryWrapper::LibraryWrapper() : itsResponsible(false)
+{
+    if (!theirInitialized) {
+        theirInitialized = true;
+        itsResponsible = true;
+        activemq::library::ActiveMQCPP::initializeLibrary();
+    }
+}
+
+LibraryWrapper::~LibraryWrapper()
+{
+    if (theirInitialized && itsResponsible) {
+        activemq::library::ActiveMQCPP::shutdownLibrary();
+    }
+}
