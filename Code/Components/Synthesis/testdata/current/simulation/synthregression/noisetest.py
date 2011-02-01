5ffc92f552ee4749d60a5ed4f01e944401f050be
Maxim Voronkov <maxim.voronkov@csiro.au>
2011-02-01 00:39:57 +0000

    improved the regression test to cover fault condition found in #3179
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3554 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/testdata/current/simulation/synthregression/noisetest.py
+++ Code/Components/Synthesis/testdata/current/simulation/synthregression/noisetest.py
@@ -34,28 +34,47 @@ def analyseResult(spr, expected_rms):
       raise RuntimeError, "Peak sensitivity figure %e Jy should be similar to rms of %e Jy in the sensitivity image (for spheroidal function gridder)" % (stats['peak'],stats['rms'])
 
 
+def doTests(spr, rmsNoise = 1e-3):
+   """
+      Actual imaging followed by tests of the results
+   """
+   # we simulate 270 cycles (9 cuts, 5 min each, 10sec cycle) and 630 baselines
+   # and add gaussian noise with sigma=1e-3 Jy
+   ncycles = 270
+   nbaselines = 630
+   noisePerPol = rmsNoise/math.sqrt(ncycles*nbaselines)
+   print "Image noise per polarisation plane for %i cycles and %i baselines (sigma per vis is %e Jy) is %e Jy" % (ncycles, nbaselines,rmsNoise,noisePerPol)
 
+   spr.addToParset("Cimager.Images.image.field1.polarisation = [\"XX\"]")
+   spr.runImager()
+   analyseResult(spr,noisePerPol)
+
+   # test noise figures after polarisation conversion
+   spr.initParset()
+   spr.addToParset("Cimager.Images.image.field1.polarisation = [\"I\"]")
+   spr.runImager()
+   analyseResult(spr,noisePerPol*math.sqrt(2.))
+
+   spr.initParset()
+   spr.addToParset("Cimager.Images.image.field1.polarisation = [\"Q\"]")
+   spr.runImager()
+   analyseResult(spr,noisePerPol*math.sqrt(2.))
+
+
+
+print "Simulating using explicit definition of the noise level"
 spr = SynthesisProgramRunner(template_parset = 'noisetest_template.in')
+variance = 1e-6
+spr.addToParset("Csimulator.noise.variance = %e" % variance)
 spr.runSimulator()
+doTests(spr, math.sqrt(variance))
 
-# we simulate 270 cycles (9 cuts, 5 min each, 10sec cycle) and 630 baselines
-# and add gaussian noise with sigma=1e-3 Jy
-ncycles = 270
-nbaselines = 630
-noisePerPol = 1e-3/math.sqrt(ncycles*nbaselines)
-print "Image noise per polarisation plane for %i cycles and %i baselines (sigma per vis is 1e-3 Jy) is %e Jy" % (ncycles, nbaselines,noisePerPol)
-
-spr.addToParset("Cimager.Images.image.field1.polarisation = [\"XX\"]")
-spr.runImager()
-analyseResult(spr,noisePerPol)
-
-# test noise figures after polarisation conversion
+print "Simulating uxing Tsys and efficiency"
+Tsys = 30
+efficiency = 0.7
+rmsNoise = 1e26*math.sqrt(2.)*1.38e-23*Tsys/efficiency/(math.pi*144/4.)/math.sqrt(16e6*10)
 spr.initParset()
-spr.addToParset("Cimager.Images.image.field1.polarisation = [\"I\"]")
-spr.runImager()
-analyseResult(spr,noisePerPol*math.sqrt(2.))
-
-spr.initParset()
-spr.addToParset("Cimager.Images.image.field1.polarisation = [\"Q\"]")
-spr.runImager()
-analyseResult(spr,noisePerPol*math.sqrt(2.))
+spr.addToParset("Csimulator.noise.Tsys = %f" % Tsys)
+spr.addToParset("Csimulator.noise.efficiency = %f" % efficiency)
+spr.runSimulator()
+doTests(spr,rmsNoise)
