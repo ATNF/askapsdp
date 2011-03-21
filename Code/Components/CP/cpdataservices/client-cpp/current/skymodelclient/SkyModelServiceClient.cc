b8993a1a80a25e32b2ffe9c1d0e3f4d37692b30e
Ben Humphreys <ben.humphreys@csiro.au>
2011-03-21 08:48:14 +0000

    Sky Model Service: Added skeleton of the C++ client interface. Also changed the name of the config file from hibernate.cfg.xml to skymodel-hibernate.cfg.xml so multiple components can be included in the one JAR file.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3771 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/CP/cpdataservices/client-cpp/current/skymodelclient/SkyModelServiceClient.cc
@@ -0,0 +1,67 @@
+/// @file SkyModelServiceClient.cc
+///
+/// @copyright (c) 2011 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Ben Humphreys <ben.humphreys@csiro.au>
+
+// Include own header file first
+#include "SkyModelServiceClient.h"
+
+// Include package level header file
+#include "askap_cpdataservices.h"
+
+// System includes
+#include <string>
+
+// ASKAPsoft includes
+#include "askap/AskapError.h"
+#include "Ice/Ice.h"
+#include "iceutils/CommunicatorConfig.h"
+#include "iceutils/CommunicatorFactory.h"
+#include "SkyModelService.h" // Ice generated interface
+
+// Using
+using namespace std;
+using namespace askap;
+using namespace askap::cp::skymodelservice;
+
+SkyModelServiceClient::SkyModelServiceClient(const std::string& locatorHost,
+                            const std::string& locatorPort)
+{
+    askap::cp::icewrapper::CommunicatorConfig config(locatorHost, locatorPort);
+    askap::cp::icewrapper::CommunicatorFactory commFactory;
+    itsComm = commFactory.createCommunicator(config);
+
+    ASKAPDEBUGASSERT(itsComm);
+
+    Ice::ObjectPrx base = itsComm->stringToProxy("SkyModelService");
+    itsService = askap::interfaces::skymodelservice::ISkyModelServicePrx::checkedCast(base);
+
+    if (!itsService) {
+        ASKAPTHROW(AskapError, "SkyModelService proxy is invalid");
+    }
+}
+
+SkyModelServiceClient::~SkyModelServiceClient()
+{
+}
