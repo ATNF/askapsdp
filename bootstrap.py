9336933f98122fe36059204c5fa36e0213d84ad1
Tony Maher <tony.maher@csiro.au>
2011-04-13 00:32:35 +0000

    Update the ASKAPsoft bootstrap process. * Python versions less then 2.6 are unsupported so enforce this at bootstrap. * Clean out the virtualenv install subdirectory so that can re-bootstrap.   I think the problem was introduced with update to virtualenv in Issue #3286.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@3859 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- bootstrap.py
+++ bootstrap.py
@@ -8,12 +8,9 @@ import subprocess
 import sys
 
 if (sys.version_info[0] < 3) and (sys.version_info[1] < 6):
-    if sys.version_info[1] < 5:
-        print ">>> Python versions less than 2.5 are unsupported. Exiting."
+        print ">>> The nominal supported version is 2.6."
+        print ">>> Python versions less than 2.6 are unsupported. Exiting."
         sys.exit(1)
-    else:
-        print ">>> Python version 2.5 is deprecated and will shortly become unsupported."
-        print ">>> The currently supported version is 2.6."
 
 ## execute an svn up command
 #
