9aa00753758a86d4be026d0e4e2c8364bc041caf
Maxim Voronkov <maxim.voronkov@csiro.au>
2011-07-20 04:10:30 +0000

    added a new unit test used to verify that the normal matrix is built correctly in the pre-averaging approach and all issues are with the data vector (#3426)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4026 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Base/scimath/current/tests/fitting/GenericNormalEquationsTest.h
+++ Code/Base/scimath/current/tests/fitting/GenericNormalEquationsTest.h
@@ -38,6 +38,8 @@
 
 #include <fitting/GenericNormalEquations.h>
 #include <fitting/DesignMatrix.h>
+#include <fitting/ComplexDiffMatrix.h>
+#include <fitting/PolXProducts.h>
 
 
 #include <cppunit/extensions/HelperMacros.h>
@@ -70,6 +72,7 @@ namespace askap
       CPPUNIT_TEST(testConstructorFromDesignMatrix);
       CPPUNIT_TEST_EXCEPTION(testNonConformanceError, askap::CheckError);
       CPPUNIT_TEST(testBlobStream);
+      CPPUNIT_TEST(testAddProduct);
       CPPUNIT_TEST_SUITE_END();
 
       private:
@@ -340,6 +343,102 @@ namespace askap
           const char* expected[3] = {"ScalarValue","Value0","Value1"};
           checkUnknowns<3>(expected);          
         }
+                        
+        // testing addition of normal equations based on a product of
+        // complex diff matrix and a vector presented by cross-products
+        void testAddProduct() {
+           ComplexDiffMatrix cdm(2,2);
+           // some dummy parameters, simple equation sum_j(gij*vj)=y_i
+           cdm(0,0) = ComplexDiff("g11", casa::Complex(1.1,-1.1));
+           cdm(0,1) = ComplexDiff("g12", casa::Complex(1.2,-1.2));
+           cdm(1,0) = ComplexDiff("g21", casa::Complex(2.1,-2.1));
+           cdm(1,1) = ComplexDiff("g22", casa::Complex(2.2,-2.2));
+           
+           // vector to multiply
+           casa::Vector<casa::Complex> vec(cdm.nColumn());
+           vec[0] = casa::Complex(10.,1.);
+           vec[1] = casa::Complex(1.,-10.);
+           
+           // measured data
+           casa::Vector<casa::Complex> measured(vec.nelements());
+           
+           // vector for actual design equations
+           ComplexDiffMatrix cdv(cdm.nRow(),1,0.);
+           for (casa::uInt row = 0; row<cdv.nRow(); ++row) {
+                for (casa::uInt col = 0; col<cdm.nColumn(); ++col) {
+                    cdv[row] += cdm(row,col) * vec[col];
+                }
+                measured[row] = cdv[row].value();
+           }
+           // perturb the measured data a bit to have a non-zero data vector
+           measured[0] += casa::Complex(0.01,-0.01);
+           measured[0] += casa::Complex(-0.02,0.02);
+           // use the standard approach via the design matrix
+           DesignMatrix dm;
+           dm.addModel(cdv,measured.reform(casa::IPosition(2,measured.nelements(),1)),casa::Matrix<double>(measured.nelements(),1,1.));
+           GenericNormalEquations ne1;
+           ne1.add(dm);
+           std::vector<std::string> unknowns1 = ne1.unknowns();
+           CPPUNIT_ASSERT_EQUAL(size_t(4), unknowns1.size());
+           
+           // now use the direct approach to obtain normal equations
+                      
+           // 1D buffer initialised to zero
+           PolXProducts pxp(vec.nelements(), casa::IPosition(), true);
+           for (casa::uInt pol1 = 0; pol1<vec.nelements(); ++pol1) {
+                for (casa::uInt pol2 = 0; pol2<vec.nelements(); ++pol2) {
+                     pxp.addModelMeasProduct(pol1,pol2, conj(vec[pol1])*measured[pol2]);
+                     if (pol1 >= pol2) {
+                         pxp.addModelProduct(pol1,pol2, conj(vec[pol1])*vec[pol2]);
+                     }
+                }
+           }
+           
+           GenericNormalEquations ne2;
+           ne2.add(cdm,pxp);
+           std::vector<std::string> unknowns2 = ne2.unknowns();
+           CPPUNIT_ASSERT_EQUAL(size_t(4), unknowns2.size());
+           
+           // check that the normal matrices obtained by these two approaches 
+           // are indeed identical for all parameters (ne1 and ne2 should be identical)
+           CPPUNIT_ASSERT_EQUAL(unknowns1.size(), unknowns2.size());
+           for (size_t par=0; par<unknowns1.size(); ++par) {
+                const std::string parName = unknowns1[par];                
+                CPPUNIT_ASSERT(contained(unknowns2,parName));
+                for (size_t par2=0; par2<unknowns1.size(); ++par2) {
+                     const std::string parName2 = unknowns1[par2];
+                     CPPUNIT_ASSERT(contained(unknowns2,parName2));
+                     const casa::Matrix<double> nm1 = ne1.normalMatrix(parName,parName2);
+                     const casa::Matrix<double> nm2 = ne2.normalMatrix(parName,parName2);
+                     CPPUNIT_ASSERT_EQUAL(nm1.shape(),nm2.shape());
+                     for (casa::uInt row=0; row<nm1.nrow(); ++row) {
+                          for (casa::uInt col=0; col<nm1.ncolumn(); ++col) {
+                               CPPUNIT_ASSERT_DOUBLES_EQUAL(nm1(row,col),nm2(row,col),1e-7);
+                          }
+                     }
+                }
+           }
+           
+           /*
+           for (size_t par=0; par<unknowns1.size(); ++par) {
+                const std::string parName = unknowns1[par];                
+                std::cout<<"par="<<parName<<" dataVector1 = "<<ne1.dataVector(parName)<<
+                           " dataVector2 = "<<ne2.dataVector(parName)<<std::endl;
+           }
+           */
+           
+        }
+    protected:
+        /// @brief helper method to check the presence of an element
+        /// @details
+        /// @param[in] cnt container
+        /// @param[in] elem element
+        /// @return true, if the element is present in the container
+        template<typename Cont> 
+        static bool contained(const Cont &cnt, const typename Cont::value_type &elem) {
+           return find(cnt.begin(),cnt.end(),elem) != cnt.end();
+        }
+        
     };
 
   }
