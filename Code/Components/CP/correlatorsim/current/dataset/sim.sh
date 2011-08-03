40556dbc09d95781e13aca9ab0388f2b9a131e85
Ben Humphreys <ben.humphreys@csiro.au>
2011-08-03 06:56:39 +0000

    Minor changes to the way logging is done.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4049 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/correlatorsim/current/dataset/sim.sh
+++ Code/Components/CP/correlatorsim/current/dataset/sim.sh
@@ -2,11 +2,7 @@
 
 export AIPSPATH=$ASKAP_ROOT/Code/Base/accessors/current
 
-echo "Extracting 10uJy model" | tee output.out
-tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/current/simulation/models/10uJy.model.small.tgz | tee -a output.out
-echo >> output.out
+tar zxvf ${ASKAP_ROOT}/Code/Components/Synthesis/testdata/current/simulation/models/10uJy.model.small.tgz
 
-echo Begin csimulator `date` >> output.out
-$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator1.in | tee -a output1.out
-$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator2.in | tee -a output2.out
-echo End csimulator `date` >> output.out
+$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator1.in | tee output1.out
+$ASKAP_ROOT/Code/Components/Synthesis/synthesis/current/install/bin/csimulator.sh -inputs simulator2.in | tee output2.out
