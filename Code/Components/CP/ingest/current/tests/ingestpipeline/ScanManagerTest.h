6447495eedbca72465f9556a584b32d49747e875
Ben Humphreys <ben.humphreys@csiro.au>
2011-08-11 05:58:57 +0000

    Remove ingestutils test, since the sub-package no longer exists.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4062 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/ingest/current/tests/ingestpipeline/ScanManagerTest.h
+++ Code/Components/CP/ingest/current/tests/ingestpipeline/ScanManagerTest.h
@@ -22,7 +22,7 @@
 /// along with this program; if not, write to the Free Software
 /// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ///
-/// @author Maxim Voronkov <maxim.voronkov@csiro.au>
+/// @author Ben Humphreys <ben.humphreys@csiro.au>
 
 // CPPUnit includes
 #include <cppunit/extensions/HelperMacros.h>
