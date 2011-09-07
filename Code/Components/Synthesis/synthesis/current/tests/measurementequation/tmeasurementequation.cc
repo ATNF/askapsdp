246cd8916306d8a455dd1754400e80f4d6d8cff3
Maxim Voronkov <maxim.voronkov@csiro.au>
2011-09-07 05:26:38 +0000

    reverted tmeasurementequation back
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4123 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Synthesis/synthesis/current/tests/measurementequation/tmeasurementequation.cc
+++ Code/Components/Synthesis/synthesis/current/tests/measurementequation/tmeasurementequation.cc
@@ -42,7 +42,7 @@ int main( int argc, char **argv)
     askapdev::testutils::AskapTestRunner runner(argv[0]);
 
     
-    /*runner.addTest(askap::synthesis::VectorOperationsTest::suite());
+    runner.addTest(askap::synthesis::VectorOperationsTest::suite());
     runner.addTest(askap::synthesis::ComponentEquationTest::suite()); 
     runner.addTest(askap::synthesis::CalibrationMETest::suite()); 
     //runner.addTest(askap::synthesis::ImageDFTEquationTest::suite());
@@ -51,7 +51,7 @@ int main( int argc, char **argv)
     runner.addTest(askap::synthesis::SynthesisParamsHelperTest::suite());
     runner.addTest(askap::synthesis::ImageParamsHelperTest::suite());
     runner.addTest(askap::synthesis::GaussianNoiseMETest::suite());
-    */runner.addTest(askap::synthesis::PolLeakageTest::suite()); 
+    runner.addTest(askap::synthesis::PolLeakageTest::suite()); 
     runner.addTest(askap::synthesis::PreAvgCalBufferTest::suite()); 
 
     const bool wasSucessful = runner.run();
