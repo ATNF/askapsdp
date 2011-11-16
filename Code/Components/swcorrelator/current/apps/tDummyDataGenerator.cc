742920de61e56104d3057b3550259f8987d263de
Maxim Voronkov <maxim.voronkov@csiro.au>
2011-11-16 07:25:53 +0000

    more useful parallel logic in place (#3949)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4252 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/swcorrelator/current/apps/tDummyDataGenerator.cc
+++ Code/Components/swcorrelator/current/apps/tDummyDataGenerator.cc
@@ -45,7 +45,7 @@
 ASKAP_LOGGER(logger, ".tDummyDataGenerator");
 
 #include <boost/thread/thread.hpp>
-
+#include <boost/scoped_array.hpp>
 
 using namespace std;
 using namespace askap;
@@ -75,25 +75,58 @@ void acquire(casa::Vector<casa::Complex> &buf1, casa::Vector<casa::Complex> &buf
   }
 }
 
+// global parameters to emulate simultaneous sampling on all antennas with potentially asynchronous arrival
+boost::condition_variable sampleTrigger;
+boost::mutex sampleTriggerMutex;
+long sampleBAT = 0;
+
 struct Worker {
-   Worker(const casa::Vector<casa::Complex> &data, const int nbeams) : itsData(data), itsNBeam(nbeams) {}
+   Worker(const casa::Vector<casa::Complex> &data, const int antID, const int chanID, const int nbeams) : 
+          itsData(data), itsAnt(antID), itsChan(chanID), itsNBeam(nbeams) 
+   {
+     ASKAPCHECK(itsNBeam > 0, "Number of beams is supposed to be positive");
+     ASKAPCHECK(itsAnt >= 0, "Antenna ID should be non-negative");
+     ASKAPCHECK(itsChan >= 0, "Spectral channel (card) ID should be non-negative");     
+     ASKAPCHECK(sizeof(float) == sizeof(long), "float and long has different sizes, packing of the data stream is likely to fail!");
+   }
    
-   void operator()() const {
+   void operator()() const {      
       boost::this_thread::disable_interruption di;
+      boost::scoped_array<float> buffer(new float[2*itsData.nelements()*itsNBeam+3]); // BAT, antenna and channel indices are in the stream
+      // C-stype packing of the 3 compulsory IDs
+      long *idPtr = (long*)buffer.get();
+      idPtr[0] = 0; // initialise BAT with 0
+      idPtr[1] = itsAnt; // antenna ID
+      idPtr[2] = itsChan; // channel ID
+      //
+      for (long beam = 0, counter = 3; beam < itsNBeam; ++beam) {
+           for (long sample = 0; sample < long(itsData.nelements()); ++sample, counter+=2) {
+                buffer[counter] = real(itsData[sample]);
+                buffer[counter+1] = imag(itsData[sample]);                
+           }
+      }
+      // buffer ready, wait for sampling trigger
       while (!boost::this_thread::interruption_requested()) {
-         boost::this_thread::sleep(boost::posix_time::seconds(1));
-         std::cout<<"test"<<std::endl;
+             boost::unique_lock<boost::mutex> lock(sampleTriggerMutex);
+             while ((sampleBAT == idPtr[0]) && !boost::this_thread::interruption_requested()) {  
+                 sampleTrigger.wait(lock);
+             }
+             //boost::this_thread::sleep(boost::posix_time::seconds(1));
+             if (!boost::this_thread::interruption_requested()) {
+                 idPtr[0] = sampleBAT;
+                 //
+                 // here we will send the buffer over the socket
+                 std::cout<<"test, BAT="<<idPtr[0]<<std::endl;
+             }
       }
       std::cout<<"finishing"<<std::endl;
    }
    
-   void exit() { 
-       itsContinueFlag = false;
-   }
 private:
    casa::Vector<casa::Complex> itsData;   
+   int itsAnt;
+   int itsChan;
    int itsNBeam;
-   bool itsContinueFlag;
 };
 
 // Main function
@@ -102,13 +135,13 @@ int main(int, const char** argv)
     try {
        casa::Timer timer;
        timer.mark();
+       casa::Time time;
     
        const float samplingRate = 32./27.*1e6; // in samples per second
        casa::Vector<casa::Complex> buf1;
        casa::Vector<casa::Complex> buf2;
-       //acquire(buf1,buf2,5.2e-6,32*31250,samplingRate);
+       acquire(buf1,buf2,5.2e-6,32*3125,samplingRate);
        // assume that antenna1 = antenna3 for this simple test
-       casa::Vector<casa::Complex> buf3(buf1);
 
        std::cout<<"initialisation of dummy data "<<"user:   " << timer.user() << " system: " << timer.system()
                                       << " real:   " << timer.real()<<std::endl;
@@ -119,12 +152,20 @@ int main(int, const char** argv)
        
        boost::thread_group threads;
        for (int cnt = 0; cnt<nChan; ++cnt) {
-            threads.create_thread(Worker(buf1, nBeam));
+            threads.create_thread(Worker(buf1, 0, cnt, nBeam));
        }
-       std::cout<<"sending signal"<<std::endl;
-       //worker.exit();
-       sleep(10);
+       
+       for (size_t cycle = 0; cycle < 10; ++cycle) {
+            std::cout<<"cycle "<<cycle<<std::endl;
+            {
+              boost::lock_guard<boost::mutex> lock(sampleTriggerMutex);
+              sampleBAT = long(time.age());
+            }
+            sampleTrigger.notify_all();
+            sleep(1);
+       }
        threads.interrupt_all();
+       sampleTrigger.notify_all();
        std::cout<<"waiting to finish"<<std::endl;       
        threads.join_all();
        //              
