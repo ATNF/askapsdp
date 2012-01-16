d4b0ded30b920c855ce925c4942a97e76e448448
Ben Humphreys <ben.humphreys@csiro.au>
2012-01-16 04:48:27 +0000

    As of r14841, not as many cpu cores are needed to run the test in a reasonable amount of time.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4403 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/pipelinetasks/current/functest/test_cmodel/run.sh
+++ Code/Components/CP/pipelinetasks/current/functest/test_cmodel/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpirun -np 7 $ASKAP_ROOT/Code/Components/CP/pipelinetasks/current/apps/cmodel.sh -inputs cmodel.in
+mpirun -np 3 $ASKAP_ROOT/Code/Components/CP/pipelinetasks/current/apps/cmodel.sh -inputs cmodel.in
