5f7644c92ff20db394ca29f3d204ce2ae2930efe
Matthew Whiting <matthew.whiting@csiro.au>
2012-03-08 02:20:37 +0000

    Updates & fixes to the taylor term creator. Now seems to work.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4498 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/simulations/current/apps/modelToTaylorTerms.cc
+++ Code/Components/Analysis/simulations/current/apps/modelToTaylorTerms.cc
@@ -41,6 +41,8 @@
 #include <casa/namespace.h>
 #include <casa/Quanta/Quantum.h>
 #include <casa/Quanta/Unit.h>
+#include <coordinates/Coordinates/Coordinate.h>
+#include <coordinates/Coordinates/SpectralCoordinate.h>
 #include <coordinates/Coordinates/CoordinateSystem.h>
 #include <images/Images/PagedImage.h>
 #include <images/Images/ImageInfo.h>
@@ -92,45 +94,51 @@ int main(int argc, const char **argv)
       }
       else{
 
-	std::string parsetFile(getInputs("-inputs", "createFITS.in", argc, argv));
+	std::string parsetFile(getInputs("-inputs", "modelToTaylorTerms.in", argc, argv));
 	ASKAPLOG_INFO_STR(logger,  "parset file " << parsetFile);
 	LOFAR::ParameterSet parset(parsetFile);
+	ASKAPLOG_INFO_STR(logger, "Full file follows:\n"<<parset);
 	LOFAR::ParameterSet subset(parset.makeSubset("model2TT."));
+	ASKAPLOG_INFO_STR(logger, "Subset follows:\n"<<subset);
 
-	std::string modelimage=subset.getString("inputmodel");
+	std::string modelimage=subset.getString("inputmodel","");
 	int nsubx=subset.getInt16("nsubx",1);
 	int nsuby=subset.getInt16("nsuby",1);
 	const int nterms=3;
 	int logevery = subset.getInt16("logevery",10);
+	ASKAPLOG_INFO_STR(logger, "Will log every "<<logevery << "% of the time");
 
-	struct wcsprm *wcs = analysis::casaImageToWCS(modelimage);      
-
-	ImageOpener::registerOpenImageFunction(ImageOpener::FITS, FITSImage::openFITSImage);
-	ImageOpener::registerOpenImageFunction(ImageOpener::MIRIAD, MIRIADImage::openMIRIADImage);
-	const LatticeBase* lattPtr = ImageOpener::openImage(modelimage);
-	if (lattPtr == 0)
-	  ASKAPTHROW(AskapError, "Requested image \"" << modelimage << "\" does not exist or could not be opened.");
-	const ImageInterface<Float>* imagePtr = dynamic_cast<const ImageInterface<Float>*>(lattPtr);
-	IPosition shape = imagePtr->shape();
-
-	int nx,ny,xmin,xmax,ymin,ymax;
+	casa::PagedImage<Float> img(modelimage);
+	IPosition shape = img.shape();
+	int specCoord = img.coordinates().findCoordinate(Coordinate::SPECTRAL);
+	int specAxis = img.coordinates().worldAxes(specCoord)[0];
+	ASKAPLOG_DEBUG_STR(logger, "Model image " << modelimage << " has shape " << shape << " and the spectral axis is #"<<specAxis );
+	
+	int nx=0,ny=0,xmin,xmax,ymin,ymax;
+	std::stringstream outputnamebase;
 	if(comms.isParallel()){
 	  nx = (comms.rank()-1) % nsubx;
 	  ny = (comms.rank()-1) / nsubx;
-	  xmin = int( nx * float(shape[wcs->lng])/float(nsubx) );
-	  xmax = int( (nx+1) * float(shape[wcs->lng])/float(nsubx) )-1;
-	  ymin = int( ny * float(shape[wcs->lat])/float(nsuby) );
-	  ymax = int( (ny+1) * float(shape[wcs->lat])/float(nsuby) )-1;
+	  xmin = int( nx * float(shape[0])/float(nsubx) );
+	  xmax = int( (nx+1) * float(shape[0])/float(nsubx) )-1;
+	  ymin = int( ny * float(shape[1])/float(nsuby) );
+	  ymax = int( (ny+1) * float(shape[1])/float(nsuby) )-1;
+	  outputnamebase<<modelimage << "_w"<<comms.rank()-1;
 	}
 	else{ // if serial mode, use the full range of x & y
 	  xmin=ymin=0;
-	  xmax=shape[wcs->lng];
-	  ymax=shape[wcs->lat];
+	  xmax=shape[0]-1;
+	  ymax=shape[1]-1;
+	  outputnamebase << modelimage;
 	}
 
+	ASKAPLOG_DEBUG_STR(logger, "isParallel="<<comms.isParallel()<< " rank="<<comms.rank()<<"   x in ["<<xmin<<","<<xmax<<"]   y in ["<<ymin << "," << ymax << "]");
+
+
+	casa::IPosition outshape(2,shape[0],shape[1]);
+	outshape[specAxis] = 1;
+	ASKAPLOG_DEBUG_STR(logger, "Shape of output images is " << outshape);
 	casa::Array<Float> outputs[nterms];
-	casa::IPosition outshape(shape);
-	outshape[wcs->spec] = 1;
 	for(int i=0;i<nterms;i++){
 	  outputs[i] = casa::Array<Float>(outshape,0.);
 	}
@@ -138,69 +146,79 @@ int main(int argc, const char **argv)
 	casa::IPosition start(shape.size(),0);
 	casa::IPosition end(shape-1);      
 
-	const int ndata=shape[wcs->spec];
+	const int ndata=shape[specAxis];
+	const int degree=nterms+2;
 	double chisq;
 	gsl_matrix *xdat, *cov;
 	gsl_vector *ydat, *w, *c;
-	xdat = gsl_matrix_alloc(ndata,nterms);
+	xdat = gsl_matrix_alloc(ndata,degree);
 	ydat = gsl_vector_alloc(ndata);
 	w = gsl_vector_alloc(ndata);
-	c = gsl_vector_alloc(nterms);
-	cov = gsl_matrix_alloc(nterms,nterms);
+	c = gsl_vector_alloc(degree);
+	cov = gsl_matrix_alloc(degree,degree);
+
       
+	double reffreq = img.coordinates().spectralCoordinate(specCoord).referenceValue()[0];
+	ASKAPLOG_DEBUG_STR(logger, "Reference = " << img.coordinates().spectralCoordinate(specCoord).referenceValue());
 	for(int i=0;i<ndata;i++){
-	  float logfreq=log10((wcs->crval[wcs->spec] + (i-wcs->crpix[wcs->spec])*wcs->cdelt[wcs->spec])/wcs->crval[wcs->spec]);
+	  double freq;
+	  if(!img.coordinates().spectralCoordinate(specCoord).toWorld(freq,double(i)))
+	    ASKAPLOG_ERROR_STR(logger, "Error converting spectral coordinate at channel " << i);
+	  //	  float logfreq=log10((wcs->crval[wcs->spec] + (i-wcs->crpix[wcs->spec])*wcs->cdelt[wcs->spec])/wcs->crval[wcs->spec]);
+	  float logfreq = log10(freq/reffreq);
 	  gsl_matrix_set(xdat,i,0,1.);
 	  gsl_matrix_set(xdat,i,1,logfreq);
 	  gsl_matrix_set(xdat,i,2,logfreq*logfreq);
-	  gsl_matrix_set(xdat,i,3,logfreq*logfreq*logfreq);
-	  gsl_matrix_set(xdat,i,4,logfreq*logfreq*logfreq*logfreq);
+// 	  gsl_matrix_set(xdat,i,3,logfreq*logfreq*logfreq);
+// 	  gsl_matrix_set(xdat,i,4,logfreq*logfreq*logfreq*logfreq);
 	  gsl_vector_set(w,i,1.);
 	}
 
-	for(int y=ymin; y<ymax; y++){
-	  for(int x=xmin; x<xmax; x++){
+	for(int y=ymin; y<=ymax; y++){
+	  for(int x=xmin; x<=xmax; x++){
 
 	    // LOOP OVER Y AND X
 	    // EXTRACT SPECTRUM FROM MODEL IMAGE
 	    // FIT TO SPECTRUM
 	    // STORE FIT RESULTS IN OUTPUT ARRAYS
 
-	    if( (x+y*(xmax-xmin+1)) % (xmax-xmin+1)*(ymax-ymin+1)/logevery == 0 )
+	    if( (x+y*(xmax-xmin+1)) % int((xmax-xmin+1)*(ymax-ymin+1)*logevery/100.) == 0 )
 	      ASKAPLOG_INFO_STR(logger, "Done " << x+y*(xmax-xmin+1) << " spectra out of " << (xmax-xmin+1)*(ymax-ymin+1) <<" with x="<<x<<" and y="<<y);
 
-	    start[wcs->lng]=end[wcs->lng]=x;
-	    start[wcs->lat]=end[wcs->lat]=y;
-
-	    casa::Array<Float> spectrum=imagePtr->getSlice(start,end);
-	    casa::Array<Float>::iterator iterSpec=spectrum.begin();
-	    for (int i=0;i<ndata;i++){
-	      gsl_vector_set(ydat,i,log10(double(*iterSpec++)));
+	    start[0]=end[0]=x;
+	    start[1]=end[1]=y;
+	    casa::Slicer specslice(start,end,casa::Slicer::endIsLast);
+	    casa::Array<Float> spectrum = img.getSlice(specslice,True);
+	    if(spectrum(IPosition(shape.size(),0))>1.e-20){
+	      casa::Array<Float>::iterator iterSpec=spectrum.begin();
+	      for (int i=0;i<ndata;i++){
+		gsl_vector_set(ydat,i,log10(double(*iterSpec++)));
+	      }
+	      gsl_multifit_linear_workspace * work = gsl_multifit_linear_alloc (ndata,degree);
+	      gsl_multifit_wlinear (xdat, w, ydat, c, cov, &chisq, work);
+	      gsl_multifit_linear_free (work);
+	      
+	      casa::IPosition outpos(2,x,y);
+	      float logflux=gsl_vector_get(c,0);
+	      outputs[0](outpos) = pow(10.,logflux);
+	      outputs[1](outpos) = gsl_vector_get(c,1);
+	      outputs[2](outpos) = gsl_vector_get(c,2);
 	    }
-	    gsl_multifit_linear_workspace * work = gsl_multifit_linear_alloc (ndata,nterms);
-	    gsl_multifit_wlinear (xdat, w, ydat, c, cov, &chisq, work);
-	    gsl_multifit_linear_free (work);
-	  
-	    casa::IPosition outpos(2,x,y);
-	    outputs[0](outpos) = pow(10.,gsl_vector_get(c,0));
-	    outputs[1](outpos) = gsl_vector_get(c,1);
-	    outputs[2](outpos) = gsl_vector_get(c,2);
-
 	  }
 	}
 
-	Unit bunit=imagePtr->units();
-	casa::Vector<casa::Quantum<Double> > beam = imagePtr->imageInfo().restoringBeam();
+	Unit bunit = img.units();
+	casa::Vector<casa::Quantum<Double> > beam = img.imageInfo().restoringBeam();
       
 	for(int t=0;t<nterms;t++){
 	  std::stringstream name;
-	  name << modelimage << "_w"<<comms.rank()-1<<".taylor." << t;
+	  name << outputnamebase.str() <<".taylor." << t;
 
-	  casa::CoordinateSystem csys = analysis::wcsToCASAcoord(wcs,1);
+	  casa::CoordinateSystem csys = img.coordinates();
 	  casa::IPosition tileshape(shape.size(),1);
-	  tileshape(wcs->lng) = std::min(128L,shape(wcs->lng));
-	  tileshape(wcs->lat) = std::min(128L,shape(wcs->lat));
-	  shape(wcs->spec)=1;
+	  tileshape(0) = std::min(128L,shape(0));
+	  tileshape(1) = std::min(128L,shape(1));
+	  shape(specAxis)=1;
 	
 	  ASKAPLOG_INFO_STR(logger, "Creating a new CASA image " << name.str() << " with the shape " << shape << " and tileshape " << tileshape);
 	  casa::PagedImage<float> img(casa::TiledShape(shape,tileshape), csys, name.str());
