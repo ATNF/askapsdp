7716f110bd435c413d17412c2bfd5e1cdf5b1239
Tony Maher <tony.maher@csiro.au>
2012-05-14 04:53:26 +0000

    See Issue #331 - Dependency cleanup. Clean up Code/Components/CP/imager/current/build.py to removes explicitc dependency analysis. Remove unused imports and modify logic so can eliminate 'import sys'.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4715 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/imager/current/build.py
+++ Code/Components/CP/imager/current/build.py
@@ -1,22 +1,12 @@
-# @file
-# build script for AutoBuild
-
-import os
-import sys
+import os.path
 
 from askapdev.rbuild.builders import Scons as Builder
-from askapdev.rbuild.dependencies import Dependency
-import askapdev.rbuild.utils as utils
-
-# Openmpi is virtual package, and want to know if the symlink has been
-# created.
-dep = Dependency(silent=True)
-dep.add_package()
-openmpi = dep.get_install_path("openmpi")
-
-if not os.path.exists(openmpi):
-    print "warn: Cannot build package as no MPI support found on this platform."
-    sys.exit(0)
 
 b = Builder(".")
-b.build()
+
+# Openmpi is virtual package, and want to know if the symlink has been created.
+openmpi = b.dep.get_install_path("openmpi")
+if not os.path.exists(openmpi):
+    print "warn: Cannot build as no MPI support found on this platform."
+else:
+    b.build()
