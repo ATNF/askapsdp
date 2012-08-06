c54fcdb88310c935ee2c8b864c533a0d4f5ec478
Maxim Voronkov <maxim.voronkov@csiro.au>
2012-08-06 00:36:06 +0000

    added utility to extract closure phases from MS produced by the sw correlator
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4843 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Code/Components/Synthesis/synthesis/current/apps/closuretest.cc
@@ -0,0 +1,140 @@
+/// @file
+/// @brief an utility to extract closure phases for sw-correlation experiment
+/// @copyright (c) 2007 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+
+
+#include <dataaccess/TableDataSource.h>
+#include <askap_accessors.h>
+#include <askap/AskapLogging.h>
+ASKAP_LOGGER(logger, "");
+
+#include <askap/AskapError.h>
+#include <dataaccess/SharedIter.h>
+
+#include <dataaccess/TableManager.h>
+#include <dataaccess/IDataConverterImpl.h>
+
+// casa
+#include <measures/Measures/MFrequency.h>
+#include <tables/Tables/Table.h>
+#include <casa/OS/Timer.h>
+#include <casa/Arrays/ArrayMath.h>
+
+
+
+// std
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+using namespace askap;
+using namespace askap::accessors;
+
+void process(const IConstDataSource &ds, size_t nAvg) {
+  IDataConverterPtr conv=ds.createConverter();  
+  conv->setFrequencyFrame(casa::MFrequency::Ref(casa::MFrequency::TOPO),"MHz");
+  conv->setEpochFrame(casa::MEpoch(casa::Quantity(55913.0,"d"),
+                      casa::MEpoch::Ref(casa::MEpoch::UTC)),"s");
+  conv->setDirectionFrame(casa::MDirection::Ref(casa::MDirection::J2000));                    
+  size_t counter = 0;
+  casa::Vector<casa::Complex> buf(3, casa::Complex(0.,0.));
+  casa::uInt nChan = 0;
+  double startTime = 0;
+    
+  std::ofstream os("phclosure.dat");
+  for (IConstDataSharedIter it=ds.createConstIterator(conv);it!=it.end();++it) {  
+       if (nChan == 0) {
+           nChan = it->nChannel();
+           startTime = it->time();
+       } else { 
+           ASKAPCHECK(nChan == it->nChannel(), 
+                  "Number of channels seem to have been changed, previously "<<nChan<<" now "<<it->nChannel());
+       }
+       ASKAPCHECK(it->nRow() == 3, "Expect 3 baselines, the accessor has "<<it->nRow()<<" rows");
+       ASKAPASSERT(it->nPol() >= 1);
+       ASKAPASSERT(it->nChannel() >= 1);
+       casa::Vector<casa::Complex> freqAvBuf(3, casa::Complex(0.,0.));
+       for (casa::uInt ch=0; ch<it->nChannel(); ++ch) {
+            freqAvBuf += it->visibility().xyPlane(0).column(0);
+       }
+       freqAvBuf /= float(it->nChannel());
+       buf += freqAvBuf;
+       if (counter == 0) {
+           startTime = it->time();
+       }
+       
+       if (++counter == nAvg) {
+           buf /= float(nAvg);
+           const float phClosure = arg(buf[0]*buf[1]*conj(buf[2]))/casa::C::pi*180.; 
+           os<<startTime<<" "<<phClosure<<std::endl;
+           buf.set(casa::Complex(0.,0.));
+           counter = 0;
+       }
+       //cout<<"time: "<<it->time()<<endl;
+  }
+  if (counter!=0) {
+      buf /= float(counter);
+      const float phClosure = arg(buf[0]*buf[1]*conj(buf[2]))/casa::C::pi*180.; 
+      os<<startTime<<" "<<phClosure<<std::endl;
+  }
+}
+
+
+int main(int argc, char **argv) {
+  try {
+     if (argc!=2) {
+         cerr<<"Usage "<<argv[0]<<" measurement_set"<<endl;
+	 return -2;
+     }
+
+     casa::Timer timer;
+
+     timer.mark();
+     TableDataSource ds(argv[1],TableDataSource::MEMORY_BUFFERS);     
+     std::cerr<<"Initialization: "<<timer.real()<<std::endl;
+     timer.mark();
+     // number of cycles to average
+     const size_t nAvg = 1;
+     process(ds, nAvg);
+     std::cerr<<"Job: "<<timer.real()<<std::endl;
+     
+  }
+  catch(const AskapError &ce) {
+     cerr<<"AskapError has been caught. "<<ce.what()<<endl;
+     return -1;
+  }
+  catch(const std::exception &ex) {
+     cerr<<"std::exception has been caught. "<<ex.what()<<endl;
+     return -1;
+  }
+  catch(...) {
+     cerr<<"An unexpected exception has been caught"<<endl;
+     return -1;
+  }
+  return 0;
+}
