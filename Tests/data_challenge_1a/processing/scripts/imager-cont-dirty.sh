0973f9fe90bc329eb9397da1ed9d5a27aebec207
Ben Humphreys <ben.humphreys@csiro.au>
2012-09-11 23:49:18 +0000

    Task #4770: Increased nwplanes from 7 to 129, turned on snapshotting, and decreased maxsupport from 2048 to 512. This allows this job to run within 4GB RAM per process.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4941 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/data_challenge_1a/processing/scripts/imager-cont-dirty.sh
+++ Tests/data_challenge_1a/processing/scripts/imager-cont-dirty.sh
@@ -7,7 +7,7 @@ cat > cimager-cont-dirty.qsub << EOF
 #!/bin/bash
 #PBS -W group_list=${QUEUEGROUP}
 #PBS -l select=1:ncpus=5:mem=23GB:mpiprocs=5+50:ncpus=6:mem=23GB:mpiprocs=6
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 ##PBS -M first.last@csiro.au
 #PBS -N cont-dirty
 #PBS -m a
@@ -29,14 +29,16 @@ Cimager.Images.image.i.dirty.frequency          = [1.420e9,1.420e9]
 Cimager.Images.image.i.dirty.nchan              = 1
 Cimager.Images.image.i.dirty.direction          = [12h30m00.00, -45.00.00.00, J2000]
 #
+Cimager.gridder.snapshotimaging                 = true
+Cimager.gridder.snapshotimaging.wtolerance      = 1000
 Cimager.gridder                                 = AWProject
-Cimager.gridder.AWProject.wmax                  = 3500
-Cimager.gridder.AWProject.nwplanes              = 7
+Cimager.gridder.AWProject.wmax                  = 1000
+Cimager.gridder.AWProject.nwplanes              = 129
 Cimager.gridder.AWProject.oversample            = 4
 Cimager.gridder.AWProject.diameter              = 12m
 Cimager.gridder.AWProject.blockage              = 2m
 Cimager.gridder.AWProject.maxfeeds              = 36
-Cimager.gridder.AWProject.maxsupport            = 2048
+Cimager.gridder.AWProject.maxsupport            = 512
 Cimager.gridder.AWProject.variablesupport       = true
 Cimager.gridder.AWProject.offsetsupport         = true
 Cimager.gridder.AWProject.frequencydependent    = true
