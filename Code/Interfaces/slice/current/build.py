413a4a645f51a7e0ff6c90d2af7b396f4a97fc1e
Tony Maher <tony.maher@csiro.au>
2012-10-10 04:14:13 +0000

    See Issue #4734.  Conver rbuid builders cmake and slicedoc to Sphinx. While here move _install() method from Code/Interfaces/slice/current/build.py into the SliceDoc builder as suggested by comments.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@4993 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Interfaces/slice/current/build.py
+++ Code/Interfaces/slice/current/build.py
@@ -20,20 +20,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
 #
-import os
 
-from askapdev.rbuild.builders import SliceDoc
-
-# When building a release it expects that the 'install' directory exists.
-# In this package we do not actually install anything but since it is a
-# dependency of other packages, the release target will try to process tjis
-# package, so create an empty install directory so it does not fail.
-# XXX Maybe this should just be moved into SliceDoc itself?
-
-class Builder(SliceDoc):
-    def _install(self):
-        if not os.path.exists(self._installdir):
-            os.makedirs(self._installdir)
+from askapdev.rbuild.builders import SliceDoc as Builder
 
 builder = Builder(".")
 builder.build()
