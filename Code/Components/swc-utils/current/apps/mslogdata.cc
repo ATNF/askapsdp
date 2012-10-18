eccb2d2883929c5f0bb15bddd6c8cd3e3ff0f959
Maxim Voronkov <maxim.voronkov@csiro.au>
2012-10-18 05:27:00 +0000

    finished utility to use sw-correlator data monitors with the data read from MS, ready for testing
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5020 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/swc-utils/current/apps/mslogdata.cc
+++ Code/Components/swc-utils/current/apps/mslogdata.cc
@@ -30,6 +30,8 @@
 /// @author Max Voronkov <maxim.voronkov@csiro.au>
 
 #include <dataaccess/TableDataSource.h>
+#include <dataaccess/IConstDataAccessor.h>
+
 #include <askap_accessors.h>
 #include <askap/AskapLogging.h>
 ASKAP_LOGGER(logger, "");
@@ -40,8 +42,13 @@ ASKAP_LOGGER(logger, "");
 #include <dataaccess/TableManager.h>
 #include <dataaccess/IDataConverterImpl.h>
 
+#include <swcorrelator/DataMonitors.h>
+#include <swcorrelator/CorrProducts.h>
+
 #include <casa/OS/Timer.h>
 #include <casa/Arrays/ArrayMath.h>
+
+#include <Common/ParameterSet.h>
 
 // std
 #include <stdexcept>
@@ -49,60 +56,169 @@ ASKAP_LOGGER(logger, "");
 #include <vector>
 #include <iomanip>
 
+// boost
+#include <boost/shared_ptr.hpp>
+
 using namespace askap;
 using namespace askap::accessors;
 
-void process(const IConstDataSource &ds, size_t nAvg) {
+struct DataLogger {
+  
+  explicit DataLogger(size_t nAvg) : itsNAvg(nAvg), itsStartTime(0.)  {}
+
+  void setupMonitor(const IConstDataAccessor &acc);
+  
+  void reset();
+  
+  void process(const IConstDataSource &ds);
+  
+protected:
+  void publish();
+  
+private:
+
+  // data monitors
+  boost::shared_ptr<swcorrelator::DataMonitors> itsMonitors;
+  
+  // buffers for data per beam
+  std::vector<boost::shared_ptr<swcorrelator::CorrProducts> > itsCorrProducts;
+
+  // number of cycles to average
+  size_t itsNAvg;
+  // time
+  double itsStartTime;
+  // buffer
+  casa::Cube<casa::Complex> itsBuffer;
+  // antenna 1 indices
+  casa::Vector<casa::uInt> itsAnt1IDs;
+  // antenna 2 indices
+  casa::Vector<casa::uInt> itsAnt2IDs;
+  // beam 1 indices
+  casa::Vector<casa::uInt> itsBeam1IDs;
+  // beam 2 indices
+  casa::Vector<casa::uInt> itsBeam2IDs;
+     
+};
+
+void DataLogger::setupMonitor(const IConstDataAccessor &acc)
+{
+  LOFAR::ParameterSet parset;
+  parset.add("monitors","basic");
+  itsMonitors.reset(new swcorrelator::DataMonitors(parset));
+  
+  const casa::uInt maxAntID = casa::max(casa::max(acc.antenna1()),casa::max(acc.antenna2()));
+  const casa::uInt maxBeamID = casa::max(casa::max(acc.feed1()),casa::max(acc.feed2()));
+  const int nBeam = int(maxBeamID)+1;
+  
+  itsMonitors->initialise(int(maxAntID)+1, nBeam, acc.nChannel());  
+  
+  itsCorrProducts.resize(nBeam);
+  for (int beam=0;beam<nBeam; ++beam) {
+       itsCorrProducts[beam].reset(new swcorrelator::CorrProducts(int(acc.nChannel()),beam));
+  }
+}
+
+
+void DataLogger::reset()
+{
+  itsAnt1IDs.resize(0);
+  itsAnt2IDs.resize(0);
+  itsBeam1IDs.resize(0);
+  itsBeam2IDs.resize(0);
+  itsBuffer.resize(0,0,0);
+  itsStartTime=0;
+  itsMonitors.reset();
+}
+
+void DataLogger::publish() {
+  for (int beam=0; beam<int(itsCorrProducts.size()); ++beam) {
+       const boost::shared_ptr<swcorrelator::CorrProducts> &cp = itsCorrProducts[beam];
+       ASKAPDEBUGASSERT(cp);
+       
+       cp->init(uint64_t(itsStartTime));
+       
+       for (casa::uInt row=0; row<itsBuffer.nrow(); ++row) {
+            ASKAPCHECK(itsBeam1IDs[row] == itsBeam2IDs[row], "Cross-beam correlations are not supported");
+            if (int(itsBeam1IDs[row]) != beam) {
+                continue;
+            }
+            int baseline = -1;
+            if ((itsAnt1IDs[row] == 0) && (itsAnt2IDs[row] == 1)) {
+                 baseline = 0;
+            } else if ((itsAnt1IDs[row] == 1) && (itsAnt2IDs[row] == 2)) {
+                 baseline = 1;
+            } else if ((itsAnt1IDs[row] == 0) && (itsAnt2IDs[row] == 2)) {
+                 baseline = 2;
+            } else {
+               ASKAPTHROW(AskapError,"Unknown baseline "<<itsAnt1IDs[row]<<" - "<<itsAnt2IDs[row]<<" for row "<<row);
+            }
+            ASKAPDEBUGASSERT(baseline >= 0);
+            cp->itsVisibility.row(baseline) = itsBuffer.xyPlane(0).row(row);
+            cp->itsFlag.row(baseline).set(false);
+       } 
+  }
+}
+
+void DataLogger::process(const IConstDataSource &ds) {
   IDataSelectorPtr sel=ds.createSelector();
-  sel->chooseFeed(1);
+  //sel->chooseFeed(1);
   IDataConverterPtr conv=ds.createConverter();  
   conv->setFrequencyFrame(casa::MFrequency::Ref(casa::MFrequency::TOPO),"MHz");
-  conv->setEpochFrame(casa::MEpoch(casa::Quantity(55913.0,"d"),
-                      casa::MEpoch::Ref(casa::MEpoch::UTC)),"s");
+  conv->setEpochFrame(casa::MEpoch(casa::Quantity(0.,"d"),
+                      casa::MEpoch::Ref(casa::MEpoch::TAI)),"us");
   conv->setDirectionFrame(casa::MDirection::Ref(casa::MDirection::J2000));                    
   size_t counter = 0;
-  casa::Vector<casa::Complex> buf(3, casa::Complex(0.,0.));
-  casa::uInt nChan = 0;
-  double startTime = 0;
-    
+      
   for (IConstDataSharedIter it=ds.createConstIterator(sel,conv);it!=it.end();++it) {  
-       if (nChan == 0) {
-           nChan = it->nChannel();
-           startTime = it->time();
+       if (itsBuffer.nelements() == 0) {
+           itsBuffer.resize(it->nRow(),it->nChannel(),it->nPol());
+           itsBuffer.set(casa::Complex(0.,0.));
+           itsStartTime = it->time();
+           itsAnt1IDs = it->antenna1().copy();
+           itsAnt2IDs = it->antenna2().copy();
+           itsBeam1IDs = it->feed1().copy();
+           itsBeam2IDs = it->feed2().copy();           
        } else { 
-           ASKAPCHECK(nChan == it->nChannel(), 
-                  "Number of channels seem to have been changed, previously "<<nChan<<" now "<<it->nChannel());
+           ASKAPCHECK(itsBuffer.shape() == it->visibility().shape(), 
+                  "The shape of the visibility cube seems to have been changed, previously "<<itsBuffer.shape()<<
+                  " now "<<it->visibility().shape());
+           ASKAPDEBUGASSERT(itsAnt1IDs.nelements() == it->nRow());
+           ASKAPDEBUGASSERT(itsAnt2IDs.nelements() == it->nRow());
+           ASKAPDEBUGASSERT(itsBeam1IDs.nelements() == it->nRow());
+           ASKAPDEBUGASSERT(itsBeam2IDs.nelements() == it->nRow());
+           for (casa::uInt row = 0; row<it->nRow(); ++row) {
+                ASKAPCHECK(itsAnt1IDs[row] == it->antenna1()[row], "Mismatch of antenna 1 index for row "<<row<<
+                           " - got "<<it->antenna1()[row]<<" expected "<<itsAnt1IDs[row]);
+                ASKAPCHECK(itsAnt2IDs[row] == it->antenna2()[row], "Mismatch of antenna 2 index for row "<<row<<
+                           " - got "<<it->antenna2()[row]<<" expected "<<itsAnt2IDs[row]);
+                ASKAPCHECK(itsBeam1IDs[row] == it->feed1()[row], "Mismatch of beam index (for the 1st antenna) for row "<<row<<
+                           " - got "<<it->feed1()[row]<<" expected "<<itsBeam1IDs[row]);
+                ASKAPCHECK(itsBeam2IDs[row] == it->feed2()[row], "Mismatch of beam index (for the 2nd antenna) for row "<<row<<
+                           " - got "<<it->feed2()[row]<<" expected "<<itsBeam2IDs[row]);
+           }       
        }
-       ASKAPCHECK(it->nRow() == 3, "Expect 3 baselines, the accessor has "<<it->nRow()<<" rows");
        ASKAPASSERT(it->nPol() >= 1);
        ASKAPASSERT(it->nChannel() >= 1);
-       // we require that 3 baselines come in certain order, so we can hard code conjugation for calculation
-       // of the closure phase
-       ASKAPCHECK(it->antenna2()[0] == it->antenna1()[1], "Expect baselines in the order 1-2,2-3 and 1-3");
-       ASKAPCHECK(it->antenna1()[0] == it->antenna1()[2], "Expect baselines in the order 1-2,2-3 and 1-3");
-       ASKAPCHECK(it->antenna2()[1] == it->antenna2()[2], "Expect baselines in the order 1-2,2-3 and 1-3");
-       //
-       casa::Vector<casa::Complex> freqAvBuf(3, casa::Complex(0.,0.));
-       for (casa::uInt ch=0; ch<it->nChannel(); ++ch) {
-            freqAvBuf += it->visibility().xyPlane(0).column(ch);
-       }
-       freqAvBuf /= float(it->nChannel());
-       buf += freqAvBuf;
+       
+       itsBuffer += it->visibility();
        if (counter == 0) {
-           startTime = it->time();
+           itsStartTime = it->time();          
        }
        
-       if (++counter == nAvg) {
-           buf /= float(nAvg);
-           // publish buf
-           buf.set(casa::Complex(0.,0.));
+       if (++counter == itsNAvg) {
+           itsBuffer /= float(itsNAvg);
+           // publish buffer
+           publish();
+           //
+           itsBuffer.set(casa::Complex(0.,0.));
            counter = 0;
        }
        //cout<<"time: "<<it->time()<<endl;
   }
   if (counter!=0) {
-      buf /= float(counter);
-      // publish buf
+      itsBuffer /= float(counter);
+      // publish buffer
+      publish();
   }
 }
 
@@ -122,7 +238,8 @@ int main(int argc, char **argv) {
      timer.mark();
      // number of cycles to average
      const size_t nAvg = 1;
-     process(ds, nAvg);
+     DataLogger dl(nAvg);
+     dl.process(ds);
      std::cerr<<"Job: "<<timer.real()<<std::endl;
      
   }
