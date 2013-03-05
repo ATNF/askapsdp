9eaad1cfcaebe39a31ddd8fffc42bc26e7d4ffee
Matthew Whiting <matthew.whiting@csiro.au>
2013-03-05 01:09:56 +0000

    Correcting the values for the full BETA case - these had been set to the half BETA case instead!
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5271 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/data_challenge_1a/processing/scripts/config.sh
+++ Tests/data_challenge_1a/processing/scripts/config.sh
@@ -130,40 +130,40 @@ elif [ ${BETA_MODE} == "full" ]; then
 # THIS IS FOR FULL BANDWIDTH
 
 # final channel, used by create-coarse-ms.sh
-    END_CHANNEL_CREATECOARSE=8208
+    END_CHANNEL_CREATECOARSE=16416
 # number of workers used by create-coarse-ms.sh
-    NUM_WORKERS_CREATECOARSE=152
-    QSUB_RANGE_CREATECOARSE="0-151"
+    NUM_WORKERS_CREATECOARSE=304
+    QSUB_RANGE_CREATECOARSE="0-303"
 
 # number of worker nodes needed for gains-calibration.sh - work with 2 worker cpus per node
-    GAINS_CAL_SELECT="1:ncpus=1:mem=23GB:mpiprocs=1+76:ncpus=2:mem=23GB:mpiprocs=2"
+    GAINS_CAL_SELECT="1:ncpus=1:mem=23GB:mpiprocs=1+152:ncpus=2:mem=23GB:mpiprocs=2"
     NUM_BEAMS_GAINSCAL=36
 
 # number of worker nodes needed for imager-cont-clean.sh - work with 2 worker cpus per node (but with nworkergroups=3)
-    CONT_CLEAN_SELECT="1:ncpus=1:mem=23GB:mpiprocs=1+76:ncpus=6:mem=23GB:mpiprocs=6"
+    CONT_CLEAN_SELECT="1:ncpus=1:mem=23GB:mpiprocs=1+152:ncpus=6:mem=23GB:mpiprocs=6"
     CONT_CLEAN_FREQ=1.270GHz
 
 # number of worker nodes needed for imager-cont-dirty.sh - work with 6 worker cpus per node, plus extra on the master's node
-    CONT_DIRTY_SELECT="1:ncpus=3:mem=23GB:mpiprocs=3+25:ncpus=6:mem=23GB:mpiprocs=6"
+    CONT_DIRTY_SELECT="1:ncpus=5:mem=23GB:mpiprocs=5+50:ncpus=6:mem=23GB:mpiprocs=6"
     CONT_DIRTY_FREQ=1.270GHz
 
 # base frequency for continuum cubes
     CONT_CUBE_FREQ_ZERO_CHAN=1.421e9
 # number of workers used for continuum cubes, and qsub range
-    NUM_WORKERS_CONT_CUBE=152
-    QSUB_RANGE_CONT_CUBE="0-151"
+    NUM_WORKERS_CONT_CUBE=304
+    QSUB_RANGE_CONT_CUBE="0-303"
 # final channel used for the make-spectral-cube call for continuum cubes
-    CONT_CUBE_FINALCH=151
+    CONT_CUBE_FINALCH=303
 
 # base frequency for spectral-line cubes
     SPECTRAL_CUBE_FREQ_ZERO_CHAN=1.421e9
 # number of workers used for spectral-line cubes, and qsub range
-    NUM_WORKERS_SPECTRAL_CUBE=8208
-    QSUB_RANGE_SPECTRAL_CUBE_1="0-4103"
-    QSUB_RANGE_SPECTRAL_CUBE_1="4104-8207"
-    QSUB_RANGE_SPECTRAL_CUBE_FULL="0-8207"
+    NUM_WORKERS_SPECTRAL_CUBE=16416
+    QSUB_RANGE_SPECTRAL_CUBE_1="0-8207"
+    QSUB_RANGE_SPECTRAL_CUBE_1="8208-16415"
+    QSUB_RANGE_SPECTRAL_CUBE_FULL="0-16415"
 # final channel used for the make-spectral-cube call for continuum cubes
-    SPECTRAL_CUBE_FINALCH=8207
+    SPECTRAL_CUBE_FINALCH=16415
 
 # reference frequency for sky model
     SKYMODEL_REFFREQ="1.270GHz"
