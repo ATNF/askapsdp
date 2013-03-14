5a64566e83d97b5adc6578ef4591ac87b27f0600
Maxim Voronkov <maxim.voronkov@csiro.au>
2013-03-14 06:28:25 +0000

    added another method of rounding to match the approach used in the software correlator (#5120)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5303 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/swc-utils/current/apps/tstbbcapture.cc
+++ Code/Components/swc-utils/current/apps/tstbbcapture.cc
@@ -9,6 +9,10 @@
 #include <casa/Arrays/ArrayMath.h>
 #include <casa/BasicSL.h>
 #include <scimath/Mathematics/FFTServer.h>
+#include <scimath/Mathematics/StatAcc.h>
+#include <scimath/Mathematics/HistAcc.h>
+#include <askap/AskapError.h>
+
 
 using namespace askap;
 using namespace askap::swcorrelator;
@@ -34,6 +38,7 @@ void fftExperiments(const std::vector<std::complex<float> > &data)
 }
 
 int main(int argc, char **argv) {
+try {
    const std::string fname = argc >= 2 ? argv[1] : "apps/BB/ant0.beam0.chan0.bat4830973926000000.dat";
    size_t nbins = 100;
    std::vector<std::complex<float> > data = CaptureWorker::read(fname);
@@ -48,8 +53,14 @@ int main(int argc, char **argv) {
    fftExperiments(data);
    //
    const int intMaxAmp = int(maxAmp);
+   // use casa methods to get the histogram because the rounding may be done differently 
+   const float binWidth = 2*maxAmp / nbins;
+   casa::HistAcc<float> histRe(-maxAmp, maxAmp, binWidth);
+   casa::HistAcc<float> histIm(-maxAmp, maxAmp, binWidth);
    std::vector<size_t> reCounts(nbins,0), imCounts(nbins,0);
    for (size_t i = 0; i<data.size(); ++i) {
+        histRe.put(std::real(data[i]));
+        histIm.put(std::imag(data[i]));
         const int re = int(std::real(data[i]));
         const int im = int(std::imag(data[i]));
         const size_t reBin = ((re + intMaxAmp) * nbins) / intMaxAmp / 2;
@@ -62,9 +73,24 @@ int main(int argc, char **argv) {
         }         
         //std::cout<<int(std::real(data[i]))<<" "<<int(std::imag(data[i]))<<std::endl;
    }
+
+   casa::Block<casa::uInt> binsRe, binsIm;
+   casa::Block<float> valsRe, valsIm;
+   const casa::uInt nbinsRe = histRe.getHistogram(binsRe, valsRe);
+   const casa::uInt nbinsIm = histIm.getHistogram(binsIm, valsIm);
+   ASKAPASSERT(nbinsRe == nbinsIm);
+
    std::ofstream os("hist.dat");
+   /*
    for (size_t i = 0; i<nbins; ++i) {
        float x = -maxAmp + (float(i)/float(nbins))*maxAmp*2;
        os << i<<" "<< x << " " << reCounts[i]<< " "<< imCounts[i]<<std::endl;
+   } */
+   for (size_t i = 0; i<binsRe.nelements(); ++i) {
+       os <<i<<" "<<valsRe[i]<<" "<<valsIm[i]<<" "<<binsRe[i]<<" "<<binsIm[i]<<std::endl;
    }
+   std::cout<<"Mean(Re)="<<histRe.getStatistics().getMean()<<" Mean(Im)="<<histIm.getStatistics().getMean()<<std::endl;
+} catch (const AskapError &ae) {
+   std::cerr<<ae.what()<<std::endl;
+}
 }
