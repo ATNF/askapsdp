3263f4bb5bb083ff6cb68e89189d2a426512eade
Tony Maher <tony.maher@csiro.au>
2013-03-26 11:04:03 +0000

    See Issue #3307. Run clean before build/install when in Tools and 3rdParty but not in Code. Do not wnat to run clean in Code becasuse developers make a change to file and do not want to recompile everything from scratch. Add explicit no_clean flag to  Code/Interfaces/cpp/current/build.py because it is a tricky double builder.  It is not actually necessary since we are in Code, but do so for safety and add comments to the builder about this fact.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5316 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Interfaces/cpp/current/build.py
+++ Code/Interfaces/cpp/current/build.py
@@ -1,10 +1,10 @@
-# @file
-# build script for AutoBuild
-
 from askapdev.rbuild.builders import Scons as Builder
 
 b = Builder(".")
 b.build()
+# See Issue #2462 as to why we need to run the builder twice.
+# The method .build() in 3rdParty and Tools does a clean before each build/install.
+# But in Code the default is not to do a clean.  However for safety, since this is a double
+# builder explicitly set the do_clean flag to false.
+b.do_clean=False
 b.build()
-
-
