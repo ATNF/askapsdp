7311ef5c1bfe942997ab7417744877ce507f459e
Matthew Whiting <matthew.whiting@csiro.au>
2013-05-23 00:58:42 +0000

    Fixing the unit test for ContinuumSelavy, that broke when I introduced the pixel positions.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5434 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/analysisutilities/current/tests/modelcomponents/ContinuumSelavyTests.h
+++ Code/Components/Analysis/analysisutilities/current/tests/modelcomponents/ContinuumSelavyTests.h
@@ -1,6 +1,6 @@
 /// @file
 ///
-/// XXX Notes on program XXX
+/// Unit tests for the ContinuumSelavy class
 ///
 /// @copyright (c) 2008 CSIRO
 /// Australia Telescope National Facility (ATNF)
@@ -38,50 +38,71 @@ namespace askap {
 
   namespace analysisutilities {
 
-	  ///#   ID           Name         RA        DEC          F_int        F_peak          F_int(fit)           F_pk(fit)  Maj(fit)  Min(fit) P.A.(fit) Maj(fit_deconv.) Min(fit_deconv.) P.A.(fit_deconv.)      Alpha       Beta                 Chisq(fit)          RMS(image)       RMS(fit) Nfree(fit) NDoF(fit) NPix(fit) NPix(obj) Guess?
-    const std::string ContinuumSelavyInput="    1a J124537-450659 187.5 -45.     0.12673451     0.04296888          1.          0.03728425   10. 5. 30. 10. 5. 30.      0.5      0.000               45.244525909          0.00452679          0.75203496          6        73        80        80      0";
+//Original input was as follows - just the header of a selavy-fitResults.txt file, with a single line
+/* 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+#      ID           Name         RA        DEC      X      Y       F_int      F_peak   F_int(fit)   F_pk(fit)  Maj(fit)  Min(fit) P.A.(fit)  Maj(fit_deconv.)   Min(fit_deconv.) P.A.(fit_deconv.)                          Alpha    Beta Chisq(fit)   RMS(image)  RMS(fit) Nfree(fit) NDoF(fit) NPix(fit) NPix(obj) Guess? 
+#                             [deg]      [deg]                      [Jy]   [Jy/beam]         [Jy]   [Jy/beam]  [arcsec]  [arcsec]     [deg]          [arcsec]           [arcsec]             [deg]                                                      [Jy/beam]                                                           
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+      70a J034439-362734  56.159180 -36.463321 1362.0 4235.7     0.08043     0.03443      0.04286     0.03440    25.425    17.936      4.97            9.9803            6.93991              7.84                       -1.06685   0.000    787.936     0.000258     1.195          6       533       552       552      0
+*/ 
 
-    class ContinuumSelavyTest : public CppUnit::TestFixture {
-	CPPUNIT_TEST_SUITE(ContinuumSelavyTest);
-	CPPUNIT_TEST(testParameters);
-	CPPUNIT_TEST(testFluxes);
-	CPPUNIT_TEST_SUITE_END();
+      const std::string ContinuumSelavyInput="      70a J034439-362734  56.159180 -36.463321 1362.0 4235.7     0.08043     0.03443      0.04286     0.03440    25.425    17.936      4.97            9.9803            6.93991              7.84                       -1.06685   0.000    787.936     0.000258     1.195          6       533       552       552      0";
+
+      const std::string RA = ContinuumSelavyInput.substr(26,9);
+      const std::string Dec = ContinuumSelavyInput.substr(36,10);
+      const double flux = atof(ContinuumSelavyInput.substr(90,7).c_str());
+      const double alpha = atof(ContinuumSelavyInput.substr(217,8).c_str());
+      const double maj = atof(ContinuumSelavyInput.substr(113,6).c_str());
+      const double min = atof(ContinuumSelavyInput.substr(123,6).c_str());
+      const double pa = atof(ContinuumSelavyInput.substr(135,4).c_str());
+
+
+      class ContinuumSelavyTest : public CppUnit::TestFixture {
+	  CPPUNIT_TEST_SUITE(ContinuumSelavyTest);
+	  CPPUNIT_TEST(testParameters);
+	  CPPUNIT_TEST(testFluxes);
+	  CPPUNIT_TEST_SUITE_END();
 	
-    private:
-	// members
-	ContinuumSelavy itsComponent;
+      private:
+	  // members
+	  ContinuumSelavy itsComponent;
 	
-    public:
+      public:
 
-	void setUp(){
+	  void setUp(){
 
-	  itsComponent.define(ContinuumSelavyInput);
-
-	}
+	      itsComponent.define(ContinuumSelavyInput);
+	  
+	  }
 
 /*****************************************/
-	void tearDown() {
-	}
+	  void tearDown() {
+	  }
 
 /*****************************************/
-	void testParameters() {
-	  CPPUNIT_ASSERT(itsComponent.ra()=="187.5");
-	  CPPUNIT_ASSERT(itsComponent.dec()=="-45.");
-	  CPPUNIT_ASSERT(fabs(itsComponent.fluxZero()-1.)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.alpha()-0.5)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.maj()-10.)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.min()-5.)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.pa()-30.)<1.e-6);
-	  CPPUNIT_ASSERT(!itsComponent.isGuess());
-	}
+	  void testParameters() {
+	      CPPUNIT_ASSERT(itsComponent.ra()==RA);
+	      CPPUNIT_ASSERT(itsComponent.dec()==Dec);
+	      CPPUNIT_ASSERT(fabs(itsComponent.fluxZero()-flux)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.alpha()-alpha)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.maj()-maj)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.min()-min)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.pa()-pa)<1.e-6);
+	      CPPUNIT_ASSERT(!itsComponent.isGuess());
+	  }
 
-	void testFluxes(){
-	  CPPUNIT_ASSERT(fabs(itsComponent.fluxZero()-1.)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.flux(1400.)-1.)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.flux(1000.)-0.84515425)<1.e-6);
-	  CPPUNIT_ASSERT(fabs(itsComponent.flux(2000.)-1.1952286)<1.e-6);
-	}
+	  void testFluxes(){
+	      const double f1400 = flux * pow(1400./itsComponent.nuZero(), alpha);
+	      const double f1000 = flux * pow(1000./itsComponent.nuZero(), alpha);
+	      const double f2000 = flux * pow(2000./itsComponent.nuZero(), alpha);
 
-    };
+	      CPPUNIT_ASSERT(fabs(itsComponent.fluxZero()-flux)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.flux(1400.)-f1400)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.flux(1000.)-f1000)<1.e-6);
+	      CPPUNIT_ASSERT(fabs(itsComponent.flux(2000.)-f2000)<1.e-6);
+	  }
+
+      };
   }
 }
