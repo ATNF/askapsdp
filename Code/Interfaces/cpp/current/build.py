0f6db28319853c25767044620ac21daa0b174765
Tony Maher <tony.maher@csiro.au>
2013-06-04 04:37:16 +0000

    See Issue #3307 - preclean before building. * Always run clean() in Tools and 3rdParty except for second builder in those packages which use two builders. * Remove install directory in Code so there can not be any old files left there.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5464 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Interfaces/cpp/current/build.py
+++ Code/Interfaces/cpp/current/build.py
@@ -2,9 +2,9 @@ from askapdev.rbuild.builders import Scons as Builder
 
 b = Builder(".")
 b.build()
+
 # See Issue #2462 as to why we need to run the builder twice.
-# The method .build() in 3rdParty and Tools does a clean before each build/install.
-# But in Code the default is not to do a clean.  However for safety, since this is a double
-# builder explicitly set the do_clean flag to false.
+# build() method in Code removes the install directory so need
+# to set the do_clean flag to False - See Issue #3307.
 b.do_clean=False
 b.build()
