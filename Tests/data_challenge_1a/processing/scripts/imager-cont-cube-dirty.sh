907f4da7a46fffd75a1da855da8abc9a341cb90e
Matthew Whiting <matthew.whiting@csiro.au>
2013-07-26 04:24:18 +0000

    Data challenge - Putting the channel images for the continuum cubes in a work directory, similar to the spectral-line imaging.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5547 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/data_challenge_1a/processing/scripts/imager-cont-cube-dirty.sh
+++ Tests/data_challenge_1a/processing/scripts/imager-cont-cube-dirty.sh
@@ -5,6 +5,23 @@
 
 imagebase=i.cube.dirty
 
+# Create a work dir directory and one to hold the output measurement sets
+SL_WORK_DIR=sl-work-dir
+if [ ! -d ${SL_WORK_DIR} ]; then
+    mkdir ${SL_WORK_DIR}
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create directory ${SL_WORK_DIR}"
+        exit 1
+    fi
+    mkdir ${SL_WORK_DIR}/MS
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create directory ${SL_WORK_DIR}/MS"
+        exit 1
+    fi
+fi
+
+mv askap.log_cfg ${SL_WORK_DIR}
+
 cat > cimager-cont-cube-dirty.qsub << EOF
 #!/bin/bash
 #PBS -W group_list=${QUEUEGROUP}
@@ -22,7 +39,7 @@ cat > cimager-cont-cube-dirty.qsub << EOF
 #
 ###########
 
-cd \${PBS_O_WORKDIR}
+cd \${PBS_O_WORKDIR}/${SL_WORK_DIR}
 
 imageName="image.${imagebase}_ch\${PBS_ARRAY_INDEX}"
 ms=MS/coarse_chan_\${PBS_ARRAY_INDEX}.ms
@@ -110,25 +127,25 @@ fi
 DEPENDS="afterok:${QSUB_CONTCUBEDIRTY}"
 
 # Run makecube using the make-spectral-cube.qsub script
-DODELETE=true
+DODELETE=false
 FIRSTCH=0
 FINALCH=${CONT_CUBE_FINALCH}
 RESTFREQ=-1.
 
-IMAGEPREFIX="image.${imagebase}_ch"
+IMAGEPREFIX="${SL_WORK_DIR}/image.${imagebase}_ch"
 IMAGESUFFIX=".restored"
 OUTPUTCUBE=image.${imagebase}.restored
 . ${SCRIPTDIR}/make-spectral-cube.sh
 
 IMAGESUFFIX=""
-IMAGEPREFIX="psf.${imagebase}_ch"
+IMAGEPREFIX="${SL_WORK_DIR}/psf.${imagebase}_ch"
 OUTPUTCUBE=psf.${imagebase}
 . ${SCRIPTDIR}/make-spectral-cube.sh
 
-IMAGEPREFIX="sensitivity.${imagebase}_ch"
+IMAGEPREFIX="${SL_WORK_DIR}/sensitivity.${imagebase}_ch"
 OUTPUTCUBE=sensitivity.${imagebase}
 . ${SCRIPTDIR}/make-spectral-cube.sh
 
-IMAGEPREFIX="weights.${imagebase}_ch"
+IMAGEPREFIX="${SL_WORK_DIR}/weights.${imagebase}_ch"
 OUTPUTCUBE=weights.${imagebase}
 . ${SCRIPTDIR}/make-spectral-cube.sh
