a25e6fbd5aacba65ec0413c63fed271817565b25
Tony Maher <tony.maher@csiro.au>
2013-08-27 23:31:49 +0000

    See Issue #3228 - Move from Hudson to Jenkins. First step is to move the scripts from inside Hudson to ASKAPsoft repository. ASKAP_ROOT/Tests/Hudson. First one is Analysis.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5595 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Tests/hudson/CP/analysis.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -l
+
+#
+# Bootstrap
+#
+unset ASKAP_ROOT
+cd $WORKSPACE/trunk
+nice python2.6 bootstrap.py
+
+#
+# Init askap environment
+#
+source initaskap.sh
+
+#
+# Build Analysis
+#
+nice rbuild -n Code/Components/Analysis/analysisutilities/current
+if [ $? -ne 0 ]; then
+    echo "rbuild -n Code/Components/Analysis/analysisutilities/current != 0"
+    exit 1
+fi
+
+nice rbuild -n Code/Components/Analysis/analysis/current
+if [ $? -ne 0 ]; then
+    echo "rbuild -n Code/Components/Analysis/analysis/current != 0"
+    exit 1
+fi
+
+nice rbuild -n Code/Components/Analysis/simulations/current
+if [ $? -ne 0 ]; then
+    echo "rbuild -n Code/Components/Analysis/simulations/current != 0"
+    exit 1
+fi
+
+#
+# Build testdata 
+#
+nice rbuild -n Code/Base/accessors/current
+if [ $? -ne 0 ]; then
+    echo "rbuild -n Code/Base/accessors/current != 0"
+    exit 1
+fi
+
+#
+# Run the unit tests
+#
+echo Running Unit Tests...
+cd $ASKAP_ROOT/Code/Components/Analysis/analysisutilities/current
+python build.py -q test
+cd $ASKAP_ROOT/Code/Components/Analysis/analysis/current
+python build.py -q test
+cd $ASKAP_ROOT/Code/Components/Analysis/simulations/current
+python build.py -q test
+echo Completed Unit Tests
+
+#
+# Run a simple duchamp test
+#
+TESTPACKAGE=/DATA/DELPHINUS_2/hum092/hudson-test-packages/analysis-test.tgz
+if [  ! -f $TESTPACKAGE ]; then
+    echo "Test package $TESTPACKAGE not found"
+    exit 1
+fi
+
+cd $WORKSPACE
+tar zxvf $TESTPACKAGE
+cd analysis-test
+./run.sh
+if [ $? -ne 0 ]; then
+    echo "Test returned != 0"
+    exit 1
+fi
