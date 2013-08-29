4232aab9cfc8ef4ff01237c8dbb86162b41bcb45
Tony Maher <tony.maher@csiro.au>
2013-08-29 00:30:05 +0000

    See Issue #3228 - Move Hudson TOS-0.20-release script to subversion.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5610 1db04d4c-0ff0-4329-ae0b-98c546476c6a

---
+++ Tests/hudson/TOS/tos-0.20-release.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -l
+
+# Bootstrap
+unset ASKAP_ROOT
+cd $WORKSPACE/TOS-0.20
+SVNREV=`svn info |grep 'Changed Rev'|awk '{print $NF;}'`
+REV=0.20_r${SVNREV}
+if [ ! -f initaskap.sh ]
+then
+    /usr/bin/python2.6 bootstrap.py
+    if [ $? -ne 0 ]; then
+       exit 1
+    fi
+fi
+# Setup ASKAP environment
+source initaskap.sh
+TOSNAME="TOS$REV"
+OUTDIR="/var/tmp/tos-releases/"
+trap 'rm -rf /tmp/release-* /tmp/TOS*' EXIT
+
+if [ -f "$OUTDIR/$TOSNAME/$TOSNAME.tgz" ]
+then
+   echo "$TOSNAME built already"
+   exit 0
+fi
+# clean Code
+#rbuild -S -t clean Code/Components/EPICS
+
+#Build TOS
+cd Code/Systems/TOS
+
+rbuild -S -t release --release-name=$TOSNAME
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+mkdir -p /var/tmp/tos-releases/$TOSNAME
+rfiles="install_release.sh fabfile.py $TOSNAME.tgz"
+for f in $rfiles
+do
+    if [ ! -f $f ]; then exit 1;fi
+    cp $f $OUTDIR/$TOSNAME/
+done
+
+# build TOS-CSS
+cd $ASKAP_ROOT/Code/Systems/TOS-CSS
+CSSNAME="TOS-CSS$REV"
+rbuild -n -t release --release-name=$CSSNAME
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+
+mkdir -p /var/tmp/tos-releases/$CSSNAME/
+rfiles="install_release.sh fabfile.py $CSSNAME.tgz"
+for f in $rfiles
+do
+    if [ ! -f $f ]; then exit 1;fi
+    cp $f $OUTDIR/$CSSNAME/
+done
+
+#clean up left overs
+RTMP="/tmp/release-${SVNREV}"
+if [ -e $RTMP ]
+then
+    rm -rf $RTMP
+fi
