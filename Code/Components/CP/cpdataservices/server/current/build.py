abfe238ba745df7d77e0e065d1f5f14a8336d1bc
Ben Humphreys <ben.humphreys@csiro.au>
2013-10-10 02:33:50 +0000

    Add lines to create wrapper scripts for the CP Java programs
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5734 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/cpdataservices/server/current/build.py
+++ Code/Components/CP/cpdataservices/server/current/build.py
@@ -1,5 +1,8 @@
 from askapdev.rbuild.builders import Ant as Builder
 
 builder = Builder(".")
+builder.add_run_script("skymodelsvc.sh", "-Xmx1024m askap/cp/skymodelsvc/Server")
+builder.add_run_script("caldatasvc.sh", "-Xmx1024m askap/cp/caldatasvc/Server")
+builder.add_run_script("rfisourcesvc.sh", "-Xmx1024m askap/cp/rfisourcesvc/Server")
 
 builder.build()
--- Code/Components/CP/manager/current/build.py
+++ Code/Components/CP/manager/current/build.py
@@ -1,5 +1,6 @@
 from askapdev.rbuild.builders import Ant as Builder
 
 builder = Builder(".")
+builder.add_run_script("cpmanager.sh", "askap/cp/manager/CpManager")
 
 builder.build()
