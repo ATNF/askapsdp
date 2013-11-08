0633e8de87e2f194d022cb53460caf7f8fa0894b
Matthew Whiting <matthew.whiting@csiro.au>
2013-11-08 10:04:45 +0000

    Various improvements to the finderEval script, including looking at completeness & reliability as a function of *both* flux and major axis. Also now able to consolidate plots into one single page.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5795 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/evaluation/current/scripts/finderEval.py
+++ Code/Components/Analysis/evaluation/current/scripts/finderEval.py
@@ -62,6 +62,8 @@ if __name__ == '__main__':
     srcmisslist = readMisses(missfile,sourceCat,'S')
     refmisslist = readMisses(missfile,refCat,'R')
 
+    doSinglePlot = inputPars.get_value('doSingleFinderPlot',False)
+
     ############################
     # Flux ratio plot, with guide lines showing noise and search limit
     imageNoise = inputPars.get_value('imageNoise','')
@@ -75,7 +77,15 @@ if __name__ == '__main__':
     fluxMin = inputPars.get_value('fluxMin',0.002)
     fluxMax = inputPars.get_value('fluxMax',1.5)
 
-    plt.figure(num=2,figsize=(8,8),dpi=72)
+    if doSinglePlot:
+        plt.figure(num=2,figsize=(12,9),dpi=72)
+        subplots_adjust(wspace=0.3,hspace=0.3)
+    else:
+        plt.figure(num=2,figsize=(8,8),dpi=72)
+
+    if doSinglePlot:
+        plt.subplot(3,4,1)
+
     plt.semilogx()
     for m in matchlist:
         fluxratio=m.src.flux()/m.ref.flux()
@@ -88,129 +98,181 @@ if __name__ == '__main__':
     plt.plot(x,(x-imageNoise*3)/x,'k--')
     plt.plot(x,(imageNoise*5)/x,'k:')
 
-    plt.xlabel('Reference flux')
-    plt.ylabel('Source flux / Reference flux')
-    plt.title(sourceCatFile)
     plt.xlim(fluxMin,fluxMax)
     plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('fluxRatio.png')
+
+    if doSinglePlot:
+        plt.xlabel('Flux')
+        plt.ylabel('Ratio(Flux)')
+    else:
+        plt.xlabel('Reference flux')
+        plt.ylabel('Source flux / Reference flux')
+        plt.title(sourceCatFile)
+        plt.savefig('fluxRatio.png')
 
     ############################
     # Major axis ratio vs flux plot        
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,2)
+    else:
+        plt.cla()
     plt.semilogx()
     for m in matchlist:
         majratio=m.src.maj/m.ref.maj
         plt.plot(m.ref.flux(),majratio,'k.')
 
-    plt.xlabel('Reference flux')
-    plt.ylabel('Source Major Axis / Reference Major Axis')
-    plt.title(sourceCatFile)
     plt.xlim(fluxMin,fluxMax)
     plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('majorAxisRatio_flux.png')
+    if doSinglePlot:
+        plt.xlabel('Flux')
+        plt.ylabel('Ratio(Major Axis)')
+    else:
+        plt.xlabel('Reference flux')
+        plt.ylabel('Source Major Axis / Reference Major Axis')
+        plt.title(sourceCatFile)
+        plt.savefig('majorAxisRatio_flux.png')
 
     
     ############################
     # Major axis ratio vs major axis plot        
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,3)
+    else:
+        plt.cla()
     for m in matchlist:
         majratio=m.src.maj/m.ref.maj
         plt.plot(m.ref.maj,majratio,'k.')
 
-    plt.xlabel('Reference major axis')
-    plt.ylabel('Source Major Axis / Reference Major Axis')
-    plt.title(sourceCatFile)
     #    plt.xlim(fluxMin,fluxMax)
     plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('majorAxisRatio_majorAxis.png')
+    if doSinglePlot:
+        plt.xlabel('Major axis')
+        plt.ylabel('Ratio(Major Axis)')
+    else:
+        plt.xlabel('Reference major axis')
+        plt.ylabel('Source Major Axis / Reference Major Axis')
+        plt.title(sourceCatFile)
+        plt.savefig('majorAxisRatio_majorAxis.png')
 
     
     ############################
     # Major axis ratio vs flux ratio plot        
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,4)
+    else:
+        plt.cla()
     for m in matchlist:
         majratio=m.src.maj/m.ref.maj
         fluxratio=m.src.flux()/m.ref.flux()
         plt.plot(fluxratio,majratio,'k.')
 
-    plt.xlabel('Source flux / Reference flux')
-    plt.ylabel('Source Major Axis / Reference Major Axis')
-    plt.title(sourceCatFile)
     plt.xlim(ratioMin,ratioMax)
     plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('majorAxisRatio_fluxRatio.png')
+    if doSinglePlot:
+        plt.xlabel('Ratio(Flux)')
+        plt.ylabel('Ratio(Major Axis)')
+    else:
+        plt.xlabel('Source flux / Reference flux')
+        plt.ylabel('Source Major Axis / Reference Major Axis')
+        plt.title(sourceCatFile)
+        plt.savefig('majorAxisRatio_fluxRatio.png')
 
     
     ############################
     # Axial ratio change, vs flux
-    plt.cla()
-    for m in matchlist:
-        srcAxialRatio=m.src.min/m.src.maj
-        refAxialRatio=m.ref.min/m.ref.maj
-        plt.plot(m.ref.flux(),srcAxialRatio/refAxialRatio,'k.')
+    #  *** DO NOT INCLUDE IN THE SINGLE PLOT ***
+    if not doSinglePlot:
+        
+        plt.cla()
+        for m in matchlist:
+            srcAxialRatio=m.src.min/m.src.maj
+            refAxialRatio=m.ref.min/m.ref.maj
+            plt.plot(m.ref.flux(),srcAxialRatio/refAxialRatio,'k.')
 
-    plt.semilogx()
-    plt.xlabel('Reference flux')
-    plt.ylabel('Source Axial Ratio / Reference Axial Ratio')
-    plt.title(sourceCatFile)
-    plt.xlim(fluxMin,fluxMax)
-    plt.ylim(ratioMin,ratioMax)
+        plt.semilogx()
+        plt.xlim(fluxMin,fluxMax)
+        plt.ylim(ratioMin,ratioMax)
+        plt.xlabel('Reference flux')
+        plt.ylabel('Source Axial Ratio / Reference Axial Ratio')
     
-    plt.savefig('axialRatioChange_flux.png')
+        plt.title(sourceCatFile)
+        plt.savefig('axialRatioChange_flux.png')
 
     
     ############################
     # Position angle change, vs flux
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,6)
+    else:
+        plt.cla()
     for m in matchlist:
         paDiff = m.src.pa - m.ref.pa
         plt.plot(m.ref.flux(),paDiff,'k.')
 
     plt.semilogx()
-    plt.xlabel('Reference flux')
-    plt.ylabel('Source Position Angle - Reference Position Angle [deg]')
-    plt.title(sourceCatFile)
     plt.xlim(fluxMin,fluxMax)
     #    plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('posangDiff_flux.png')
+    if doSinglePlot:
+        plt.xlabel('Reference flux')
+        plt.ylabel('Diff(Position Angle)')
+    else:
+        plt.xlabel('Reference flux')
+        plt.ylabel('Source Position Angle - Reference Position Angle [deg]')
+        plt.title(sourceCatFile)
+        plt.savefig('posangDiff_flux.png')
 
     ############################
     # Position angle change, vs major axis
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,7)
+    else:
+        plt.cla()
     for m in matchlist:
         paDiff = m.src.pa - m.ref.pa
         plt.plot(m.ref.maj,paDiff,'k.')
 
     plt.semilogx()
-    plt.xlabel('Major Axis')
-    plt.ylabel('Source Position Angle - Reference Position Angle [deg]')
-    plt.title(sourceCatFile)
     #    plt.xlim(fluxMin,fluxMax)
     #    plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('posangDiff_majorAxis.png')
+    if doSinglePlot:
+        plt.xlabel('Major Axis')
+        plt.ylabel('Diff(Position Angle)')
+    else:
+        plt.xlabel('Major Axis')
+        plt.ylabel('Source Position Angle - Reference Position Angle [deg]')
+        plt.title(sourceCatFile)
+        plt.savefig('posangDiff_majorAxis.png')
 
     ############################
     # Positional offsets
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,5)
+    else:
+        plt.cla()
+    dra=[]
+    ddec=[]
     for m in matchlist:
-        dra = (m.src.ra - m.ref.ra)*cos(m.ref.dec*pi/180.) * 3600.
-        ddec = (m.src.dec - m.ref.dec) * 3600.
-        plt.plot(dra,ddec,'k.')
+        dra.append((m.src.ra - m.ref.ra)*cos(m.ref.dec*pi/180.) * 3600.)
+        ddec.append((m.src.dec - m.ref.dec) * 3600.)
 
-    plt.xlabel('Source RA - Reference RA [arcsec]')
-    plt.ylabel('Source Dec - Reference Dec [arcsec]')
-    plt.title(sourceCatFile)
+    dra=np.array(dra,dtype=float)
+    ddec=np.array(ddec,dtype=float)    
+    plt.plot(dra,ddec,'k.')
+    
+    #plot error ellipse
+    angle=linspace(0,2*pi,100)
+    plt.plot(dra.std()*cos(angle)+dra.mean(),ddec.std()*sin(angle)+ddec.mean(),'r-')
+
     #    plt.xlim(fluxMin,fluxMax)
     #    plt.ylim(ratioMin,ratioMax)
-    
-    plt.savefig('posOffsets.png')
+    if doSinglePlot:
+        plt.xlabel(r'$\delta$RA [arcsec]')
+        plt.ylabel(r'$\delta$Dec [arcsec]')
+    else:
+        plt.xlabel('Source RA - Reference RA [arcsec]')
+        plt.ylabel('Source Dec - Reference Dec [arcsec]')
+        plt.title(sourceCatFile)
+        plt.savefig('posOffsets.png')
 
     
     ############################
@@ -220,6 +282,10 @@ if __name__ == '__main__':
     for m in matchlist:
         f.append(m.ref.flux())
         f.append(m.src.flux())
+    for r in refmisslist:
+        f.append(r.flux())
+    for s in srcmisslist:
+        f.append(s.flux())
     f=np.array(f,dtype=float)
     minFlux=floor(log10(f.min())*2.)/2.
     maxFlux=ceil(log10(f.max())*2.)/2.
@@ -264,28 +330,39 @@ if __name__ == '__main__':
     print reliabilityBinnedByFlux
     print fluxBin
     
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,9)
+    else:
+        plt.cla()
     plt.semilogx()
     plt.step(fluxBinComp,completenessBinnedByFluxValid)
     plt.ylim(0,1.1)
     print minFlux,maxFlux
     plt.xlim(10**minFlux,10**maxFlux)
-    plt.xlabel('Reference Flux')
+    plt.xlabel('Flux')
     plt.ylabel('Completeness')
-    plt.title(sourceCatFile)
-    plt.savefig('completeness.png')
+    if not doSinglePlot:
+        plt.title(sourceCatFile)
+        plt.savefig('completeness.png')
 
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,10)
+    else:
+        plt.cla()
     plt.semilogx()
     plt.step(fluxBinRel,reliabilityBinnedByFluxValid)
     plt.ylim(0,1.1)
-    plt.xlim(minFlux,maxFlux)
-    plt.xlabel('Reference Flux')
+    plt.xlim(10**minFlux,10**maxFlux)
+    plt.xlabel('Flux')
     plt.ylabel('Reliability')
-    plt.title(sourceCatFile)
-    plt.savefig('reliability.png')
+    if not doSinglePlot:
+        plt.title(sourceCatFile)
+        plt.savefig('reliability.png')
 
-    plt.cla()
+    if doSinglePlot:
+        plt.subplot(3,4,8)
+    else:
+        plt.cla()
     plt.plot(reliabilityBinnedByFluxJoint,completenessBinnedByFluxJoint,'bo')
     plt.plot(reliabilityBinnedByFluxJoint,completenessBinnedByFluxJoint,'b-')
     plt.plot(reliabilityBinnedByFluxJoint[0],completenessBinnedByFluxJoint[0],'ro')
@@ -294,10 +371,97 @@ if __name__ == '__main__':
     plt.xlim(0,1.1)
     plt.xlabel('Reliability')
     plt.ylabel('Completeness')
-    plt.title(sourceCatFile)
-    plt.savefig('completeness_reliability.png')
+    if not doSinglePlot:
+        plt.title(sourceCatFile)
+        plt.savefig('completeness_reliability.png')
 
+    #############################
 
+    f=[]
+    for m in matchlist:
+        f.append(m.ref.flux())
+        f.append(m.src.flux())
+    for r in refmisslist:
+        f.append(r.flux())
+    for s in srcmisslist:
+        f.append(s.flux())
+    f=np.array(f,dtype=float)
+    minFlux=floor(log10(f.min())*2.)/2.
+    maxFlux=ceil(log10(f.max())*2.)/2.
+    a=[]
+    for m in matchlist:
+        a.append(m.src.maj)
+        a.append(m.ref.maj)
+    for r in refmisslist:
+        a.append(r.maj)
+    for s in srcmisslist:
+        a.append(s.maj)
+    a=np.array(a,dtype=float)
+    amin=floor(a.min()/5.)*5
+    amax=ceil(a.max()/5.)*5
+
+    nmatch2d=np.zeros(((amax-amin)/5.,(maxFlux-minFlux)*10.))
+    nmissSrc2d=np.zeros(((amax-amin)/5.,(maxFlux-minFlux)*10.))
+    nmissRef2d=np.zeros(((amax-amin)/5.,(maxFlux-minFlux)*10.))
+
+    for m in matchlist:
+        abin=int((m.ref.maj-amin)/5.)
+        fbin=int((log10(m.src.flux())-minFlux)*10)
+        nmatch2d[abin][fbin] += 1
+    for s in srcmisslist:
+        abin=int((s.maj-amin)/5.)
+        fbin=int((log10(s.flux())-minFlux)*10)
+        nmissSrc2d[abin][fbin] += 1
+    for r in refmisslist:
+        abin=int((r.maj-amin)/5.)
+        fbin=int((log10(r.flux())-minFlux)*10)
+        nmissRef2d[abin][fbin] += 1
     
+    nSrc2d = nmatch2d + nmissSrc2d
+    nRef2d = nmatch2d + nmissRef2d
+
+#    # Do the inverse to make the plotting better.
+#    comp2d = 1-nmatch2d/nRef2d
+#    rel2d  = 1-nmatch2d/nSrc2d
+    comp2d = nmatch2d/nRef2d
+    rel2d  = nmatch2d/nSrc2d
+#
+#    for x in range(comp2d.shape[0]):
+#        for y in range(comp2d.shape[1]):
+#            if isnan(comp2d[x][y]):
+#                comp2d[x][y]=-1.
+#    for x in range(rel2d.shape[0]):
+#        for y in range(rel2d.shape[1]):
+#            if isnan(rel2d[x][y]):
+#                rel2d[x][y]=-1.
+
+    if doSinglePlot:
+        plt.subplot(3,4,11)
+    else:
+        plt.cla()
+    extent=(minFlux,maxFlux,amin,amax)
+    plt.imshow(comp2d,cmap='rainbow',interpolation='nearest',origin='lower',extent=extent)
+    plt.axis('normal')
+    plt.ylim(amin,amax)
+    plt.xlabel('log10(Flux)')
+    plt.ylabel('Major axis')
+    plt.title('Completeness')
+    
+    if doSinglePlot:
+        plt.subplot(3,4,12)
+    else:
+        plt.cla()
+    extent=(minFlux,maxFlux,amin,amax)
+    plt.imshow(rel2d,cmap='rainbow',interpolation='nearest',origin='lower',extent=extent)
+    plt.axis('normal')
+    plt.ylim(amin,amax)
+    plt.xlabel('log10(Flux)')
+    plt.ylabel('Major axis')
+    plt.title('Reliability')
+    
+    #############################
+    if doSinglePlot:
+        plt.suptitle(sourceCatFile,y=0.95)
+        plt.savefig('finderEval.png')
     
     plt.close()
