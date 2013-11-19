1b39fde20b82ec69fa375f1c1c91f17c0f211d4d
Tony Maher <tony.maher@csiro.au>
2013-11-19 23:35:59 +0000

    See Issue #2897 - remove the explicit no doc handling in package build.py files. This is handled by doc.py command in rbuild.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@5836 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Analysis/evaluation/current/build.py
+++ Code/Components/Analysis/evaluation/current/build.py
@@ -1,10 +1,4 @@
-import sys
-
 from askapdev.rbuild.builders import Setuptools as Builder
-
-if 'doc' in sys.argv:
-    print 'warn: unable to handle doc target'
-    sys.exit(0)
 
 builder = Builder(".")
 builder.build()
--- Code/Interfaces/python/current/build.py
+++ Code/Interfaces/python/current/build.py
@@ -1,10 +1,6 @@
-import sys
-
 from askapdev.rbuild.builders import Setuptools as Builder
 
 # This generated package is not documented. The docs are build as part of ice.
-if 'doc' in sys.argv:
-    sys.exit(0)
 
 builder = Builder(".")
 builder.build()
