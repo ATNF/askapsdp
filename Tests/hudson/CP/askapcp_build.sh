ba42f29358fc3d38de13538055d8b3670f02e03b
Ben Humphreys <ben.humphreys@csiro.au>
2014-02-06 07:42:39 +0000

    Update Jenkins askapcp_build script to generate a release tarball
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@6024 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/hudson/CP/askapcp_build.sh
+++ Tests/hudson/CP/askapcp_build.sh
@@ -18,11 +18,25 @@ source initaskap.sh
 export AIPSPATH=$ASKAP_ROOT/Code/Base/accessors/current
 source initaskap.sh
 
+cd Code/Systems/cpapps
+
 #
-# Build CP
+# Build CP applications
 #
-nice rbuild -a -M -S -T Code/Systems/cpapps
+nice rbuild -a -M -S -T
 if [ $? -ne 0 ]; then
-    echo "Error: rbuild -a -M -S -T Code/Systems/cpapps"
+    echo "Error: rbuild -a -M -S -T"
     exit 1
 fi
+
+#
+# Build a release tarball
+#
+nice rbuild -n  -M -S -T -t release 
+if [ $? -ne 0 ]; then
+    echo "Error: rbuild rbuild -n  -M -S -T -t release"
+    exit 1
+fi
+
+# Need a "standard name" for the artifact
+mv release-*.tgz cpapps-release.tgz
