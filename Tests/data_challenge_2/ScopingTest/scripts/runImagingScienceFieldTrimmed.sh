84b2c30c228f0ae4caee5d6f180c732f3d3d18bf
Matthew Whiting <matthew.whiting@csiro.au>
2014-02-18 00:28:47 +0000

    Getting the naming right for the linmos jobs.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@6065 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/data_challenge_2/ScopingTest/scripts/runImagingScienceFieldTrimmed.sh
+++ Tests/data_challenge_2/ScopingTest/scripts/runImagingScienceFieldTrimmed.sh
@@ -5,48 +5,50 @@ inputTrimFlag=${doTrim}
 
 doTrim=true
 POINTING=0
+imdepend=""
 while [ $POINTING -lt 9 ]; do
     . ${scriptdir}/runImagingScienceField.sh
-    depend="${depend}:${latestID}"
+    imdepend="${imdepend}:${latestID}"
     POINTING=`expr $POINTING + 1`
 done
 
 doTrim=${inputTrimFlag}
 
+. ${scriptdir}/getTags.sh
 
-linmosqsub=linmos_${tag}.qsub
+linmosqsub=linmos_${imtag}.qsub
 cat > $linmosqsub <<EOF
 #!/bin/bash -l
 #PBS -l walltime=01:00:00
 #PBS -l mppwidth=1
 #PBS -l mppnppn=1
-#PBS -N linmos
+#PBS -N linmos${imtag}
 #PBS -m a
 #PBS -j oe
 #PBS -v ASKAP_ROOT,AIPSPATH
 
 cd \$PBS_O_WORKDIR
 
-linmos=${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/current/apps/linmos.sh
+linmos=\${ASKAP_ROOT}/Code/Components/Synthesis/synthesis/current/apps/linmos.sh
 
-suffix=uncor
-
-parset=parsets/linmos_${suffix}.in
-cat > $parset <<EOF_INNER
-linmos.names = [image.i.dirty.science.${imtag}0..8.restored]
+parset=parsets/linmos_${imtag}.in
+cat > \${parset} <<EOF_INNER
+linmos.names = [image.i.dirty.science.${imtag}.0..8.restored]
 linmos.weights = [weights.i.dirty.science.${imtag}.0..8]
 linmos.outname = image.i.dirty.science.${imtag}.linmos.restored
 linmos.outweight = weights.i.dirty.science.${imtag}.linmos
 linmos.weighttype = FromWeightImages
 #linmos.weightstate  = Corrected
 EOF_INNER
-log=logs/linmos_${suffix}.log
+log=logs/linmos_${imtag}.log
 
-aprun ${linmos} -c ${parset} > ${log}
+aprun \${linmos} -c \${parset} > \${log}
 
 
 EOF
 
-qsub ${depend} ${linmosqsub}
+ID=`qsub -Wdepend=afterok${imdepend} ${linmosqsub}`
+echo "Have submitted a linmos job for type ${imtag} with ID=${ID}, via 'qsub -Wdepend=afterok${imdepend} ${linmosqsub}'"
+
 
 
