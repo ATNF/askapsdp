9eae10fcc8544a060932a3206ab5cd78a0f26a67
Ben Humphreys <ben.humphreys@csiro.au>
2014-02-21 03:16:34 +0000

    The ASKAP CP build (for the Cray) now builds and archives documentation artifacts
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/Src/trunk@6095 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tests/hudson/CP/askapcp_build.sh
+++ Tests/hudson/CP/askapcp_build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -l
 
 #
+# Make a directory to contain the artifacts
+#
+ARTIFACTS_DIR=$WORKSPACE/artifacts
+mkdir -p $ARTIFACTS_DIR
+if [ $? -ne 0 ]; then
+    echo "Error: failed to make artifacts directory"
+    exit 1
+fi
+
+#
 # Bootstrap
 #
 unset ASKAP_ROOT
@@ -32,11 +42,44 @@ fi
 #
 # Build a release tarball
 #
-nice rbuild -n  -M -S -T -t release 
+nice rbuild -n -M -S -T -t release 
 if [ $? -ne 0 ]; then
-    echo "Error: rbuild rbuild -n  -M -S -T -t release"
+    echo "Error: rbuild -n -M -S -T -t release"
     exit 1
 fi
 
 # Need a "standard name" for the artifact
 mv release-*.tgz cpapps-release.tgz
+mv cpapps-release.tgz $ARTIFACTS_DIR
+
+#
+# Build user documentation
+#
+cd $WORKSPACE/trunk/Code/Components/CP/docs/current
+nice rbuild -n -M -S -T -t doc
+if [ $? -ne 0 ]; then
+    echo "Error: rbuild -n -M -S -T -t doc failed"
+    exit 1
+fi
+cd doc/_build
+nice tar zcvf ${ARTIFACTS_DIR}/cp-docs.tgz html
+if [ $? -ne 0 ]; then
+    echo "Error: tar failed"
+    exit 1
+fi
+
+#
+# Build admin documentation
+#
+cd $WORKSPACE/trunk/Code/Components/CP/docs_admin/current
+nice rbuild -n -M -S -T -t doc
+if [ $? -ne 0 ]; then
+    echo "Error: rbuild -n -M -S -T -t doc failed"
+    exit 1
+fi
+cd doc/_build
+nice tar zcvf ${ARTIFACTS_DIR}/cp-docs-admin.tgz html
+if [ $? -ne 0 ]; then
+    echo "Error: tar failed"
+    exit 1
+fi
