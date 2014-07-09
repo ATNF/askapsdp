68a7a0088bfbbf3f6d1752fdad7c1298122e5fc0
Ben Humphreys <ben.humphreys@csiro.au>
2014-07-09 04:58:25 +0000

    The cpmanager now no longer conditionally compiles based on the existance of Java 7.
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6403 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/CP/manager/current/build.py
+++ Code/Components/CP/manager/current/build.py
@@ -1,22 +1,4 @@
-import subprocess
-import string
-import sys
 from askapdev.rbuild.builders import Ant as Builder
-
-##########################################################################
-# This is a temporary check for Java 7. Once we deprecate our Java 6 only
-# platforms (Debian Squeeze , OSX Snow Leopard) this check can be removed
-def is_java7():
-    sp = subprocess.Popen(["java", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = sp.communicate()
-    sp.wait()
-    idx = out[1].find('1.7')
-    return idx != -1
-
-if not is_java7():
-    print "Note: No Java 7 present, skipping build"
-    sys.exit(0)
-##########################################################################
 
 builder = Builder(".")
 builder.add_run_script("cpmanager.sh", "askap/cp/manager/CpManager")
