824f47161d75531662e17b49d17b9e396b460c39
Ben Humphreys <ben.humphreys@csiro.au>
2014-07-16 07:48:26 +0000

    Bug #6179: Use svnversion command instead of inspecting .svn
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6414 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tools/Dev/rbuild/askapdev/rbuild/utils/get_svn_revision.py
+++ Tools/Dev/rbuild/askapdev/rbuild/utils/get_svn_revision.py
@@ -1,7 +1,7 @@
-##  @file
-# askapdev.rbuild module containing tools to assist with the build process.
+# @file get_svn_revision.py
+# Fetch the subversion revision number from the repository
 #
-# @copyright (c) 2006 CSIRO
+# @copyright (c) 2006,2014 CSIRO
 # Australia Telescope National Facility (ATNF)
 # Commonwealth Scientific and Industrial Research Organisation (CSIRO)
 # PO Box 76, Epping NSW 1710, Australia
@@ -26,65 +26,17 @@
 # @author Robert Crida <robert.crida@ska.ac.za>
 #
 
-import os
-import re
+import subprocess
 
 def get_svn_revision():
-    '''Function 'lifted' from setuptools.commands.egg_info.
-    If it was a member function which was static then we could have used it
-    more easily.  Instead we duplicate it here.
-    '''
-    maxRevision = 0
-    minRevision = 1e10
-    errorFlag = False
-    urlre = re.compile('url="([^"]+)"')
-    revre = re.compile('revision="(\d+)"')
+    sp = subprocess.Popen("svnversion", stdout=subprocess.PIPE)
+    (stdout, stderr) = sp.communicate()
+    if sp.wait() != 0:
+        return "nonsvn"
 
-    for base, dirs, files in os.walk(os.curdir):
-        try:
-            if '.svn' not in dirs:
-                dirs[:] = []
-                continue    # no sense walking uncontrolled subdirs
-            dirs.remove('.svn')
-            efh = open(os.path.join(base, '.svn', 'entries'))
-            data = efh.read()
-            efh.close()
-
-            if (data.startswith('8') or data.startswith('9') or
-               data.startswith('10') or data.startswith('11') or
-               data.startswith('12')):
-                data = map(str.splitlines, data.split('\n\x0c\n'))
-                del data[0][0]  # get rid of the '8', '9', '10'
-                dirurl = data[0][3]
-                dirrev = int(data[0][2])
-                maxlocalrev = max([int(d[2] or dirrev) for d in data if len(d)>9])
-                minlocalrev = min([int(d[2] or dirrev) for d in data if len(d)>9])
-            elif data.startswith('<?xml'):
-                dirurl = urlre.search(data).group(1)    # get repository URL
-                maxlocalrev = max([int(m.group(1)) for m in revre.finditer(data)])
-                minlocalrev = min([int(m.group(1)) for m in revre.finditer(data)])
-            else:
-                from warnings import warn
-                warn("unrecognized .svn/entries format; skipping "+base)
-                dirs[:] = []
-                continue
-            if base == os.curdir:
-                base_url = dirurl+'/'   # save the root url
-            elif not dirurl.startswith(base_url):
-                dirs[:] = []
-                continue    # not part of the same svn tree, skip it
-            maxRevision = max(maxRevision, maxlocalrev)
-            minRevision = min(minRevision, minlocalrev)
-        except:
-            # Don't break if something goes wrong!
-            errorFlag = True
-
-    if maxRevision != minRevision:
-        revision = "[%s-%s]" % (str(minRevision), str(maxRevision))
-        if (maxRevision+minRevision) == minRevision:
-            revision = "nonsvn"
+    # Generally error conditions will result in a non-numeric first
+    # digit so as a safeguard at least check that
+    if stdout and stdout[0].isdigit():
+        return stdout.rstrip()
     else:
-        revision = str(maxRevision)
-    if errorFlag:
-        revision += "ERRORS"
-    return revision
+        return "nonsvn"
