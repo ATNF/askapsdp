f98a2869bd87f0a65234c8d9ce7fa4220dd07a41
Maxim Voronkov <maxim.voronkov@csiro.au>
2014-07-17 03:50:03 +0000

    moved supported utilities into a separate package (#6176)
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6416 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Code/Components/Utilities/current/apps/genoffsets.cc
+++ Code/Components/Utilities/current/apps/genoffsets.cc
@@ -24,7 +24,7 @@
 /// along with this program; if not, write to the Free Software
 /// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
-#include <askap_swcutils.h>
+#include <askap_utilities.h>
 #include <askap/AskapLogging.h>
 ASKAP_LOGGER(logger, "genoffsets");
 
