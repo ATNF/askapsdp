4b4f989025eaf48eb5a01b6e0c2311268f952bae
Ben Humphreys <ben.humphreys@csiro.au>
2014-09-09 07:28:02 +0000

    Update Boost from 1.55.0 to 1.56.0
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6522 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- 3rdParty/boost/boost-1.56.0/build.py
+++ 3rdParty/boost/boost-1.56.0/build.py
@@ -8,7 +8,7 @@ boost_dir = boost_version.replace('.','_').replace('-','_')
 
 builder = Builder(pkgname=boost_dir, confcommand='bootstrap.sh',
                   buildcommand='./bjam', installcommand='./bjam install')
-builder.remote_archive = "boost_1_55_0.tar.bz2"
+builder.remote_archive = "boost_1_56_0.tar.bz2"
 
 builder.add_option('--with-libraries=python,date_time,filesystem,program_options,thread,system,regex --prefix=builder._prefix' )
 builder.nowarnings = True
