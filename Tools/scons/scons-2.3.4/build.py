5103d0ec97eb03a46628f863760427597cac0e09
Ben Humphreys <ben.humphreys@csiro.au>
2015-01-14 03:06:35 +0000

    ASKAPSDP-1641: Update Scons dependency to 2.3.4
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6672 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- Tools/scons/scons-2.3.4/build.py
+++ Tools/scons/scons-2.3.4/build.py
@@ -1,6 +1,6 @@
 from askapdev.rbuild.builders import Setuptools as Builder
 
 builder = Builder()
-builder.remote_archive = "scons-2.2.0.tar.gz"
+builder.remote_archive = "scons-2.3.4.tar.gz"
 builder.nowarnings = True
 builder.build()
