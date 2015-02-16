58164717ad0cda6488272c4614f56392bae5218b
Ben Humphreys <ben.humphreys@csiro.au>
2015-02-16 07:25:25 +0000

    ASKAPSDP-1646: Fixed undefined "_OPENSSL_ia32cap_P" error on OSX
    In using OpenSSL to generate SHA1 checksums in the "casdaupload" utility
    an error "Undefined symbols for architecture x86_64: _OPENSSL_ia32cap_P"
    occured. Disabling "asm" on the OSX build resolves this problem.
    
    
    git-svn-id: https://svn.atnf.csiro.au/askapsdp/trunk@6743 1db04d4c-0ff0-4329-ae0b-98c546476c6a

--- 3rdParty/openssl/openssl-1.0.1h/build.py
+++ 3rdParty/openssl/openssl-1.0.1h/build.py
@@ -5,6 +5,12 @@ platform = utils.get_platform()
 
 if platform['system'] == 'Darwin':
     builder = Builder(confcommand='Configure darwin64-x86_64-cc')
+
+    # This works around the problem "Undefined symbols for architecture x86_64:
+    # _OPENSSL_ia32cap_P" experienced in using OpenSSL in the "casdaupload"
+    #  utility to generate SHA1 checksums. See Jira issue ASKAPSDP-1646
+    builder.add_option('no-asm')
+
 elif platform['system'] == 'Linux' and platform['architecture'] == '64bit':
     builder = Builder(confcommand='Configure linux-x86_64')
 else:
